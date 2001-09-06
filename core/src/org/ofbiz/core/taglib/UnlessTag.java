ff47c8b05d35fd00a70c3469305b22d7e4f222f3
epabst <>
2001-09-06 16:26:00 +0000

    simplify a ton by just extending IfTag

--- core/src/org/ofbiz/core/taglib/UnlessTag.java
+++ core/src/org/ofbiz/core/taglib/UnlessTag.java
@@ -1,6 +1,9 @@
 /*
  * $Id$
- * $Log$ 
+ * $Log$
+ * Revision 1.1  2001/09/01 01:59:06  azeneski
+ * Added two new JSP tags.
+ * 
  */
 
 package org.ofbiz.core.taglib;
@@ -40,139 +43,9 @@ import org.ofbiz.core.util.Debug;
  * @version 1.0
  * Created on August 31, 2001, 7:57 PM
  */
-public class UnlessTag extends BodyTagSupport {
-    
-    private String name = null;
-    private String value = null;
-    private String type = null;
-    private int size = -1;
-    
-    public void setName(String name) {
-        this.name = name;
-    }
-    
-    public void setValue(String value) {
-        this.value = value;
-    }
-    
-    public void setType(String type) {
-        this.type = type;
-    }
-    
-    public void setSize(String size) throws NumberFormatException {
-        this.size = Integer.parseInt(size);
-    }
-    
-    public String getName() {
-        return name;
-    }
-    
-    public String getValue() {
-        return value;
-    }
-    
-    public String getType() {
-        return type;
-    }
-    
-    public int getSize() {
-        return size;
-    }
-    
+public class UnlessTag extends IfTag {
     public int doStartTag() throws JspTagException {
-        
-        Object object = null;
-        try {
-            object = pageContext.findAttribute(name);
-            if ( object == null )
-                return EVAL_BODY_TAG;
-        }
-        catch ( Exception e ) { return EVAL_BODY_TAG; }
-        Debug.logInfo("Found object, and is not null");
-        
-        if ( size == -1 && value == null && type == null )
-            return SKIP_BODY;
-        
-        if (  size > -1 ) {
-            // Assume the object is a Collection and compare the size.
-            try {
-                Collection c = (Collection) object;
-                if ( c.size() > size )
-                    return SKIP_BODY;
-            }
-            catch ( Exception e ) { return EVAL_BODY_TAG; }
-        }
-        
-        else if ( type.equalsIgnoreCase("String") ) {
-            // Assume the object is a string and compare to the String value of value.
-            try {
-                String s = (String) object;
-                if ( s.equals(value) )
-                    return SKIP_BODY;
-            }
-            catch ( Exception e ) { return EVAL_BODY_TAG; }
-        }
-        
-        else if ( type.equalsIgnoreCase("Integer") ) {
-            // Assume the object is a Integer and compare to the Integer value of value.
-            try {
-                Integer i = (Integer) object;
-                Integer v = new Integer(value);
-                if ( i == v )
-                    return SKIP_BODY;
-            }
-            catch ( Exception e ) { return EVAL_BODY_TAG; }
-        }
-        
-        else if ( type.equalsIgnoreCase("Double") ) {
-            // Assume the object is a Double and compare to the Double value of value.
-            try {
-                Double d = (Double) object;
-                Double v = new Double(value);
-                if ( d == v )
-                    return SKIP_BODY;
-            }
-            catch ( Exception e ) { return EVAL_BODY_TAG; }
-        }
-        
-        else if ( type.equalsIgnoreCase("Boolean") ) {
-            // Assume the object is a Boolean and compare to the Boolean value of value.
-            try {
-                Boolean b = (Boolean) object;
-                Boolean v = new Boolean(value);
-                if ( b.equals(v) )
-                    return SKIP_BODY;
-            }
-            catch ( Exception e ) { return EVAL_BODY_TAG; }
-        }
-        
-        else {
-            // Assume the object is an Object and compare to the Object named value.
-            Object valueObject = null;
-            try {
-                valueObject = pageContext.findAttribute(value);
-                if ( valueObject != null && valueObject.equals(object) )
-                    return SKIP_BODY;
-            }
-            catch ( Exception e ) { return EVAL_BODY_TAG; }
-        }
-        
-        return EVAL_BODY_TAG;        
-    }
-    
-    public int doAfterBody() {
-        return SKIP_BODY;
-    }
-    
-    public int doEndTag() {
-        try {
-            BodyContent body = getBodyContent();
-            if (body != null) {
-                JspWriter out = body.getEnclosingWriter();
-                out.print(body.getString());
-            }
-        }
-        catch(IOException e) { Debug.logError(e,"IfTag Error."); }
-        return EVAL_PAGE;
+        //do the opposite of what IfTag does. EVAL_BODY_TAG -> SKIP_BODY, vice-versa
+        return (EVAL_BODY_TAG + SKIP_BODY) - super.doStartTag();
     }
 }
