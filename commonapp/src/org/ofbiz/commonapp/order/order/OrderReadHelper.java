a0e34cb72de7a2e8357ffebc30e13350e3f4efc2
epabst <>
2001-09-12 17:14:33 +0000

    added helpers

---
+++ commonapp/src/org/ofbiz/commonapp/order/order/OrderReadHelper.java
@@ -0,0 +1,216 @@
+/*
+ * OrderHelper.java
+ *
+ * Created on August 28, 2001, 11:25 AM
+ */
+
+package org.ofbiz.commonapp.order.order;
+
+import java.util.*;
+
+import org.ofbiz.core.entity.*;
+import org.ofbiz.core.util.*;
+
+/**
+ *
+ * XXX
+ * adjustment:  
+ * order item/amount:  multiply by quantity (unit price adjustment)
+ * order item/percentage: multiply by unit price * quantity
+ * order header/amount: flat amount
+ * order header/percentage: multiply by order subtotal (sum of item totals)
+ *
+ * adjustments MUST only be a percentage or an amount
+ *
+ * @author  epabst
+ * @version 
+ */
+public class OrderReadHelper {
+    private GenericValue orderHeader;
+    private Double orderItemTotal;
+    private Double totalPrice;
+    
+    public OrderReadHelper(GenericValue orderHeader) {
+        this.orderHeader = orderHeader;
+    }
+
+    public String getShippingMethod() {
+        GenericValue shipmentPreference = orderHeader.getRelatedOne("OrderShipmentPreference");
+        return shipmentPreference.getString("carrierPartyId") + " " 
+                + shipmentPreference.getRelatedOne("CarrierShipmentMethod").getRelatedOne("ShipmentMethodType").getString("description");
+    }
+    
+    private static GenericValue getFirst(Collection values) {
+        if ((values != null) && (values.size() > 0)) {
+            return (GenericValue) values.iterator().next();
+        } else {
+            return null;
+        }
+    }
+    
+    public GenericValue getShippingAddress() {
+        GenericHelper helper = orderHeader.getHelper();
+        GenericValue orderContactMech = getFirst(helper.findByAnd("OrderContactMech", UtilMisc.toMap("contactMechPurposeTypeId", 
+                "SHIPPING_LOCATION", "orderId", orderHeader.getString("orderId")), null));
+        if (orderContactMech != null) {
+            GenericValue contactMech = orderContactMech.getRelatedOne("ContactMech");
+            if (contactMech != null) {
+                return contactMech.getRelatedOne("PostalAddress");
+            }
+        }
+        return null;
+    }
+        
+    public double getShippingTotal() {
+        GenericHelper helper = orderHeader.getHelper();
+        Iterator shippingChargeIter = helper.findByAnd("OrderAdjustment", UtilMisc.toMap(
+              "orderId", orderHeader.getString("orderId"), 
+                "orderAdjustmentTypeId", "SHIPPING_CHARGES"), null).iterator();
+        double result = 0.0;
+        while (shippingChargeIter.hasNext()) {
+            GenericValue shippingCharge = (GenericValue) shippingChargeIter.next();
+            //FIXME should check percentage and watch for null amount
+            result += shippingCharge.getDouble("amount").doubleValue();
+        }
+        return result;
+    }
+    
+    public double getTotalPrice() {
+        if (totalPrice == null) {
+            double total = getOrderItemsTotal();
+            Debug.log("getTotalPrice: itemsTotal=" + total);
+            Iterator iter = getAdjustmentIterator();
+            while (iter.hasNext()) {
+                Adjustment adjustment = (Adjustment) iter.next();
+                Debug.log("getTotalPrice: adjustment=" + adjustment.getAmount());
+                total += adjustment.getAmount();
+            }
+            totalPrice = new Double(total);
+        }//else already set
+        Debug.log("getTotalPrice: result=" + totalPrice);
+        return totalPrice.doubleValue();
+    }
+    
+    public String getStatusString() {
+        Collection orderStatusList = orderHeader.getRelated("OrderStatus");
+        Set orderStatusIdSet = new HashSet();
+        Iterator orderStatusIter = orderStatusList.iterator();
+        while (orderStatusIter.hasNext()) {
+            orderStatusIdSet.add(((GenericValue) orderStatusIter.next()).getString("statusId"));
+        }
+        Iterator orderStatusIdIter = orderStatusIdSet.iterator();
+        String orderStatusIds;
+        if (orderStatusIdIter.hasNext()) {
+            orderStatusIds = orderStatusIdIter.next().toString();
+            while (orderStatusIdIter.hasNext()) {
+                orderStatusIds += "/" + orderStatusIdIter.next().toString();
+            }
+        } else {
+            orderStatusIds = "(unspecified)";
+        }
+        return orderStatusIds;
+    }
+    
+    public GenericValue getBillToPerson() {
+        GenericHelper helper = orderHeader.getHelper();
+        Collection billToRoleList = helper.findByAnd("OrderRole", UtilMisc.toMap(
+                "orderId", orderHeader.getString("orderId"), 
+                "roleTypeId", "BILL_TO_CUSTOMER"), null);
+        if (billToRoleList.size() > 0) {
+            GenericValue billToRole = (GenericValue) billToRoleList.iterator().next();
+            //XXX this will fail: 
+            //  return billToRole.getRelatedOne("Party").getRelatedOne("Person");
+            return helper.findByPrimaryKey("Person", UtilMisc.toMap("partyId", billToRole.getString("partyId")));
+        } else {
+            return null;
+        }
+    }
+    
+    public double getOrderItemsTotal() {
+        double result = 0.0;
+        Iterator itemIter = orderHeader.getRelated("OrderItem").iterator();
+        while (itemIter.hasNext()) {
+            result += getOrderItemTotal((GenericValue) itemIter.next());
+        }
+        return result;
+    }
+   
+    
+    public double getOrderItemTotal(GenericValue orderItem) {
+        double result = orderItem.getDouble("unitPrice").doubleValue() * orderItem.getDouble("quantity").doubleValue();
+        Debug.log("getOrderItemTotal: itemTotal=" + result);
+        return result;
+        //FIXME should include adjustments as well
+    }
+    
+    /** Iterator of OrderReadHelper.Adjustment */
+    public Iterator getAdjustmentIterator() {
+        return new AdjustmentIterator(orderHeader.getRelated("OrderAdjustment"), getOrderItemsTotal());
+    }
+    
+    private class AdjustmentIterator implements Iterator {
+        private Iterator orderAdjustmentIter;
+        private double basePrice;
+        
+        private AdjustmentIterator(Collection orderAdjustments, double basePrice) {
+            this.basePrice = basePrice;
+            this.orderAdjustmentIter = orderAdjustments.iterator();
+        }
+        
+        public boolean hasNext() {
+            return orderAdjustmentIter.hasNext();
+        }
+        
+        public Object next() {
+            GenericValue orderAdjustment = (GenericValue) orderAdjustmentIter.next();
+            return new Adjustment(orderAdjustment, basePrice);
+        }
+        
+        public void remove() { throw new UnsupportedOperationException(); }
+    }
+    
+    public class Adjustment {
+        private GenericValue orderAdjustment;
+        private String description;
+        private double amount;
+        
+        private Adjustment(GenericValue orderAdjustment, double basePrice) {
+            GenericValue orderAdjustmentType = orderAdjustment.getRelatedOne("OrderAdjustmentType");
+            if (orderAdjustmentType != null) {
+                this.description = orderAdjustmentType.getString("description");
+                if ("SHIPPING_CHARGES".equals(orderAdjustmentType.getString("orderAdjustmentTypeId"))) {
+                    //put the shipping method in the adjustment description
+                    this.description = UtilFormatOut.ifNotEmpty(getShippingMethod(), "", " ") + this.description;
+                }//else do nothing
+            } else {
+                //if not linked to an adjustment type, leave the description generic
+                this.description = "Adjustment"; 
+            }
+                        
+            Double adjustmentAmount = orderAdjustment.getDouble("amount");
+            Double adjustmentPercentage = orderAdjustment.getDouble("percentage");
+            if ((adjustmentAmount != null) != (adjustmentPercentage != null)) {
+                if (adjustmentAmount != null) {
+                    this.amount = adjustmentAmount.doubleValue();
+                } else {
+                    this.amount = adjustmentPercentage.doubleValue() * basePrice;
+                    this.description = UtilFormatOut.formatPercentage(adjustmentPercentage) 
+                            + " " + this.description;
+                }
+            } else {
+                throw new IllegalArgumentException(
+                        "Either amount or percentage must be specified for adjustment: " + orderAdjustment);
+            }
+        }
+        
+        /** include the percentage amount, if applicable */
+        public String getDescription() {
+            return this.description;
+        }
+        
+        /** uses either the amount or percentage */
+        public double getAmount() {
+            return this.amount;
+        }
+    }
+}
---
+++ commonapp/src/org/ofbiz/commonapp/party/contact/ContactHelper.java
@@ -0,0 +1,54 @@
+/*
+ * $Id$
+ * $Log$
+ */
+package org.ofbiz.commonapp.party.contact;
+
+import java.util.*;
+
+import org.ofbiz.core.entity.*;
+import org.ofbiz.core.util.*;
+
+/**
+ * <p><b>Title:</b> ContactHelper.java
+ * <p><b>Description:</b> Accessors for Contact Mechanisms.
+ * <p>Copyright (c) 2001 The Open For Business Project (www.ofbiz.org) and repected authors.
+ * <p>Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ * <p>The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ * <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ *  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ *  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *@author <a href="mailto:epabst@bigfoot.com">Eric Pabst</a>
+ *@version 1.0
+ *@created Sep 12, 2001
+ */
+public class ContactHelper {  
+    public static Collection getContactMech(GenericValue party, String contactMechTypeId, boolean includeOld) {
+        Collection result = new LinkedList();
+        Date now = new java.util.Date();
+        Iterator partyContactMechIter = party.getRelated("PartyContactMech").iterator();
+        while (partyContactMechIter.hasNext()) {
+            GenericValue partyContactMech = (GenericValue) partyContactMechIter.next();
+            if (includeOld || partyContactMech.get("thruDate") == null || partyContactMech.getTimestamp("thruDate").after(now)) {
+                GenericValue contactMech = partyContactMech.getRelatedOne("ContactMech");
+                if (contactMechTypeId == null || contactMechTypeId.equals(contactMech.get("contactMechTypeId"))) {
+                    result.add(contactMech);
+                }//else wrong type
+            }//else old and includeOld is false
+        }
+        return result;
+    }
+}
