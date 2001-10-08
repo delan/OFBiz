698a00007832ae6761a55a4c00b53005717f6fe5
azeneski <>
2001-10-08 17:23:04 +0000

    Added license.

--- ecommerce/src/org/ofbiz/ecommerce/checkout/ShippingEvents.java
+++ ecommerce/src/org/ofbiz/ecommerce/checkout/ShippingEvents.java
@@ -1,3 +1,7 @@
+/** 
+ * $Id$
+ */
+
 package org.ofbiz.ecommerce.checkout;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +15,32 @@ import java.util.ArrayList;
 import org.ofbiz.ecommerce.shoppingcart.*;
 import org.ofbiz.core.util.*;
 
+/**
+ * <p><b>Title:</b> ShippingEvents.java
+ * <p><b>Description:</b> Events used for processing shipping fees..
+ * <p>Copyright (c) 2001 The Open For Business Project and repected authors.
+ * <p>Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ * <p>The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ * <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ *  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ *  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @author Andy Zeneski (jaz@zsolv.com)
+ * @version 1.0
+ * Created on October 8, 2001, 1:17 PM
+ */
 public class ShippingEvents {
     
     public static final String UPS_RATES_URL = "http://www.ups.com/using/services/rave/qcostcgi.cgi";
