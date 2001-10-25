e8b309eda6849b8b20e305c6008a89242a7110d8
jonesde <>
2001-10-25 16:39:35 +0000

    Added echo of module names

--- setup/ofbiz/updateall.sh
+++ setup/ofbiz/updateall.sh
@@ -1,20 +1,29 @@
 cd core
+echo "--- core ---"
 cvs update
 cd ../commonapp
+echo "--- commonapp ---"
 cvs update
 cd ../ecommerce
+echo "--- ecommerce ---"
 cvs update
 cd ../catalog
+echo "--- catalog ---"
 cvs update
 cd ../ordermgr
+echo "--- ordermgr ---"
 cvs update
 cd ../partymgr
+echo "--- partymgr ---"
 cvs update
 cd ../wfengine
+echo "--- wfengine ---"
 cvs update
 cd ../setup
+echo "--- setup ---"
 cvs update
 cd ../website
+echo "--- website ---"
 cvs update
 cd ..
 
