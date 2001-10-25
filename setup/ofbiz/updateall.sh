535b9a69ff0d978c772fe5ad33ed92c20fb2306e
jonesde <>
2001-10-25 16:10:47 +0000

    Simple cvs update all mods scripts

---
+++ setup/ofbiz/updateall.sh
@@ -0,0 +1,20 @@
+cd core
+cvs update
+cd ../commonapp
+cvs update
+cd ../ecommerce
+cvs update
+cd ../catalog
+cvs update
+cd ../ordermgr
+cvs update
+cd ../partymgr
+cvs update
+cd ../wfengine
+cvs update
+cd ../setup
+cvs update
+cd ../website
+cvs update
+cd ..
+
