239e104fe3f0271add850cb691b722deb3888285
jonesde <>
2001-11-07 01:35:23 +0000

    More mistake removal

--- workmgr/setup/jboss/bin/ofbiz.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set JBOSS_CLASSPATH=%JBOSS_CLASSPATH%;%JAVA_HOME%/lib/tools.jar
-.\run.bat ofbiz
-    
--- workmgr/setup/jboss/bin/ofbiz.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export JBOSS_CLASSPATH=$JBOSS_CLASSPATH:$JAVA_HOME/lib/tools.jar
-/bin/sh ./run.sh ofbiz
-    
