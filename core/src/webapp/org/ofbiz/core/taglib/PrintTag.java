1785bdd26c389269d72bc4762cf03ea25adb0d4c
jonesde <>
2001-11-07 23:31:19 +0000

    Added very simple print tag, basicly prints an attribute from the pageContext unless it is null

---
+++ core/src/webapp/org/ofbiz/core/taglib/PrintTag.java
@@ -0,0 +1,68 @@
+package org.ofbiz.core.taglib;
+
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import org.ofbiz.core.util.Debug;
+
+/**
+ * <p><b>Title:</b> PrintTag.java
+ * <p><b>Description:</b> Prints an attribute from the pageContext
+ * <p>Copyright (c) 2001 The Open For Business Project and repected authors.
+ * <p>Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ * <p>The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ * <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ *  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ *  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @author <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
+ * @version 1.0
+ * Created on November 7, 2001
+ */
+public class PrintTag extends TagSupport {
+  private String attribute = null;
+  
+  public String getAttribute() {
+    return attribute;
+  }
+  
+  public void setAttribute(String attribute) {
+    this.attribute = attribute;
+  }
+  
+  public int doStartTag() throws JspTagException {
+    if(attribute == null) return SKIP_BODY;
+    Object obj = pageContext.getAttribute(attribute);
+    if(obj == null) return SKIP_BODY;
+    
+    try {
+      JspWriter out = pageContext.getOut();
+      out.print(obj.toString());
+    }
+    catch ( IOException e ) {
+      throw new JspTagException(e.getMessage());
+    }
+    
+    return SKIP_BODY;
+  }
+}
