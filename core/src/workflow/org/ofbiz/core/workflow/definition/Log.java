b8ce8d6f7d0073340fa3e9c4582a3d5cbb3b42fe
rbb36 <>
2001-11-11 00:15:42 +0000

    Added log4j, began implementation

---
+++ core/src/workflow/org/ofbiz/core/workflow/definition/Log.java
@@ -0,0 +1,230 @@
+package org.ofbiz.core.workflow.definition;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.File;
+import java.io.PrintStream;
+import java.io.PipedOutputStream;
+import java.util.Enumeration;
+import java.util.Properties;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Category;
+import org.apache.log4j.Priority;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.spi.LoggingEvent;
+
+/**
+ */
+public abstract class Log {
+    
+    /**
+     * <b>Edit this in the source code for easiest use by a large team.</b>
+     * For example, you could set this to
+     * "http://projectserver.company.com/etc/log4j.client.properties",
+     * and post a default configuration file on your team's web server.
+     * Then, all your team members would have to do to insure log
+     * initialization would be to call, "Log.init()". 
+     */
+    public static final String DEFAULT_CONFIG_URL = null;
+    
+    // -------------------------------------------------------------
+    // CONSTANTS
+    // -------------------------------------------------------------
+    
+    public static final String ROOT_CATEGORY_NAME = "log4j.rootCategory";
+    
+    public static final String[] DEFAULT_CONFIG_DIRECTORIES = {
+        ".", "etc", "conf",
+    };
+    public static final String[] DEFAULT_DEPTHS = {
+        ".", "..", "../..",
+    };
+    public static final
+        String DEFAULT_CONFIG_FILE_NAME = "log4j.properties";
+    
+    public static final String LEVEL_DEBUG = "Debug";
+    public static final String LEVEL_INFO = "Info";
+    public static final String LEVEL_WARN = "Warn";
+    public static final String LEVEL_ERROR = "Error";
+    public static final String LEVEL_FATAL = "Fatal";
+    
+    /** Indicates all log messages should be sent to the bit bucket. */
+    public static final int FALLBACK_CONFIG_NULL = 0;
+    /** Indicates all log messages should be sent to the command line. */
+    public static final int FALLBACK_CONFIG_CLI = 1;
+    
+    /** <b>Edit this in source code for easiest use by a large team.</b> */
+    public static final int DEFAULT_FALLBACK_CONFIG = FALLBACK_CONFIG_CLI;
+    
+    // -------------------------------------------------------------
+    // STATIC PARAMETERS AND ACCESSORS
+    // -------------------------------------------------------------
+    
+    // PARAMETERS
+    private static boolean _initialized = false;
+    private static URL _configUrl = null;
+    private static int _fallbackConfig = DEFAULT_FALLBACK_CONFIG;
+    
+    // GETTERS
+    public static boolean isInitialized() { return( getInitialized() ); }
+    public static boolean getInitialized() { return( _initialized ); }
+    public static URL getConfigUrl() { return( _configUrl ); }
+    public static int getFallbackConfig() { return( _fallbackConfig ); }
+    
+    // SETTERS
+    protected static void setInitialized( boolean initialized ) {
+        _initialized = initialized;
+    }
+    /**
+     * IMHO, you should alter DEFAULT_CONFIG_URL in the source of
+     * this file and <b>never call this method</b>, except when doing
+     * a reinitialization at runtime to debug a flakey process.
+     */
+    public static void setConfigUrl( URL url ) {
+        _configUrl = url;
+    }
+    /**
+     * IMHO, you should alter DEFAULT_CONFIG_URL in the source of
+     * this file and <b>never call this method</b>, except when doing
+     * a reinitialization at runtime to debug a flakey process.
+     */
+    public static void setFallbackConfig( int fallback ) {
+        _fallbackConfig = fallback;
+    } 
+    
+    // --------------------------------------------------------
+    // CONSTRUCTORS
+    // --------------------------------------------------------
+    
+    private Log() {}
+    
+    // --------------------------------------------------------
+    // PUBLIC API
+    // --------------------------------------------------------
+    
+    /**
+     * <b>If not initialized</b>, initializes from the set
+     * configuration URL.
+     */ 
+    public static void init() {
+        if( getInitialized() ) { return; }
+        synchronized( Log.class ) {
+            // Check again.
+            if( getInitialized() ) { return; }
+            forceInit();
+        }
+    }
+    
+    /** Forces initialization from the set configuration URL. */
+    public synchronized static void forceInit() {
+        reinitialize( getBestConfigUrl() );
+    }
+    
+    /**
+     * <b>Call reinitialize() when you're done.</b>
+     * Forces reinitialization from the given URL, but
+     * <b>does not alter</b> the set configuration URL.
+     * Thus, calling
+     * <pre><tt>
+     *   Log.reinitialize( url );
+     *   ... do suspect stuff ...
+     *   Log.reinitialize();
+     * </tt></pre>
+     * will use temporary logging for the suspect section.
+     */
+    public synchronized static void reinitialize( URL url ) {
+        setInitialized( false );
+        final PrintStream err = System.err;
+        try { System.setErr( new PrintStream( new PipedOutputStream() ) ); }
+        catch( Exception e ) {}
+        
+        try {
+            // Initialize
+            if( url != null ) {
+                try {
+                    PropertyConfigurator.resetConfiguration();
+                    PropertyConfigurator.configure( url );
+                    setInitialized( true );
+                } catch( Exception e ) {
+                    fallbackInit();
+                }
+            } else {
+                fallbackInit();
+            }
+        } finally {
+            // Restore System.err
+            try { System.setErr( err ); }
+            catch( Exception e ) {}
+        }
+    }
+    
+    // ---------------------------------------------------------
+    // INTERNAL API
+    // ---------------------------------------------------------
+    
+    protected static URL getBestConfigUrl() {
+        final String cliUrlString =
+            System.getProperty( "log4j.configuration" );
+        final URL setUrl = getConfigUrl();
+        
+        if( cliUrlString != null ) {
+            try {
+                final URL url = new URL( cliUrlString );
+                url.openConnection();
+                return( url );
+            } catch( Exception e ) {}
+        }
+        if( setUrl != null ) {
+            try {
+                setUrl.openConnection();
+                return( setUrl );
+            } catch( Exception e ) {}
+        }
+        for( int j = 0; j < DEFAULT_DEPTHS.length; j++ ) {
+            final String depth = DEFAULT_DEPTHS[j];
+            for( int i = 0; i < DEFAULT_CONFIG_DIRECTORIES.length; i++ ) {
+                final String dir = DEFAULT_CONFIG_DIRECTORIES[i];
+                final String fileName = 
+                    depth + "/" + dir + "/" + DEFAULT_CONFIG_FILE_NAME;
+                try {
+                    final File file = new File( fileName );
+                    if( file.isFile() ) { return( file.toURL() ); }
+                } catch( Exception e ) {}
+            }
+        }
+        
+        return( null );
+    }
+    
+    protected static void fallbackInit() {
+        if( getFallbackConfig() == FALLBACK_CONFIG_NULL ) {
+            nullInit();
+        } else {
+            cliInit();
+        }
+    }
+    
+    protected static void cliInit() {
+        BasicConfigurator.resetConfiguration();
+        BasicConfigurator.configure();
+        setInitialized( true );
+    }
+    
+    protected static void nullInit() {
+        BasicConfigurator.resetConfiguration();
+        final Appender nullAppender = new AppenderSkeleton() {
+                public void append( LoggingEvent event ) {}
+                public void doAppend( LoggingEvent event ) {}
+                public void close() {}
+                public boolean requiresLayout() { return( false ); }
+            };
+        nullAppender.setName( "NULL_APPENDER" );
+        Category.getRoot().removeAllAppenders();
+        Category.getRoot().setPriority( Priority.FATAL );
+        Category.getRoot().addAppender( nullAppender );
+        setInitialized( true );
+    }
+}
--- core/src/workflow/org/ofbiz/core/workflow/definition/XpdlParser.java
+++ core/src/workflow/org/ofbiz/core/workflow/definition/XpdlParser.java
@@ -3,6 +3,9 @@
 
 /*
  * $Log$
+ * Revision 1.2  2001/11/09 03:27:42  rbb36
+ * This works if run from core/src/workflow
+ *
  * Revision 1.1  2001/11/04 13:14:44  rbb36
  * Initial Blank Checkin
  *
@@ -10,17 +13,19 @@
 
 package org.ofbiz.core.workflow.definition;
 
+// SUN IMPORTS
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.*;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.FactoryConfigurationError;
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
+// FOREIGN IMPORTS
+import org.apache.log4j.Category;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.DOMException;
@@ -28,6 +33,10 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+// OPEN FOR BUSINESS IMPORTS
 import org.ofbiz.core.util.*;
 
 /** 
@@ -58,13 +67,155 @@ import org.ofbiz.core.util.*;
  */
 public class XpdlParser {
     
-    public static void main( String[] args ) throws Exception {
-        Document document = null;
-        DocumentBuilderFactory factory =
+    // -------------------------------------------------------
+    // LOG INITIALIZATION
+    // -------------------------------------------------------
+    
+    static{ Log.init(); }
+    private static final Category cat =
+        Category.getInstance( XpdlParser.class.getName() );
+    
+    // -------------------------------------------------------
+    // CONSTANTS
+    // -------------------------------------------------------
+    
+    public static final
+        String ELEM_NAME_PACKAGE = "Package";
+    public static final
+        String ELEM_NAME_PACKAGE_HEADER = "PackageHeader";
+    public static final
+        String ELEM_NAME_REDEFINABLE_HEADER = "RedefinableHeader";
+    public static final
+        String ELEM_NAME_CONFORMANCE_CLASS = "ConformanceClass";
+    public static final
+        String ELEM_NAME_EXTERNAL_PACKAGES = "ExternalPackages";
+    public static final
+        String ELEM_NAME_TYPE_DECLARATIONS = "TypeDeclarations";
+    public static final
+        String ELEM_NAME_PARTICIPANTS = "Participants";
+    public static final
+        String ELEM_NAME_APPLICATIONS = "Applications";
+    public static final
+        String ELEM_NAME_DATA_FIELDS = "DataFields";
+    public static final
+        String ELEM_NAME_WORKFLOW_PROCESSES = "WorkflowProcesses";
+    public static final
+        String ELEM_NAME_EXTENDED_ATTRIBUTES = "ExtendedAttributes";
+    
+    public static final String[] LEGAL_ELEM_NAMES = {
+        ELEM_NAME_PACKAGE,
+        ELEM_NAME_PACKAGE_HEADER,
+        ELEM_NAME_REDEFINABLE_HEADER,
+        ELEM_NAME_CONFORMANCE_CLASS,
+        ELEM_NAME_EXTERNAL_PACKAGES,
+        ELEM_NAME_TYPE_DECLARATIONS,
+        ELEM_NAME_PARTICIPANTS,
+        ELEM_NAME_APPLICATIONS,
+        ELEM_NAME_DATA_FIELDS,
+        ELEM_NAME_WORKFLOW_PROCESSES,
+        ELEM_NAME_EXTENDED_ATTRIBUTES,
+    };
+    
+    // ---------------------------------------------------------
+    // PUBLIC API
+    // ---------------------------------------------------------
+    
+    public static Object parse( String fileName ) {
+        final File file = new File( fileName );
+        final DocumentBuilderFactory factory =
             DocumentBuilderFactory.newInstance();
+        
+        DocumentBuilder builder;
+        Document document;
+        Element docElement;
+        Object pakkage = null;
+        
         factory.setValidating( true );
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        File sampleFile = new File( "../../docs/examples/sample.xpdl" );
-        document = builder.parse( sampleFile );
+        try {
+            builder = factory.newDocumentBuilder();
+            document = builder.parse( file );
+            docElement = document.getDocumentElement();
+            pakkage = (Object)parseElement( ELEM_NAME_PACKAGE, docElement );
+        } catch( ParserConfigurationException e ) {
+            cat.fatal( e.getMessage(), e );
+        } catch( SAXException e ) {
+            cat.error( e.getMessage(), e );
+        } catch( IOException e ) {
+            cat.error( e.getMessage(), e );
+        }
+        
+        return( pakkage );
+    }
+    
+    // ---------------------------------------------------------
+    // RUNTIME, TEST, AND SAMPLE METHODS
+    // ---------------------------------------------------------
+    
+    public static void main( String[] args ) throws Exception {
+        final String sampleFileName = "../../docs/examples/sample.xpdl";
+        final Object o = parse( sampleFileName );
+    }
+    
+    // -------------------------------------------------------
+    // INTERNAL API
+    // -------------------------------------------------------
+    
+    protected static Method getParseMethod( String elementName )
+        throws NoSuchMethodException {
+        final String methodName = "parse" + elementName;
+        final Class[] argClasses = new Class[]{ Element.class };
+        return( XpdlParser.class.getMethod( methodName, argClasses ) );
+    }
+    
+    // -------------------------------------------------------
+    // FACTORIES
+    // -------------------------------------------------------
+    
+    /**
+     * Dynamically invokes the parse method for the given
+     * element type, returns an object of the appropriate
+     * runtime type. Returns null if any exception occurs,
+     * and writes the exception to Log4j.
+     */
+    public static Object parseElement( String elementName,
+                                       Element element ) {
+        try {
+            final Object[] args = new Object[]{ element };
+            final Method method = getParseMethod( elementName );
+            return( method.invoke( null, args ) );
+        } catch( NoSuchMethodException e ) {
+            cat.error
+                ( "Attempt to Parse Unknown Element: " + elementName, e );
+            return( null );
+        } catch( Exception e ) {
+            cat.error
+                ( "Unanticipated Exception Parsing: " + elementName, e );
+            return( null );
+        }
+    }
+    
+    /**
+     * Parses an XPDL Package element into an instance of a
+     * Package object.
+     */
+    public static Object parsePackage( Element packageElement ) {
+        cat.fatal( "parsePackage( Element ) has not been implemented",
+                   new Exception() );
+        return( null );
+    }
+    
+    /** Parses an XPDL Join element into an instance of a Join object. */
+    public static Join parseJoin( Element joinElement ) {
+        final String ATTR_KEY_TYPE = "Type";
+        final String ATTR_VALUE_XOR = "XOR";
+        final String ATTR_VALUE_AND = "AND";
+        
+        final String typeAttr = joinElement.getAttribute( ATTR_KEY_TYPE );
+        int type = Join.AND;
+        
+        if( typeAttr.equals( ATTR_VALUE_XOR ) ) { type = Join.XOR; }
+        
+        return( new Join( type ) );
     }
 }
+
