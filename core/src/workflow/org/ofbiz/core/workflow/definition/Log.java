3e3bc457657514819789ac59af0074028a4570a1
rbb36 <>
2001-11-11 02:27:12 +0000

    Added CVS headers and copyright notice

--- core/src/workflow/org/ofbiz/core/workflow/definition/Log.java
+++ core/src/workflow/org/ofbiz/core/workflow/definition/Log.java
@@ -1,3 +1,10 @@
+/* $Id$ */
+/* $Source$ */
+
+/*
+ * $Log$
+ */
+
 package org.ofbiz.core.workflow.definition;
 
 import java.net.MalformedURLException;
@@ -16,7 +23,31 @@ import org.apache.log4j.Priority;
 import org.apache.log4j.PropertyConfigurator;
 import org.apache.log4j.spi.LoggingEvent;
 
-/**
+/** 
+ * <p>Log - Initializes Log4j with standard settings.
+ * <p>Copyright (c) 2001 The Open For Business Project - www.ofbiz.org
+ *
+ * <p>Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * <p>The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @author <a href='mailto:ofb@traxel.com'>Robert Bushman</a>
+ * @created Sat Nov 10 19:22:42 MST 2001
+ * @version 0.0
  */
 public abstract class Log {
     
