7a9fbde502ff7ab03ca217a84b4eb6eb904e68a2
azeneski <>
2001-12-03 20:29:46 +0000

    Update ModelService to validate types, new utility for Object typing

--- core/src/share/org/ofbiz/core/service/ModelService.java
+++ core/src/share/org/ofbiz/core/service/ModelService.java
@@ -5,6 +5,7 @@
 package org.ofbiz.core.service;
 
 import java.util.*;
+import java.lang.reflect.*;
 import org.ofbiz.core.util.*;
 
 /**
@@ -39,26 +40,65 @@ public class ModelService {
     
   /** The name of this service */
   public String name;
+  /** The name of the engine from engine.properties */
   public String engineName;
+  /** The package name or location of this service */  
   public String location;
+  /** The method or function to invoke for this service */
   public String invoke;
-
   /** Context Information, a list of parameters required by the service */
-  public Map contextInfo;
-  
+  public Map contextInfo;  
   /** Result Information, a list of values returned by the service */
   public Map resultInfo;
   
+  /** Validates a map of name, object types to a map of name, objects
+   * @param info The map of name, object types
+   * @param test The map to test its value types.
+   * @returns true if validation is successful
+   */
   public static boolean validate(Map info, Map test) {
-      // validate keys first
+      // * Validate keys first
       Set testSet = test.keySet();
       Set keySet = info.keySet();
+      
+      // This test is to see if the keys of the test map match exactly
       if ( !testSet.containsAll(keySet) || !keySet.containsAll(testSet) )
           return false;
       
-      // validate types next
-      String DEFAULT_PACKAGE = "java.lang.";
-      
+      // * Validate types next   
+      // Warning - the class types MUST be accessible to this classloader            
+      String DEFAULT_PACKAGE = "java.lang."; // We will test both the raw value and this + raw value
+      Iterator i = keySet.iterator();
+      while ( i.hasNext() ) {
+          Object key = i.next();
+          Object testObject = test.get(key);
+          String infoType = (String) info.get(key);   
+          Class infoClass = null;
+          try {
+              infoClass = ObjectType.loadClass(infoType);
+          }
+          catch ( SecurityException se ) {
+              throw new RuntimeException("Problems with classloader");
+          }
+          catch ( ClassNotFoundException cnf ) {
+              try {
+                  infoClass = ObjectType.loadClass(DEFAULT_PACKAGE + infoType);
+              }
+              catch ( SecurityException se2 ) {
+                  throw new RuntimeException("Problems with classloader");
+              }
+              catch ( ClassNotFoundException e ) {
+                  throw new RuntimeException("Cannot load the type class of: " + infoType);
+              }
+          }
+          
+          if ( infoClass == null )
+              throw new RuntimeException("Illegal type found in info map");
+          
+          if ( !ObjectType.instanceOf(testObject,infoClass) )
+              return false;
+      }
+                
       return true;
   }
 }
---
+++ core/src/share/org/ofbiz/core/util/ObjectType.java
@@ -0,0 +1,156 @@
+/**
+ * $Id$
+ */
+
+package org.ofbiz.core.util;
+
+/**
+ * <p><b>Title:</b> ObjectType
+ * <p><b>Description:</b> None
+ * <p>Copyright (c) 2001 The Open For Business Project - www.ofbiz.org
+ *
+ * <p>Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ * <p>The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ * <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ *  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ *  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *@author     <a href="mailto:jaz@zsolv.com">Andy Zeneski</a>
+ *@created    December 3, 2001
+ *@version    1.0
+ */
+public class ObjectType {
+    
+    /** Loads a class with the current thread's context classloader 
+     * @param className The name of the class to load
+     */
+    public static Class loadClass(String className) throws SecurityException, ClassNotFoundException {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        Class c = loader.loadClass(className);
+        return c;
+    }
+    
+    /** Returns an instance of the specified class
+     * @param className Name of the class to instantiate
+     */
+    public static Object getInstance(String className) throws SecurityException, ClassNotFoundException,
+    InstantiationException, IllegalAccessException {
+        Class c = loadClass(className);
+        Object o = c.newInstance();
+        return o;
+    }
+    
+    /** Tests if an object properly implements the specified interface
+     * @param obj Object to test
+     * @param interfaceName Name of the interface to test against
+     */
+    public static boolean interfaceOf(Object obj, String interfaceName) throws SecurityException, ClassNotFoundException {
+        Class interfaceClass = loadClass(interfaceName);
+        return interfaceOf(obj,interfaceClass);
+    }
+
+        /** Tests if an object properly implements the specified interface
+     * @param obj Object to test
+     * @param interfaceObject to test against
+     */
+    public static boolean interfaceOf(Object obj, Object interfaceObject) {
+        Class interfaceClass = interfaceObject.getClass();
+        return interfaceOf(obj,interfaceClass);
+    }
+    
+    /** Tests if an object properly implements the specified interface
+     * @param obj Object to test
+     * @param interfaceClass Class to test against
+     */   
+    public static boolean interfaceOf(Object obj, Class interfaceClass) {
+        Class objectClass = obj.getClass();
+        while ( objectClass != null ) {
+            Class[] ifaces = objectClass.getInterfaces();
+            for ( int i = 0; i < ifaces.length; i++ ) {
+                if ( ifaces[i] == interfaceClass ) return true;
+            }
+            objectClass = objectClass.getSuperclass();
+        }
+        return false;
+    }
+
+    /** Tests if an object is an instance of or a sub-class of the parent
+     * @param obj Object to test
+     * @param parentName Name of the parent class to test against
+     */    
+    public static boolean isOrSubOf(Object obj, String parentName) throws SecurityException, ClassNotFoundException {
+        Class parentClass = loadClass(parentName);
+        return isOrSubOf(obj,parentClass);
+    }
+
+    /** Tests if an object is an instance of or a sub-class of the parent
+     * @param obj Object to test
+     * @param parentObject Object to test against
+     */       
+    public static boolean isOrSubOf(Object obj, Object parentObject) {
+        Class parentClass = parentObject.getClass();
+        return isOrSubOf(obj,parentClass);
+    }
+
+    /** Tests if an object is an instance of or a sub-class of the parent
+     * @param obj Object to test
+     * @param parentClass Class to test against
+     */        
+    public static boolean isOrSubOf(Object obj, Class parentClass) {
+        Class objectClass = obj.getClass();
+        while ( objectClass != null ) {
+            if ( objectClass == parentClass ) return true;
+            objectClass = objectClass.getSuperclass();
+        }
+        return false;
+    }
+        
+    /** Tests if an object is an instance of a sub-class of or properly implements an interface
+     * @param obj Object to test
+     * @param typeName Name of the class to test against
+     */
+    public static boolean instanceOf(Object obj, String typeName) throws SecurityException, ClassNotFoundException {
+        Class typeClass = loadClass(typeName);
+        return instanceOf(obj,typeClass);
+    }
+
+    /** Tests if an object is an instance of a sub-class of or properly implements an interface
+     * @param obj Object to test
+     * @param typeObject Object to test against
+     */    
+    public static boolean instanceOf(Object obj, Object typeObject) {
+        Class typeClass = typeObject.getClass();
+        return instanceOf(obj,typeClass);
+    }
+
+    /** Tests if an object is an instance of a sub-class of or properly implements an interface
+     * @param obj Object to test
+     * @param typeClass Class to test against
+     */    
+    public static boolean instanceOf(Object obj, Class typeClass) {
+        Class objectClass = obj.getClass();
+        if ( typeClass.isInterface() )
+            return interfaceOf(obj,typeClass);
+        else
+            return isOrSubOf(obj,typeClass);
+    }
+    
+    
+}
+
+
+
+
+
