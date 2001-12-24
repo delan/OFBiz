fb3de3d2c521bd2492b1e11596b5fe9fe24e91fb
jonesde <>
2001-12-24 23:21:18 +0000

    Shell file to setup ofbiz.home Java define from OFBIZ_HOME env var, or derived from CATALINA_HOME

---
+++ setup/catalina/bin/ofbiz.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+# -----------------------------------------------------------------------------
+# ofbiz.sh - OFBIZ Wrapper for the Start/Stop Script for the CATALINA Server
+#
+# Environment Variable Prequisites
+#
+#   OFBIZ_HOME    (Optional) May point at your Catalina "build" directory.
+#                 If not present, $CATALINA_HOME/../ofbiz is assumed, in
+#                 other words #CATALINA_HOME and the top 'ofbiz' directory
+#                 are sibling directories.
+#
+#   CATALINA_HOME (Optional) May point at your Catalina "build" directory.
+#                 If not present, the parent directory of the current working 
+#                 directory is assumed.
+#
+#   JAVA_HOME     Must point at your Java Development Kit installation.
+#
+# $Id$
+# -----------------------------------------------------------------------------
+
+# Do this for Linux/Java conflicts
+ulimit -s 2048
+
+# ----- Verify and Set Required Environment Variables -------------------------
+
+if [ -z "$CATALINA_HOME" ] ; then
+  export CATALINA_HOME=".."
+fi
+
+if [ -z "$OFBIZ_HOME" ] ; then
+  export OFBIZ_HOME="$CATALINA_HOME/../ofbiz"
+fi
+
+if [ -z "$JAVA_HOME" ] ; then
+  echo You must set JAVA_HOME to point at your Java Development Kit installation
+  exit 1
+fi
+
+# ----- Set CATALINA_OPTS and Start Catalina -----------------------------------------
+
+export CATALINA_OPTS="$CATALINA_OPTS -Dofbiz.home=$OFBIZ_HOME"
+
+echo "ofbiz.sh - Running Catalina with the following options:"
+echo " JAVA_HOME=$JAVA_HOME"
+echo " CATALINA_HOME=$CATALINA_HOME"
+echo " OFBIZ_HOME=$OFBIZ_HOME"
+echo " CATALINA_OPTS=$CATALINA_OPTS"
+echo " -- RUNNING $CATALINA_HOME/bin/catalina.sh $1 --"
+
+$CATALINA_HOME/bin/catalina.sh $1
