04a23e1a2397bbb732ae57ccbff94f3077d09e06
azeneski <>
2002-02-07 22:09:22 +0000

    Small formatting change

--- core/src/share/org/ofbiz/core/entity/model/ModelViewEntity.java
+++ core/src/share/org/ofbiz/core/entity/model/ModelViewEntity.java
@@ -1,25 +1,25 @@
 /*
  * $Id$
  *
- * <p>Copyright (c) 2001 The Open For Business Project - www.ofbiz.org
+ * Copyright (c) 2001 The Open For Business Project - www.ofbiz.org
  *
- * <p>Permission is hereby granted, free of charge, to any person obtaining a
- *  copy of this software and associated documentation files (the "Software"),
- *  to deal in the Software without restriction, including without limitation
- *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
- *  and/or sell copies of the Software, and to permit persons to whom the
- *  Software is furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
  *
- * <p>The above copyright notice and this permission notice shall be included
- *  in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
  *
- * <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
- *  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
- *  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package org.ofbiz.core.entity.model;
@@ -28,9 +28,9 @@ import java.util.*;
 import org.ofbiz.core.util.*;
 
 /**
- * This class extends ModelEntity and provides adition information appropriate to view entities
+ * This class extends ModelEntity and provides additional information appropriate to view entities
  *
- *@author <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
+ *@author     <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
  *@created    November 9, 2001
  *@version    1.0
  */
@@ -69,7 +69,8 @@ public class ModelViewEntity extends ModelEntity {
             String aliasedEntityName = (String) entry.getValue();
             ModelEntity aliasedEntity = (ModelEntity) entityCache.get(aliasedEntityName);
             if (aliasedEntity == null) {
-                Debug.logError("[ModelViewEntity.populateFields] ERROR: could not find ModelEntity for entity name: " + aliasedEntityName);
+                Debug.logError("[ModelViewEntity.populateFields] ERROR: could not find ModelEntity for entity name: " +
+                               aliasedEntityName);
                 continue;
             }
             memberModelEntities.put(entry.getKey(), aliasedEntity);
@@ -81,7 +82,8 @@ public class ModelViewEntity extends ModelEntity {
             String aliasedEntityName = (String) memberEntityNames.get(alias.entityAlias);
             ModelEntity aliasedEntity = (ModelEntity) entityCache.get(aliasedEntityName);
             if (aliasedEntity == null) {
-                Debug.logError("[ModelViewEntity.populateFields] ERROR: could not find ModelEntity for entity name: " + aliasedEntityName);
+                Debug.logError("[ModelViewEntity.populateFields] ERROR: could not find ModelEntity for entity name: " +
+                               aliasedEntityName);
                 continue;
             }
 
@@ -97,7 +99,8 @@ public class ModelViewEntity extends ModelEntity {
 
             ModelField aliasedField = aliasedEntity.getField(alias.field);
             if (aliasedField == null) {
-                Debug.logError("[ModelViewEntity.populateFields] ERROR: could not find ModelField for field name \"" + alias.field + "\" on entity with name: " + aliasedEntityName);
+                Debug.logError("[ModelViewEntity.populateFields] ERROR: could not find ModelField for field name \"" +
+                               alias.field + "\" on entity with name: " + aliasedEntityName);
                 continue;
             }
             field.type = aliasedField.type;
