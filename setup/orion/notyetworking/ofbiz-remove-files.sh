f26cda562dde7367a8c15ae38fb4bf1ca80739e9
jonesde <>
2002-02-07 06:04:42 +0000

    A LOT closer to working, but still can't get the UserTransaction from JNDI

---
+++ setup/orion/notyetworking/ofbiz-remove-files.sh
@@ -0,0 +1,27 @@
+
+#!/bin/sh
+
+# remove files from $ORION_HOME directory that came from OFBiz
+
+if [ -z "$ORION_HOME" ] ; then
+  export ORION_HOME="."
+fi
+
+# get all of the ofbiz jars
+rm $ORION_HOME/lib/ofbcore-share.jar $ORION_HOME/lib/ofbcore-workflow.jar $ORION_HOME/lib/ofbcore-rules.jar
+rm $ORION_HOME/lib/commonapp.jar
+
+# get all of the ofbiz config files
+rm $ORION_HOME/lib/cache.properties $ORION_HOME/lib/debug.properties $ORION_HOME/lib/security.properties
+rm $ORION_HOME/lib/servicesengine.properties $ORION_HOME/lib/controlservlet.properties
+rm $ORION_HOME/lib/localdtds.properties 
+#rm $ORION_HOME/lib/tyrexdomain.xml
+
+# get all of the third party jars
+#rm $ORION_HOME/lib/tyrex-0.9.8.5.jar $ORION_HOME/lib/ots-jts_1.0.jar
+rm $ORION_HOME/lib/castor-0.9.3.9.jar $ORION_HOME/lib/hsqldb.jar $ORION_HOME/lib/jdbc7.1-1.3.jar
+rm $ORION_HOME/lib/mm.mysql-2.0.8-bin.jar $ORION_HOME/lib/log4j.jar
+rm $ORION_HOME/lib/axis.jar $ORION_HOME/lib/clutil.jar $ORION_HOME/lib/wsdl4j.jar
+rm $ORION_HOME/lib/bsh-1.2b3.jar $ORION_HOME/lib/jakarta-oro-2.0.4.jar
+rm $ORION_HOME/lib/xerces.jar
+
---
+++ setup/orion/notyetworking/ofbiz.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# -----------------------------------------------------------------------------
+# ofbiz.sh - OFBIZ Wrapper for the Start/Stop Script for the CATALINA Server
+#
+# Environment Variable Prequisites
+#
+#   OFBIZ_HOME    (Optional) May point at your ofbiz directory.
+#                 If not present, $CATALINA_HOME/../ofbiz is assumed, in
+#                 other words #CATALINA_HOME and the top 'ofbiz' directory
+#                 are sibling directories.
+#
+#   JAVA_HOME     Must point at your Java Development Kit installation.
+#
+# $Id$
+# -----------------------------------------------------------------------------
+
+# Do this for Linux/Java conflicts
+ulimit -s 2048
+
+# ----- Verify and Set Required Environment Variables -------------------------
+
+if [ -z "$OFBIZ_HOME" ] ; then
+  export OFBIZ_HOME="../ofbiz"
+fi
+
+if [ -z "$JAVA_HOME" ] ; then
+  echo You must set JAVA_HOME to point at your Java Development Kit installation
+  exit 1
+fi
+
+echo "ofbiz.sh - Running Orion with the following options:"
+echo " JAVA_HOME=$JAVA_HOME"
+echo " OFBIZ_HOME=$OFBIZ_HOME"
+echo
+#echo "CLASSPATH=$CLASSPATH"
+echo
+echo " -- RUNNING $JAVA_HOME/bin/java -Dofbiz.home=$OFBIZ_HOME -jar orion.jar $1 --"
+
+$JAVA_HOME/bin/java -Dofbiz.home=$OFBIZ_HOME -jar orion.jar $1
+
