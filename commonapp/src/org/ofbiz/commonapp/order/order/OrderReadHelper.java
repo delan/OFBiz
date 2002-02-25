a2c638d6f8fc82ddee8ef6bbde28a46a5eda48d3
azeneski <>
2002-02-25 15:05:06 +0000

    added affiliate methods

--- commonapp/src/org/ofbiz/commonapp/order/order/OrderReadHelper.java
+++ commonapp/src/org/ofbiz/commonapp/order/order/OrderReadHelper.java
@@ -1,6 +1,9 @@
 /*
  * $Id$
  * $Log$
+ * Revision 1.12  2001/10/17 06:52:02  jonesde
+ * Finished round of reserved word replacements
+ *
  * Revision 1.11  2001/09/28 21:51:21  jonesde
  * Big update for fromDate PK use, organization stuff
  *
@@ -30,6 +33,7 @@
 package org.ofbiz.commonapp.order.order;
 
 import java.util.*;
+
 import org.ofbiz.core.entity.*;
 import org.ofbiz.core.util.*;
 
@@ -61,187 +65,222 @@ import org.ofbiz.core.util.*;
  *@version    1.0
  */
 public class OrderReadHelper {
-  private GenericValue orderHeader;
-  private Double totalPrice;
-  
-  public OrderReadHelper(GenericValue orderHeader) {
-    this.orderHeader = orderHeader;
-  }
-  
-  public String getShippingMethod() {
-    try {
-      GenericValue shipmentPreference = null;
-      Iterator tempIter = UtilMisc.toIterator(orderHeader.getRelated("OrderShipmentPreference"));
-      if(tempIter != null && tempIter.hasNext()) { shipmentPreference = (GenericValue)tempIter.next(); }
-      if(shipmentPreference != null) {
-        GenericValue carrierShipmentMethod = shipmentPreference.getRelatedOne("CarrierShipmentMethod");
-        if(carrierShipmentMethod != null) {
-          GenericValue shipmentMethodType = carrierShipmentMethod.getRelatedOne("ShipmentMethodType");
-          if(shipmentMethodType != null) {
-            return UtilFormatOut.checkNull(shipmentPreference.getString("carrierPartyId")) + " " + UtilFormatOut.checkNull(shipmentMethodType.getString("description"));
-          }
-        }
-        return UtilFormatOut.checkNull(shipmentPreference.getString("carrierPartyId"));
-      }
-      return "";
-    }
-    catch(GenericEntityException e) { Debug.logWarning(e); }    
-    return "";
-  }
-  
-  public GenericValue getShippingAddress() {
-    GenericDelegator delegator = orderHeader.getDelegator();
-    try {
-      GenericValue orderContactMech = EntityUtil.getFirst(orderHeader.getRelatedByAnd("OrderContactMech", UtilMisc.toMap(
-              "contactMechPurposeTypeId", "SHIPPING_LOCATION")));
-      if (orderContactMech != null) {
-        GenericValue contactMech = orderContactMech.getRelatedOne("ContactMech");
-        if (contactMech != null) {
-          return contactMech.getRelatedOne("PostalAddress");
-        }
-      }
-    }
-    catch(GenericEntityException e) { Debug.logWarning(e); }    
-    return null;
-  }
 
-  public GenericValue getBillingAddress() {
-    GenericDelegator delegator = orderHeader.getDelegator();
-    try {
-      GenericValue orderContactMech = EntityUtil.getFirst(orderHeader.getRelatedByAnd("OrderContactMech", UtilMisc.toMap("contactMechPurposeTypeId", "BILLING_LOCATION")));
-      if (orderContactMech != null) {
-        GenericValue contactMech = orderContactMech.getRelatedOne("ContactMech");
-        if (contactMech != null) {
-          return contactMech.getRelatedOne("PostalAddress");
+    private GenericValue orderHeader;
+    private Double totalPrice;
+
+    public OrderReadHelper(GenericValue orderHeader) {
+        this.orderHeader = orderHeader;
+    }
+
+    public String getShippingMethod() {
+        try {
+            GenericValue shipmentPreference = null;
+            Iterator tempIter = UtilMisc.toIterator(orderHeader.getRelated("OrderShipmentPreference"));
+            if (tempIter != null && tempIter.hasNext()) {
+                shipmentPreference = (GenericValue) tempIter.next();
+            }
+            if (shipmentPreference != null) {
+                GenericValue carrierShipmentMethod = shipmentPreference.getRelatedOne("CarrierShipmentMethod");
+                if (carrierShipmentMethod != null) {
+                    GenericValue shipmentMethodType = carrierShipmentMethod.getRelatedOne("ShipmentMethodType");
+                    if (shipmentMethodType != null) {
+                        return UtilFormatOut.checkNull(shipmentPreference.getString("carrierPartyId")) + " " + UtilFormatOut.checkNull(shipmentMethodType.getString("description"));
+                    }
+                }
+                return UtilFormatOut.checkNull(shipmentPreference.getString("carrierPartyId"));
+            }
+            return "";
+        } catch (GenericEntityException e) {
+            Debug.logWarning(e);
         }
-      }
+        return "";
     }
-    catch(GenericEntityException e) { Debug.logWarning(e); }    
-    return null;
-  }
-  
-  public double getTotalPrice() {
-    if (totalPrice == null) {
-      double total = getOrderItemsTotal();
-      Iterator iter = getAdjustmentIterator();
-      while (iter.hasNext()) {
-        Adjustment adjustment = (Adjustment) iter.next();
-        total += adjustment.getAmount();
-      }
-      totalPrice = new Double(total);
-    }//else already set
-    return totalPrice.doubleValue();
-  }
-  
-  public String getStatusString() {
-    Collection orderStatusList = null;
-    try { orderStatusList = orderHeader.getRelated("OrderStatus"); }
-    catch(GenericEntityException e) { Debug.logWarning(e); return ""; }
-    
-    Set orderStatusIdSet = new HashSet();
-    Iterator orderStatusIter = orderStatusList.iterator();
-    while (orderStatusIter.hasNext()) {
-      orderStatusIdSet.add(((GenericValue) orderStatusIter.next()).getString("statusId"));
-    }
-    Iterator orderStatusIdIter = orderStatusIdSet.iterator();
-    String orderStatusIds;
-    if (orderStatusIdIter.hasNext()) {
-      orderStatusIds = orderStatusIdIter.next().toString();
-      while (orderStatusIdIter.hasNext()) {
-        orderStatusIds += "/" + orderStatusIdIter.next().toString();
-      }
-    } else {
-      orderStatusIds = "(unspecified)";
-    }
-    return orderStatusIds;
-  }
-  
-  public GenericValue getBillToPerson() {
-    GenericDelegator delegator = orderHeader.getDelegator();
-    try {
-      GenericEntity billToRole = EntityUtil.getFirst(orderHeader.getRelatedByAnd("OrderRole", UtilMisc.toMap("roleTypeId", "BILL_TO_CUSTOMER")));
-      if (billToRole != null) {
-        return delegator.findByPrimaryKey("Person", UtilMisc.toMap("partyId", billToRole.getString("partyId")));
-      } 
-      else {
+
+    public GenericValue getShippingAddress() {
+        GenericDelegator delegator = orderHeader.getDelegator();
+        try {
+            GenericValue orderContactMech = EntityUtil.getFirst(orderHeader.getRelatedByAnd("OrderContactMech", UtilMisc.toMap(
+                    "contactMechPurposeTypeId", "SHIPPING_LOCATION")));
+            if (orderContactMech != null) {
+                GenericValue contactMech = orderContactMech.getRelatedOne("ContactMech");
+                if (contactMech != null) {
+                    return contactMech.getRelatedOne("PostalAddress");
+                }
+            }
+        } catch (GenericEntityException e) {
+            Debug.logWarning(e);
+        }
         return null;
-      }
     }
-    catch(GenericEntityException e) { Debug.logWarning(e); }
-    return null;
-  }
-  
-  public String getDistributorId() {
-    GenericDelegator delegator = orderHeader.getDelegator();
-    try {
-      GenericEntity distributorRole = EntityUtil.getFirst(orderHeader.getRelatedByAnd("OrderRole", UtilMisc.toMap("roleTypeId", "DISTRIBUTOR")));
-      return distributorRole == null ? null : distributorRole.getString("partyId");
-    }
-    catch(GenericEntityException e) { Debug.logWarning(e); }
-    return null;
-  }
-  
-  public double getOrderItemsTotal() {
-    double result = 0.0;
-    Iterator itemIter = null;
-    try { itemIter = UtilMisc.toIterator(orderHeader.getRelated("OrderItem")); }
-    catch(GenericEntityException e) { Debug.logWarning(e); itemIter = null; }
 
-    while(itemIter != null && itemIter.hasNext()) {
-      result += getOrderItemTotal((GenericValue) itemIter.next());
+    public GenericValue getBillingAddress() {
+        GenericDelegator delegator = orderHeader.getDelegator();
+        try {
+            GenericValue orderContactMech = EntityUtil.getFirst(orderHeader.getRelatedByAnd("OrderContactMech", UtilMisc.toMap("contactMechPurposeTypeId", "BILLING_LOCATION")));
+            if (orderContactMech != null) {
+                GenericValue contactMech = orderContactMech.getRelatedOne("ContactMech");
+                if (contactMech != null) {
+                    return contactMech.getRelatedOne("PostalAddress");
+                }
+            }
+        } catch (GenericEntityException e) {
+            Debug.logWarning(e);
+        }
+        return null;
     }
-    return result;
-  }
-  
-  
-  public double getOrderItemTotal(GenericValue orderItem) {
-    double result = orderItem.getDouble("unitPrice").doubleValue() * orderItem.getDouble("quantity").doubleValue();
-    //FIXME should include adjustments as well
-    return result;
-  }
-  
-  /** Iterator of OrderReadHelper.Adjustment */
-  public Iterator getAdjustmentIterator() {
-    try {
-      return new AdjustmentIterator(orderHeader.getRelated("OrderAdjustment"), getOrderItemsTotal());
+
+    public double getTotalPrice() {
+        if (totalPrice == null) {
+            double total = getOrderItemsTotal();
+            Iterator iter = getAdjustmentIterator();
+            while (iter.hasNext()) {
+                Adjustment adjustment = (Adjustment) iter.next();
+                total += adjustment.getAmount();
+            }
+            totalPrice = new Double(total);
+        }//else already set
+        return totalPrice.doubleValue();
     }
-    catch(GenericEntityException e) { Debug.logWarning(e); return null; }
-  }
-  
-  private class AdjustmentIterator implements Iterator {
-    private Iterator orderAdjustmentIter;
-    private double basePrice;
-    
-    private AdjustmentIterator(Collection orderAdjustments, double basePrice) {
-      this.basePrice = basePrice;
-      this.orderAdjustmentIter = orderAdjustments.iterator();
+
+    public String getStatusString() {
+        Collection orderStatusList = null;
+        try {
+            orderStatusList = orderHeader.getRelated("OrderStatus");
+        } catch (GenericEntityException e) {
+            Debug.logWarning(e);
+            return "";
+        }
+
+        Set orderStatusIdSet = new HashSet();
+        Iterator orderStatusIter = orderStatusList.iterator();
+        while (orderStatusIter.hasNext()) {
+            orderStatusIdSet.add(((GenericValue) orderStatusIter.next()).getString("statusId"));
+        }
+        Iterator orderStatusIdIter = orderStatusIdSet.iterator();
+        String orderStatusIds;
+        if (orderStatusIdIter.hasNext()) {
+            orderStatusIds = orderStatusIdIter.next().toString();
+            while (orderStatusIdIter.hasNext()) {
+                orderStatusIds += "/" + orderStatusIdIter.next().toString();
+            }
+        } else {
+            orderStatusIds = "(unspecified)";
+        }
+        return orderStatusIds;
     }
-    
-    public boolean hasNext() {
-      return orderAdjustmentIter.hasNext();
+
+    public GenericValue getBillToPerson() {
+        GenericDelegator delegator = orderHeader.getDelegator();
+        try {
+            GenericEntity billToRole = EntityUtil.getFirst(orderHeader.getRelatedByAnd("OrderRole", UtilMisc.toMap("roleTypeId", "BILL_TO_CUSTOMER")));
+            if (billToRole != null) {
+                return delegator.findByPrimaryKey("Person", UtilMisc.toMap("partyId", billToRole.getString("partyId")));
+            } else {
+                return null;
+            }
+        } catch (GenericEntityException e) {
+            Debug.logWarning(e);
+        }
+        return null;
     }
-    
-    public Object next() {
-      GenericValue orderAdjustment = (GenericValue) orderAdjustmentIter.next();
-      GenericValue orderAdjustmentType = null;
-      try { orderAdjustmentType = orderAdjustment.getRelatedOne("OrderAdjustmentType"); }
-      catch(GenericEntityException e) { Debug.logWarning(e); orderAdjustmentType = null; }
-      String description;
-      if (orderAdjustmentType != null) {
-        description = orderAdjustmentType.getString("description");
-      } else {
-        //if not linked to an adjustment type, leave the description generic
-        description = "Adjustment";
-      }
-      Adjustment result = new Adjustment(description, orderAdjustment.getDouble("amount"), orderAdjustment.getDouble("percentage"), basePrice);
-      if ("SHIPPING_CHARGES".equals(orderAdjustmentType.getString("orderAdjustmentTypeId"))
-      && (getShippingMethod() != null)) {
-        //put the shipping method in the adjustment description
-        result.prependDescription(getShippingMethod() + " ");
-      }//else keep as is
-      return result;
+
+    public String getDistributorId() {
+        GenericDelegator delegator = orderHeader.getDelegator();
+        try {
+            GenericEntity distributorRole = EntityUtil.getFirst(orderHeader.getRelatedByAnd("OrderRole", UtilMisc.toMap("roleTypeId", "DISTRIBUTOR")));
+            return distributorRole == null ? null : distributorRole.getString("partyId");
+        } catch (GenericEntityException e) {
+            Debug.logWarning(e);
+        }
+        return null;
     }
-    
-    public void remove() { throw new UnsupportedOperationException(); }
-  }
+
+    public String getAffiliateId() {
+        GenericDelegator delegator = orderHeader.getDelegator();
+        try {
+            GenericEntity distributorRole = EntityUtil.getFirst(orderHeader.getRelatedByAnd("OrderRole", UtilMisc.toMap("roleTypeId", "AFFILIATE")));
+            return distributorRole == null ? null : distributorRole.getString("partyId");
+        } catch (GenericEntityException e) {
+            Debug.logWarning(e);
+        }
+        return null;
+    }
+
+    public double getOrderItemsTotal() {
+        double result = 0.0;
+        Iterator itemIter = null;
+        try {
+            itemIter = UtilMisc.toIterator(orderHeader.getRelated("OrderItem"));
+        } catch (GenericEntityException e) {
+            Debug.logWarning(e);
+            itemIter = null;
+        }
+
+        while (itemIter != null && itemIter.hasNext()) {
+            result += getOrderItemTotal((GenericValue) itemIter.next());
+        }
+        return result;
+    }
+
+
+    public double getOrderItemTotal(GenericValue orderItem) {
+        double result = orderItem.getDouble("unitPrice").doubleValue() * orderItem.getDouble("quantity").doubleValue();
+        //FIXME should include adjustments as well
+        return result;
+    }
+
+    /** Iterator of OrderReadHelper.Adjustment */
+    public Iterator getAdjustmentIterator() {
+        try {
+            return new AdjustmentIterator(orderHeader.getRelated("OrderAdjustment"), getOrderItemsTotal());
+        } catch (GenericEntityException e) {
+            Debug.logWarning(e);
+            return null;
+        }
+    }
+
+    private class AdjustmentIterator implements Iterator {
+
+        private Iterator orderAdjustmentIter;
+        private double basePrice;
+
+        private AdjustmentIterator(Collection orderAdjustments, double basePrice) {
+            this.basePrice = basePrice;
+            this.orderAdjustmentIter = orderAdjustments.iterator();
+        }
+
+        public boolean hasNext() {
+            return orderAdjustmentIter.hasNext();
+        }
+
+        public Object next() {
+            GenericValue orderAdjustment = (GenericValue) orderAdjustmentIter.next();
+            GenericValue orderAdjustmentType = null;
+            try {
+                orderAdjustmentType = orderAdjustment.getRelatedOne("OrderAdjustmentType");
+            } catch (GenericEntityException e) {
+                Debug.logWarning(e);
+                orderAdjustmentType = null;
+            }
+            String description;
+            if (orderAdjustmentType != null) {
+                description = orderAdjustmentType.getString("description");
+            } else {
+                //if not linked to an adjustment type, leave the description generic
+                description = "Adjustment";
+            }
+            Adjustment result = new Adjustment(description, orderAdjustment.getDouble("amount"), orderAdjustment.getDouble("percentage"), basePrice);
+            if ("SHIPPING_CHARGES".equals(orderAdjustmentType.getString("orderAdjustmentTypeId"))
+                    && (getShippingMethod() != null)) {
+                //put the shipping method in the adjustment description
+                result.prependDescription(getShippingMethod() + " ");
+            }//else keep as is
+            return result;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
 }
