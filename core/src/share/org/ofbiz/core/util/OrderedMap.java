e96e72aabb4ab1bbf1cb976cc04735075a027175
azeneski <>
2002-04-05 21:54:07 +0000

    added new OrderedSet and OrderedMap classes

---
+++ core/src/share/org/ofbiz/core/util/OrderedMap.java
@@ -0,0 +1,91 @@
+/*
+ * $Id$
+ *
+ *  Copyright (c) 2002 The Open For Business Project (www.ofbiz.org)
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ *  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ *  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.ofbiz.core.util;
+
+import java.util.*;
+
+/**
+ * OrderedMap - HashMap backed by a linked list.
+ *
+ * @author     <a href="mailto:jaz@jflow.net">Andy Zeneski</a>
+ * @version    1.0
+ * @created    April 5, 2002
+ */
+public class OrderedMap extends HashMap {
+
+    private List orderedKeys = new LinkedList();
+
+    /**
+     * Returns a set view of the keys contained in this map.  Unlike a HashMap
+     * the set is NOT backed by the map, so changes to the map are NOT reflected
+     * in the set, and vice-versa.
+     *
+     * @return a set view of the keys contained in this map.
+     */
+    public Set keySet() {
+        return new OrderedSet(orderedKeys);
+    }
+
+    /**
+     * Associates the specified value with the specified key in this map.
+     * If the map previously contained a mapping for this key, the old
+     * value is replaced.
+     *
+     * @param key key with which the specified value is to be associated.
+     * @param value value to be associated with the specified key.
+     * @return previous value associated with specified key, or <tt>null</tt>
+     *	       if there was no mapping for key.  A <tt>null</tt> return can
+     *	       also indicate that the HashMap previously associated
+     *	       <tt>null</tt> with the specified key.
+     */
+    public Object put(Object key, Object value) {
+        if (!orderedKeys.contains(key))
+            orderedKeys.add(key);
+        return super.put(key, value);
+    }
+
+    /**
+     * Removes all mappings from this map.
+     */
+    public void clear() {
+        super.clear();
+        orderedKeys.clear();
+    }
+
+    /**
+     * Removes the mapping for this key from this map if present.
+     *
+     * @param key key whose mapping is to be removed from the map.
+     * @return previous value associated with specified key, or <tt>null</tt>
+     *	       if there was no mapping for key.  A <tt>null</tt> return can
+     *	       also indicate that the map previously associated <tt>null</tt>
+     *	       with the specified key.
+     */
+    public Object remove(Object key) {
+        if (orderedKeys.contains(key))
+            orderedKeys.remove(key);
+        return super.remove(key);
+    }
+
+}
---
+++ core/src/share/org/ofbiz/core/util/OrderedSet.java
@@ -0,0 +1,82 @@
+/*
+ * $Id$
+ *
+ *  Copyright (c) 2002 The Open For Business Project (www.ofbiz.org)
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ *  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ *  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.ofbiz.core.util;
+
+import java.util.*;
+
+/**
+ * OrderedSet - Set interface wrapper around a LinkedList
+ *
+ * @author     <a href="mailto:jaz@jflow.net">Andy Zeneski</a>
+ * @version    1.0
+ * @created    April 5, 2002
+ */
+public class OrderedSet extends AbstractSet {
+
+    // This set's back LinkedList
+    private List backedList = new LinkedList();
+
+
+    /**
+     * Constructs a set containing the elements of the specified
+     * collection, in the order they are returned by the collection's
+     * iterator.
+     *
+     * @param c the collection whose elements are to be placed into this set.
+     */
+    public OrderedSet(Collection c) {
+        Iterator i = c.iterator();
+        while (i.hasNext())
+            add(i.next());
+    }
+
+    /**
+     * Returns an iterator over the elements in this set.
+     *
+     * @return an iterator over the elements in this set.
+     */
+    public Iterator iterator() {
+        return backedList.iterator();
+    }
+
+    /**
+     * Returns the number of elements in this set.
+     *
+     * @return the number of elements in this set.
+     */
+     public int size() {
+         return backedList.size();
+     }
+
+    /**
+     * Appends the specified element to the end of this set.
+
+     * @param obj element to be appended to this set.
+     * @return <tt>true</tt> (as per the general contract of
+     * <tt>Collection.add</tt>).
+     */
+    public boolean add(Object obj) {
+        return backedList.add(obj);
+    }
+}
