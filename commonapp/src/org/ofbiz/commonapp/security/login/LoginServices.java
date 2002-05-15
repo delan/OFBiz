8c2554917b44cca6a9f94fafad2d6c7c89527d8e
jonesde <>
2002-05-15 03:30:36 +0000

    Returned to the ORIGINAL design so that if the current password is null, it won't complain about it, ie useful for create new customer, etc

--- commonapp/src/org/ofbiz/commonapp/security/login/LoginServices.java
+++ commonapp/src/org/ofbiz/commonapp/security/login/LoginServices.java
@@ -216,8 +216,10 @@ public class LoginServices {
     }
 
     public static void checkNewPassword(GenericValue userLogin, String currentPassword, String newPassword, String newPasswordVerify, String passwordHint, List errorMessageList, boolean adminUser) {
-        if ((currentPassword == null && !adminUser) || (!adminUser && userLogin != null && currentPassword != null && !currentPassword.equals(userLogin.getString("currentPassword"))))
+        if ((currentPassword == null) || 
+            (!adminUser && userLogin != null && currentPassword != null && !currentPassword.equals(userLogin.getString("currentPassword")))) {
             errorMessageList.add("Old Password was not correct, please re-enter.");
+        }
 
         if (!UtilValidate.isNotEmpty(newPassword) || !UtilValidate.isNotEmpty(newPasswordVerify)) {
             errorMessageList.add("Password or verify password missing.");
