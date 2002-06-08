715359d64f309801ff163c6c7d33206d7e9c9452
jonesde <>
2002-06-08 19:54:21 +0000

    Added tag for content urls

---
+++ core/src/webapp/org/ofbiz/core/taglib/ContentUrlTag.java
@@ -0,0 +1,77 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2002 The Open For Business Project - www.ofbiz.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package org.ofbiz.core.taglib;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+import javax.servlet.jsp.tagext.*;
+
+import org.ofbiz.core.control.*;
+import org.ofbiz.core.util.*;
+
+/**
+ * ContentUrlTag - Creates a URL string prepending the content prefix from url.properties
+ *
+ * @author     <a href="mailto:jaz@zsolv.com">Andy Zeneski</a>
+ * @version    1.0
+ * @created    August 4, 2001
+ */
+public class ContentUrlTag extends BodyTagSupport {
+
+    public static final String module = UrlTag.class.getName();
+
+    public int doEndTag() throws JspException {
+        HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
+
+        BodyContent body = getBodyContent();
+        String bodyString = body.getString();
+
+        StringBuffer newURL = new StringBuffer();
+        if (request.isSecure()) {
+            String prefix = UtilProperties.getPropertyValue("url", "content.url.prefix.secure");
+            if (prefix != null) {
+                newURL.append(prefix);
+            }
+        } else {
+            String prefix = UtilProperties.getPropertyValue("url", "content.url.prefix.standard");
+            if (prefix != null) {
+                newURL.append(prefix);
+            }
+        }
+        
+        newURL.append(bodyString);
+        body.clearBody();
+
+        try {
+            getPreviousOut().print(newURL.toString());
+        } catch (IOException e) {
+            throw new JspException(e.getMessage());
+        }
+        return SKIP_BODY;
+    }
+}
