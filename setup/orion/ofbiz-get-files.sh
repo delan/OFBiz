679b1ef76cd5cae5eba11a4615559f411fc98ace
jonesde <>
2002-06-24 22:03:33 +0000

    Changed pgjdc2.jar to postgresql.jar

--- setup/orion/ofbiz-get-files.sh
+++ setup/orion/ofbiz-get-files.sh
@@ -23,7 +23,7 @@ cp -i $OFBIZ_HOME/commonapp/etc/localdtds.properties $ORION_HOME/lib
 
 # get all of the third party jars
 #cp -i $OFBIZ_HOME/lib/common/tyrex-1.0.jar $OFBIZ_HOME/lib/common/ots-jts_1.0.jar $ORION_HOME/lib
-cp -i $OFBIZ_HOME/lib/common/castor-0.9.3.9.jar $OFBIZ_HOME/lib/common/hsqldb.jar $OFBIZ_HOME/lib/common/pgjdbc2.jar $ORION_HOME/lib
+cp -i $OFBIZ_HOME/lib/common/castor-0.9.3.9.jar $OFBIZ_HOME/lib/common/hsqldb.jar $OFBIZ_HOME/lib/common/postgresql.jar $ORION_HOME/lib
 cp -i $OFBIZ_HOME/lib/common/mm.mysql-2.0.8-bin.jar $OFBIZ_HOME/lib/common/log4j.jar $ORION_HOME/lib
 cp -i $OFBIZ_HOME/lib/share/axis.jar $OFBIZ_HOME/lib/share/clutil.jar $OFBIZ_HOME/lib/share/wsdl4j.jar $ORION_HOME/lib
 cp -i $OFBIZ_HOME/lib/share/bsh-1.2b6.jar $OFBIZ_HOME/lib/share/jakarta-oro-2.0.4.jar $ORION_HOME/lib
