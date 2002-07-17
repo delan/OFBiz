80825aaa6fb303191cba5ef8d01179eff4aded80
jonesde <>
2002-07-17 06:40:38 +0000

    Made a little dummy Map class for use in UtilMisc.toMap methods with 1-4 name/value pairs; should create very fast and turns into a wrapper around a normal HashMap when modified or in various other conditions

--- core/src/share/org/ofbiz/core/util/UtilMisc.java
+++ core/src/share/org/ofbiz/core/util/UtilMisc.java
@@ -52,30 +52,33 @@ public class UtilMisc {
      * @return The resulting Map
      */
     public static Map toMap(String name1, Object value1) {
-        Map fields = new HashMap();
+        return new UtilMisc.SimpleMap(name1, value1);
+        /*Map fields = new HashMap();
         fields.put(name1, value1);
-        return fields;
+        return fields;*/
     }
 
     /** Create a map from passed nameX, valueX parameters
      * @return The resulting Map
      */
     public static Map toMap(String name1, Object value1, String name2, Object value2) {
-        Map fields = new HashMap();
+        return new UtilMisc.SimpleMap(name1, value1, name2, value2);
+        /*Map fields = new HashMap();
         fields.put(name1, value1);
         fields.put(name2, value2);
-        return fields;
+        return fields;*/
     }
 
     /** Create a map from passed nameX, valueX parameters
      * @return The resulting Map
      */
     public static Map toMap(String name1, Object value1, String name2, Object value2, String name3, Object value3) {
-        Map fields = new HashMap();
+        return new UtilMisc.SimpleMap(name1, value1, name2, value2, name3, value3);
+        /*Map fields = new HashMap();
         fields.put(name1, value1);
         fields.put(name2, value2);
         fields.put(name3, value3);
-        return fields;
+        return fields;*/
     }
 
     /** Create a map from passed nameX, valueX parameters
@@ -83,12 +86,13 @@ public class UtilMisc {
      */
     public static Map toMap(String name1, Object value1, String name2, Object value2, String name3,
                             Object value3, String name4, Object value4) {
-        Map fields = new HashMap();
+        return new UtilMisc.SimpleMap(name1, value1, name2, value2, name3, value3, name4, value4);
+        /*Map fields = new HashMap();
         fields.put(name1, value1);
         fields.put(name2, value2);
         fields.put(name3, value3);
         fields.put(name4, value4);
-        return fields;
+        return fields;*/
     }
 
     /** Create a map from passed nameX, valueX parameters
@@ -124,7 +128,7 @@ public class UtilMisc {
      * @return The resulting List
      */
     public static List toList(Object obj1) {
-        List list = new ArrayList();
+        List list = new ArrayList(1);
         list.add(obj1);
         return list;
     }
@@ -133,7 +137,7 @@ public class UtilMisc {
      * @return The resulting List
      */
     public static List toList(Object obj1, Object obj2) {
-        List list = new ArrayList();
+        List list = new ArrayList(2);
         list.add(obj1);
         list.add(obj2);
         return list;
@@ -143,7 +147,7 @@ public class UtilMisc {
      * @return The resulting List
      */
     public static List toList(Object obj1, Object obj2, Object obj3) {
-        List list = new ArrayList();
+        List list = new ArrayList(3);
         list.add(obj1);
         list.add(obj2);
         list.add(obj3);
@@ -154,7 +158,7 @@ public class UtilMisc {
      * @return The resulting List
      */
     public static List toList(Object obj1, Object obj2, Object obj3, Object obj4) {
-        List list = new ArrayList();
+        List list = new ArrayList(4);
         list.add(obj1);
         list.add(obj2);
         list.add(obj3);
@@ -166,7 +170,7 @@ public class UtilMisc {
      * @return The resulting List
      */
     public static List toList(Object obj1, Object obj2, Object obj3, Object obj4, Object obj5) {
-        List list = new ArrayList();
+        List list = new ArrayList(5);
         list.add(obj1);
         list.add(obj2);
         list.add(obj3);
@@ -179,7 +183,7 @@ public class UtilMisc {
      * @return The resulting List
      */
     public static List toList(Object obj1, Object obj2, Object obj3, Object obj4, Object obj5, Object obj6) {
-        List list = new ArrayList();
+        List list = new ArrayList(6);
         list.add(obj1);
         list.add(obj2);
         list.add(obj3);
@@ -209,5 +213,174 @@ public class UtilMisc {
             paramMap.put(name, request.getParameter(name));
         }
         return (Map) paramMap;
+    }
+
+    /** This is meant to be very quick to create and use for small sized maps, perfect for how we usually use UtilMisc.toMap */
+    protected static class SimpleMap implements Map {
+        protected Map realMapIfNeeded = null;
+        
+        int sizeValue;
+        String name1 = null;
+        String name2 = null;
+        String name3 = null;
+        String name4 = null;
+        Object value1 = null;
+        Object value2 = null;
+        Object value3 = null;
+        Object value4 = null;
+        
+        public SimpleMap(String name1, Object value1) {
+            sizeValue = 1;
+            this.name1 = name1; this.value1 = value1;
+        }
+        public SimpleMap(String name1, Object value1, String name2, Object value2) {
+            sizeValue = 2;
+            this.name1 = name1; this.value1 = value1;
+            this.name2 = name2; this.value2 = value2;
+        }
+        public SimpleMap(String name1, Object value1, String name2, Object value2, String name3, Object value3) {
+            sizeValue = 3;
+            this.name1 = name1; this.value1 = value1;
+            this.name2 = name2; this.value2 = value2;
+            this.name3 = name3; this.value3 = value3;
+        }
+        public SimpleMap(String name1, Object value1, String name2, Object value2, String name3, Object value3, String name4, Object value4) {
+            sizeValue = 4;
+            this.name1 = name1; this.value1 = value1;
+            this.name2 = name2; this.value2 = value2;
+            this.name3 = name3; this.value3 = value3;
+            this.name4 = name4; this.value4 = value4;
+        }
+        
+        protected void makeRealMap() {
+            realMapIfNeeded = new HashMap();
+            if (name1 != null) realMapIfNeeded.put(name1, value1);
+            if (name2 != null) realMapIfNeeded.put(name2, value2);
+            if (name3 != null) realMapIfNeeded.put(name3, value3);
+            if (name4 != null) realMapIfNeeded.put(name4, value4);
+        }
+        
+        public void clear() {
+            if (realMapIfNeeded != null) {
+                realMapIfNeeded.clear();
+            } else {
+                realMapIfNeeded = new HashMap();
+                name1 = null;
+                name2 = null;
+                name3 = null;
+                name4 = null;
+                value1 = null;
+                value2 = null;
+                value3 = null;
+                value4 = null;
+            }
+        }
+        
+        public boolean containsKey(Object obj) {
+            if (realMapIfNeeded != null) {
+                return realMapIfNeeded.containsKey(obj);
+            } else {
+                if (name1 != null && name1.equals(obj)) return true;
+                if (name2 != null && name2.equals(obj)) return true;
+                if (name3 != null && name3.equals(obj)) return true;
+                if (name4 != null && name4.equals(obj)) return true;
+                return false;
+            }
+        }
+        
+        public boolean containsValue(Object obj) {
+            if (realMapIfNeeded != null) {
+                return realMapIfNeeded.containsValue(obj);
+            } else {
+                if (value1 != null && value1.equals(obj)) return true;
+                if (value2 != null && value2.equals(obj)) return true;
+                if (value3 != null && value3.equals(obj)) return true;
+                if (value4 != null && value4.equals(obj)) return true;
+                return false;
+            }
+        }
+        
+        public java.util.Set entrySet() {
+            if (realMapIfNeeded != null) {
+                return realMapIfNeeded.entrySet();
+            } else {
+                this.makeRealMap();
+                return realMapIfNeeded.entrySet();
+            }
+        }
+        
+        public Object get(Object obj) {
+            if (realMapIfNeeded != null) {
+                return realMapIfNeeded.get(obj);
+            } else {
+                if (name1 != null && name1.equals(obj)) return value1;
+                if (name2 != null && name2.equals(obj)) return value2;
+                if (name3 != null && name3.equals(obj)) return value3;
+                if (name4 != null && name4.equals(obj)) return value4;
+                return null;
+            }
+        }
+        
+        public boolean isEmpty() {
+            if (realMapIfNeeded != null) {
+                return realMapIfNeeded.isEmpty();
+            } else {
+                if (this.sizeValue == 0) return true;
+                return false;
+            }
+        }
+        
+        public java.util.Set keySet() {
+            if (realMapIfNeeded != null) {
+                return realMapIfNeeded.keySet();
+            } else {
+                this.makeRealMap();
+                return realMapIfNeeded.keySet();
+            }
+        }
+        
+        public Object put(Object obj, Object obj1) {
+            if (realMapIfNeeded != null) {
+                return realMapIfNeeded.put(obj, obj1);
+            } else {
+                this.makeRealMap();
+                return realMapIfNeeded.put(obj, obj1);
+            }
+        }
+        
+        public void putAll(java.util.Map map) {
+            if (realMapIfNeeded != null) {
+                realMapIfNeeded.putAll(map);
+            } else {
+                this.makeRealMap();
+                realMapIfNeeded.putAll(map);
+            }
+        }
+        
+        public Object remove(Object obj) {
+            if (realMapIfNeeded != null) {
+                return realMapIfNeeded.remove(obj);
+            } else {
+                this.makeRealMap();
+                return realMapIfNeeded.remove(obj);
+            }
+        }
+        
+        public int size() {
+            if (realMapIfNeeded != null) {
+                return realMapIfNeeded.size();
+            } else {
+                return this.sizeValue;
+            }
+        }
+        
+        public java.util.Collection values() {
+            if (realMapIfNeeded != null) {
+                return realMapIfNeeded.values();
+            } else {
+                this.makeRealMap();
+                return realMapIfNeeded.values();
+            }
+        }
     }
 }
