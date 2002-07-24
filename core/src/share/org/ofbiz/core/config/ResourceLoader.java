e3a82280152c5bbca6543a7f6f8e27ff6682175b
ccnelson <>
2002-07-24 22:20:17 +0000

    Added invalidateDocument method to support reloading EntityConfig

--- core/src/share/org/ofbiz/core/config/ResourceLoader.java
+++ core/src/share/org/ofbiz/core/config/ResourceLoader.java
@@ -83,6 +83,13 @@ public abstract class ResourceLoader {
         }
     }
 
+
+	public static void invalidateDocument(String xmlFilename) throws GenericConfigException {
+		synchronized (ResourceLoader.class) {
+			docSaveMap.remove(xmlFilename);
+		}
+	}
+	
     public static Document getXmlDocument(String xmlFilename) throws GenericConfigException {
         Document document = (Document) docSaveMap.get(xmlFilename);
         if (document == null) {
