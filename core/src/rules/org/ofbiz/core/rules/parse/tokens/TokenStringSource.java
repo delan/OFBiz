82a96ea6000c76bba2aa8d6d6aecfa5e48bc20a1
jonesde <>
2002-08-18 00:28:26 +0000

    Fixed the other toArray type casting problems; special thanks to superbabu for the tip

--- core/src/rules/org/ofbiz/core/rules/parse/tokens/TokenStringSource.java
+++ core/src/rules/org/ofbiz/core/rules/parse/tokens/TokenStringSource.java
@@ -57,115 +57,114 @@ import java.util.*;
  * @version 1.0
  */
 public class TokenStringSource {
-  protected Tokenizer tokenizer;
-  protected String delimiter;
-  protected TokenString cachedTokenString = null;
-  /**
-   * Constructs a TokenStringSource that will read TokenStrings
-   * using the specified tokenizer, delimited by the specified
-   * delimiter.
-   *
-   * @param   tokenizer   a tokenizer to read tokens from
-   *
-   * @param   delimiter   the character that fences off where one
-   *                      TokenString ends and the next begins
-   *
-   * @returns   a TokenStringSource that will read TokenStrings
-   *            from the specified tokenizer, delimited by the
-   *            specified delimiter
-   */
-  public TokenStringSource(
-  Tokenizer tokenizer, String delimiter) {
-    
-    this.tokenizer = tokenizer;
-    this.delimiter = delimiter;
-  }
-  /**
-   * The design of <code>nextTokenString</code> is that is
-   * always returns a cached value. This method will (at least
-   * attempt to) load the cache if the cache is empty.
-   */
-  protected void ensureCacheIsLoaded() {
-    if (cachedTokenString == null) {
-      loadCache();
+    protected Tokenizer tokenizer;
+    protected String delimiter;
+    protected TokenString cachedTokenString = null;
+    /**
+     * Constructs a TokenStringSource that will read TokenStrings
+     * using the specified tokenizer, delimited by the specified
+     * delimiter.
+     *
+     * @param   tokenizer   a tokenizer to read tokens from
+     *
+     * @param   delimiter   the character that fences off where one
+     *                      TokenString ends and the next begins
+     *
+     * @returns   a TokenStringSource that will read TokenStrings
+     *            from the specified tokenizer, delimited by the
+     *            specified delimiter
+     */
+    public TokenStringSource(
+    Tokenizer tokenizer, String delimiter) {
+        
+        this.tokenizer = tokenizer;
+        this.delimiter = delimiter;
     }
-  }
-  /**
-   * Returns true if the source has more TokenStrings.
-   *
-   * @return   true, if the source has more TokenStrings that
-   *           have not yet been popped with <code>
-   *           nextTokenString</code>.
-   */
-  public boolean hasMoreTokenStrings() {
-    ensureCacheIsLoaded();
-    return cachedTokenString != null;
-  }
-  /**
-   * Loads the next TokenString into the cache, or sets the
-   * cache to null if the source is out of tokens.
-   */
-  protected void loadCache() {
-    List tokenVector = nextVector();
-    if (tokenVector.isEmpty()) {
-      cachedTokenString = null;
-    }
-    else {
-      Token tokens[] = (Token[]) tokenVector.toArray();
-      cachedTokenString = new TokenString(tokens);
-    }
-  }
-  /**
-   * Shows the example in the class comment.
-   *
-   * @param args ignored
-   */
-  public static void main(String args[]) {
-    
-    String s = "I came; I saw; I left in peace;";
-    
-    TokenStringSource tss =
-    new TokenStringSource(new Tokenizer(s), ";");
-    
-    while (tss.hasMoreTokenStrings()) {
-      System.out.println(tss.nextTokenString());
-    }
-  }
-  /**
-   * Returns the next TokenString from the source.
-   *
-   * @return   the next TokenString from the source
-   */
-  public TokenString nextTokenString() {
-    ensureCacheIsLoaded();
-    TokenString returnTokenString = cachedTokenString;
-    cachedTokenString = null;
-    return returnTokenString;
-  }
-  /**
-   * Returns a List of the tokens in the source up to either
-   * the delimiter or the end of the source.
-   *
-   * @return   a List of the tokens in the source up to either
-   *           the delimiter or the end of the source.
-   */
-  protected List nextVector() {
-    List v = new ArrayList();
-    try {
-      while (true) {
-        Token tok = tokenizer.nextToken();
-        if (tok.ttype() == Token.TT_EOF ||
-        tok.sval().equals(delimiter)) {
-          
-          break;
+    /**
+     * The design of <code>nextTokenString</code> is that is
+     * always returns a cached value. This method will (at least
+     * attempt to) load the cache if the cache is empty.
+     */
+    protected void ensureCacheIsLoaded() {
+        if (cachedTokenString == null) {
+            loadCache();
         }
-        v.add(tok);
-      }
     }
-    catch(IOException e) {
-      throw new InternalError(
-      "Problem tokenizing string: " + e);
+    /**
+     * Returns true if the source has more TokenStrings.
+     *
+     * @return   true, if the source has more TokenStrings that
+     *           have not yet been popped with <code>
+     *           nextTokenString</code>.
+     */
+    public boolean hasMoreTokenStrings() {
+        ensureCacheIsLoaded();
+        return cachedTokenString != null;
     }
-    return v;
-  }
+    /**
+     * Loads the next TokenString into the cache, or sets the
+     * cache to null if the source is out of tokens.
+     */
+    protected void loadCache() {
+        List tokenVector = nextVector();
+        if (tokenVector.isEmpty()) {
+            cachedTokenString = null;
+        } else {
+            Token tokens[] = (Token[]) tokenVector.toArray(new Token[tokenVector.size()]);
+            cachedTokenString = new TokenString(tokens);
+        }
+    }
+    /**
+     * Shows the example in the class comment.
+     *
+     * @param args ignored
+     */
+    public static void main(String args[]) {
+        
+        String s = "I came; I saw; I left in peace;";
+        
+        TokenStringSource tss =
+        new TokenStringSource(new Tokenizer(s), ";");
+        
+        while (tss.hasMoreTokenStrings()) {
+            System.out.println(tss.nextTokenString());
+        }
+    }
+    /**
+     * Returns the next TokenString from the source.
+     *
+     * @return   the next TokenString from the source
+     */
+    public TokenString nextTokenString() {
+        ensureCacheIsLoaded();
+        TokenString returnTokenString = cachedTokenString;
+        cachedTokenString = null;
+        return returnTokenString;
+    }
+    /**
+     * Returns a List of the tokens in the source up to either
+     * the delimiter or the end of the source.
+     *
+     * @return   a List of the tokens in the source up to either
+     *           the delimiter or the end of the source.
+     */
+    protected List nextVector() {
+        List v = new ArrayList();
+        try {
+            while (true) {
+                Token tok = tokenizer.nextToken();
+                if (tok.ttype() == Token.TT_EOF ||
+                tok.sval().equals(delimiter)) {
+                    
+                    break;
+                }
+                v.add(tok);
+            }
+        }
+        catch(IOException e) {
+            throw new InternalError(
+            "Problem tokenizing string: " + e);
+        }
+        return v;
+    }
 }
