50110b30bc8686f7df4ed737ce78a45df181db4f
azeneski <>
2002-10-17 23:31:29 +0000

    added new interface engine

---
+++ core/src/service/org/ofbiz/core/service/engine/InterfaceEngine.java
@@ -0,0 +1,66 @@
+/*
+ * $Id$
+ *
+ *  Copyright (c) 2001, 2002 The Open For Business Project - www.ofbiz.org
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ *  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ *  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ofbiz.core.service.engine;
+
+import java.util.*;
+
+import org.ofbiz.core.service.*;
+
+/**
+ * InterfaceEngine.java
+ * 
+ * @author     <a href="mailto:jaz@jflow.net">Andy Zeneski</a>
+ * @created    Oct 17, 2002
+ * @version    2.0
+ */
+public class InterfaceEngine implements GenericEngine {
+
+    /**
+     * @see org.ofbiz.core.service.engine.GenericEngine#runSync(java.lang.String, org.ofbiz.core.service.ModelService, java.util.Map)
+     */
+    public Map runSync(String localName, ModelService modelService, Map context) throws GenericServiceException {
+        throw new GenericServiceException("Interface services cannot be invoked; try invoking an implementing service.");
+    }
+
+    /**
+     * @see org.ofbiz.core.service.engine.GenericEngine#runSyncIgnore(java.lang.String, org.ofbiz.core.service.ModelService, java.util.Map)
+     */
+    public void runSyncIgnore(String localName, ModelService modelService, Map context) throws GenericServiceException {        
+       throw new GenericServiceException("Interface services cannot be invoked; try invoking an implementing service.");
+    }
+
+    /**
+     * @see org.ofbiz.core.service.engine.GenericEngine#runAsync(java.lang.String, org.ofbiz.core.service.ModelService, java.util.Map, org.ofbiz.core.service.GenericRequester, boolean)
+     */
+    public void runAsync(String localName, ModelService modelService, Map context, GenericRequester requester, boolean persist) throws GenericServiceException {
+       throw new GenericServiceException("Interface services cannot be invoked; try invoking an implementing service.");
+    }        
+
+    /**
+     * @see org.ofbiz.core.service.engine.GenericEngine#runAsync(java.lang.String, org.ofbiz.core.service.ModelService, java.util.Map, boolean)
+     */
+    public void runAsync(String localName, ModelService modelService, Map context, boolean persist) throws GenericServiceException {        
+        throw new GenericServiceException("Interface services cannot be invoked; try invoking an implementing service.");
+    }
+}
