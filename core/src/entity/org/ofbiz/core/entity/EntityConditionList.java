330cb9781da6d90d55fe4824dc3cc5f026e87458
jonesde <>
2002-11-02 14:10:15 +0000

    Added new EntityConditionList, works basicly the same as the EntityExprList, but is more general for EntityConditions; this is probably what I should have done in the first place instead of EntityExprList.

---
+++ core/src/entity/org/ofbiz/core/entity/EntityConditionList.java
@@ -0,0 +1,99 @@
+/*
+ * $Id$
+ *
+ * <p>Copyright (c) 2001 The Open For Business Project - www.ofbiz.org
+ *
+ * <p>Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ * <p>The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ * <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ *  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ *  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.ofbiz.core.entity;
+
+
+import java.io.*;
+import java.util.*;
+import org.ofbiz.core.entity.model.*;
+import org.ofbiz.core.entity.jdbc.*;
+import org.ofbiz.core.util.*;
+
+
+/**
+ * Encapsulates a list of EntityConditions to be used as a single EntityCondition combined as specified
+ *
+ *@author     <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
+ *@created    Nov 13, 2001
+ *@version    1.0
+ */
+public class EntityConditionList extends EntityCondition {
+
+    protected List condList;
+    protected EntityOperator operator;
+
+    protected EntityConditionList() {}
+
+    public EntityConditionList(List condList, EntityOperator operator) {
+        this.condList = condList;
+        this.operator = operator;
+    }
+
+    public String makeWhereString(ModelEntity modelEntity, List entityConditionParams) {
+        // if (Debug.verboseOn()) Debug.logVerbose("makeWhereString for entity " + modelEntity.getEntityName());
+        StringBuffer whereStringBuffer = new StringBuffer();
+
+        if (condList != null && condList.size() > 0) {
+            for (int i = 0; i < condList.size(); i++) {
+                EntityCondition condition = (EntityCondition) condList.get(i);
+
+                whereStringBuffer.append(condition.makeWhereString(modelEntity, entityConditionParams));
+                if (i < condList.size() - 1) {
+                    whereStringBuffer.append(' ');
+                    whereStringBuffer.append(operator.getCode());
+                    whereStringBuffer.append(' ');
+                }
+            }
+        }
+        return whereStringBuffer.toString();
+    }
+
+    public void checkCondition(ModelEntity modelEntity) throws GenericModelException {
+        // if (Debug.verboseOn()) Debug.logVerbose("checkCondition for entity " + modelEntity.getEntityName());
+        Iterator exprIter = condList.iterator();
+
+        while (exprIter.hasNext()) {
+            EntityCondition entityCondition = (EntityCondition) exprIter.next();
+
+            entityCondition.checkCondition(modelEntity);
+        }
+    }
+
+    public String toString() {
+        StringBuffer toStringBuffer = new StringBuffer();
+
+        toStringBuffer.append("[conditionList::");
+        if (condList != null && condList.size() > 0) {
+            for (int i = 0; i < condList.size(); i++) {
+                EntityCondition condition = (EntityCondition) condList.get(i);
+
+                toStringBuffer.append(condition.toString());
+                if (i > 0) toStringBuffer.append("::");
+            }
+        }
+        toStringBuffer.append(']');
+        return toStringBuffer.toString();
+    }
+}
