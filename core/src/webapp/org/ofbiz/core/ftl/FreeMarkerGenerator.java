c3d71570d319c2aa382f4bdb9270c8bf4fd95df3
jonesde <>
2003-02-12 11:07:04 +0000

    Added FreeMarkerGenerator, but it doesn't currently work due to who knows what; renamed FreeMarkerViewHandler for consisteny because I was bored and frustrated with the other junk

---
+++ core/src/webapp/org/ofbiz/core/ftl/FreeMarkerGenerator.java
@@ -0,0 +1,139 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2001, 2002 The Open For Business Project - www.ofbiz.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package org.ofbiz.core.ftl;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.jpublish.JPublishContext;
+import org.jpublish.Page;
+import org.jpublish.Repository;
+import org.jpublish.util.JPublishContextMap;
+
+import com.anthonyeden.lib.config.Configuration;
+import com.anthonyeden.lib.log.LogManager;
+import com.anthonyeden.lib.log.Logger;
+import com.wspublisher.WSPException;
+import com.wspublisher.generators.AbstractGenerator;
+import freemarker.ext.beans.BeansWrapper;
+import freemarker.template.SimpleHash;
+import freemarker.template.Template;
+
+import org.ofbiz.core.util.Debug;
+
+
+/**
+ * This generator uses FreeMarker to process a template file, which (hopefully)
+ * uses the content on the given page.
+ *
+ * @author     <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
+ * @version    $Revision$
+ * @since      2.1
+ */
+public class FreeMarkerGenerator extends AbstractGenerator {
+    public static final String NAME = "ofbiz-freemarker";
+    private static Logger log = LogManager.getLogger(FreeMarkerGenerator.class.getName());
+    
+    public FreeMarkerGenerator() {
+        super(NAME);
+        //System.out.println("=========== Initializing OFBiz FreeMarkerGenerator ===========");
+    }
+    
+    /*
+     * @see Generator#generate(Page, JPublishContext)
+     */
+    public void generate(JPublishContext inContext, Configuration inConfig, OutputStream inOutput) throws Throwable {
+        Writer out = new OutputStreamWriter(inOutput);
+        Page page = (Page) inContext.get("page");
+
+        //System.out.println("=========== Running generate ===========");
+        
+        //First we let the child generator work
+        InputStream in = null;
+        if (inConfig != null && inConfig.getChild("generator") != null) {
+            //Seems like we should evaluate a filter approach to improve performance here with Velocity
+            ByteArrayOutputStream tmpout = new ByteArrayOutputStream();
+            subGenerate(inContext, inConfig, tmpout);
+            in = new ByteArrayInputStream(tmpout.toByteArray());
+        } else {
+            Repository notwrapped = page.getRepository();
+            in = notwrapped.getInputStream(page.getContentPath());
+        }
+        
+        try {
+            //should cache this I guess
+            /*String template = inConfig.getChildValue("template");
+             */
+            Template parsedTemplate = new Template(page.getContentFile().getAbsolutePath(), new FileReader(page.getContentFile()));
+            Writer writer = new OutputStreamWriter(inOutput ,"UTF-8");
+            
+            try {
+                Map map = new JPublishContextMap(inContext);
+                SimpleHash root = new SimpleHash();
+                root.put("Static", BeansWrapper.getDefaultInstance().getStaticModels());
+
+                //TODO: should make the config file tell us what TemplateModel to create
+                for (Iterator iter = map.entrySet().iterator(); iter.hasNext();) {
+                    Map.Entry entry = (Map.Entry) iter.next();
+                    String key = (String) entry.getKey();
+                    root.put(key, BeansWrapper.getDefaultInstance().wrap(inContext.get(key)));
+                    //System.out.println("==== Adding to the freemarker root " + key + ":" + inContext.get(key));
+                }
+                /*
+                DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+                Document doc = docBuilder.parse(in);
+
+                TemplateModel document = new DOMNodeModel(doc);
+                root.put("document", document);
+                 */
+                parsedTemplate.process(root, writer);
+                writer.flush();
+            } finally {
+                writer.close();
+            }
+            
+            
+            //parsedTemplate = getConfiguration().getTemplate(templateFile.getName(), templateEncoding);
+        } catch (IOException ioe) {
+            throw new WSPException(ioe);
+        }
+        
+    }
+    
+    protected  freemarker.template.Configuration getConfiguration() throws IOException {
+        freemarker.template.Configuration config = freemarker.template.Configuration.getDefaultConfiguration();
+        config.setDirectoryForTemplateLoading(getSiteContext().getRealPageRoot());
+        return config;
+    }
+}
---
+++ core/src/webapp/org/ofbiz/core/ftl/FreeMarkerViewHandler.java
@@ -0,0 +1,146 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2001, 2002 The Open For Business Project - www.ofbiz.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package org.ofbiz.core.ftl;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import org.ofbiz.core.util.*;
+import org.ofbiz.core.view.*;
+
+import freemarker.ext.beans.BeanModel;
+import freemarker.ext.beans.BeansWrapper;
+import freemarker.ext.servlet.HttpRequestHashModel;
+import freemarker.ext.servlet.HttpRequestParametersHashModel;
+import freemarker.ext.servlet.HttpSessionHashModel;
+import freemarker.ext.servlet.ServletContextHashModel;
+import freemarker.template.Configuration;
+import freemarker.template.ObjectWrapper;
+import freemarker.template.SimpleHash;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+/**
+ * FreemarkerViewHandler - Freemarker Template Engine View Handler
+ *
+ * @author     <a href="mailto:jaz@ofbiz.org">Andy Zeneski</a>
+ * @version    $Revision$
+ * @since      2.1
+ */
+public class FreeMarkerViewHandler implements ViewHandler {
+    
+    public static final String module = FreemarkerViewHandler.class.getName();
+    
+    protected ServletContext servletContext = null;
+    protected Configuration config = null;
+
+    public void init(ServletContext context) throws ViewHandlerException {
+        this.servletContext = context;
+        config = Configuration.getDefaultConfiguration();
+        try {        
+            config.setDirectoryForTemplateLoading(new File(context.getRealPath("/")));
+        } catch (IOException e) {
+            Debug.logError(e, "Problems creating file for root path.", module);
+        }
+    }    
+    
+    public void render(String name, String page, String info, String contentType, String encoding, 
+            HttpServletRequest request, HttpServletResponse response) throws ViewHandlerException {                
+        if (page == null || page.length() == 0) 
+            throw new ViewHandlerException("Invalid template source");
+        
+        // make the root context (data model) for freemarker            
+        SimpleHash root = prepOfbizRoot(request, response, servletContext);
+        
+        // get the template
+        Template template = null;
+        try {
+            template = config.getTemplate(page, request.getLocale());
+        } catch (IOException e) {
+            throw new ViewHandlerException("Cannot open template file: " + page, e);
+        }
+        
+        // process the template & flush the output
+        try {
+            template.process(root, response.getWriter());
+            response.flushBuffer();
+        } catch (TemplateException te) {
+            throw new ViewHandlerException("Problems processing Freemarker template", te);
+        } catch (IOException ie) {
+            throw new ViewHandlerException("Problems writing to output stream", ie);
+        }       
+    }
+    
+    public static SimpleHash prepOfbizRoot(HttpServletRequest request, HttpServletResponse response, ServletContext servletContext) {
+        SimpleHash root = new SimpleHash();
+        
+        root.put("Static", BeansWrapper.getDefaultInstance().getStaticModels());
+
+        // add in the OFBiz objects
+        BeanModel delegatorModel = new BeanModel(request.getAttribute("delegator"), BeansWrapper.getDefaultInstance());
+        root.put("delegator", delegatorModel);
+        
+        BeanModel dispatcherModel = new BeanModel(request.getAttribute("dispatcher"), BeansWrapper.getDefaultInstance());
+        root.put("dispatcher", dispatcherModel);
+        
+        BeanModel securityModel = new BeanModel(request.getAttribute("security"), BeansWrapper.getDefaultInstance());
+        root.put("security", securityModel);
+        
+        BeanModel userLoginModel = new BeanModel(request.getAttribute("userLogin"), BeansWrapper.getDefaultInstance());
+        root.put("userLogin", userLoginModel);    
+        
+        // add the response object (for transforms) to the context as a BeanModel
+        BeanModel responseBModel = new BeanModel(response, BeansWrapper.getDefaultInstance());
+        root.put("responseBean", responseBModel);
+        
+        // add the request object (for transforms) to the context as a BeanModel
+        BeanModel requestBModel = new BeanModel(request, BeansWrapper.getDefaultInstance());
+        root.put("requestBean", requestBModel);        
+                
+        // add the servlet context    
+        ServletContextHashModel servletContextModel = new ServletContextHashModel(servletContext, ObjectWrapper.SIMPLE_WRAPPER);     
+        root.put("application", servletContextModel);
+
+        // add the session
+        HttpSession session = request.getSession();
+        HttpSessionHashModel sessionModel = new HttpSessionHashModel(session, ObjectWrapper.SIMPLE_WRAPPER);           
+        root.put("session", sessionModel);
+
+        // add the request
+        HttpRequestHashModel requestModel = new HttpRequestHashModel(request, ObjectWrapper.SIMPLE_WRAPPER);            
+        root.put("request", requestModel);
+
+        // add the request parameters                        
+        HttpRequestParametersHashModel requestParametersModel = new HttpRequestParametersHashModel(request);
+        root.put("requestParameters", requestParametersModel);
+        
+        // add the OFBiz transforms
+        root.put("ofbizUrl", new OFBizUrl());
+
+        return root;
+    }
+}
