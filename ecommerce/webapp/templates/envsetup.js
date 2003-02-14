202dc0859ee29547d2c966d84731455bdc13c4e1
jonesde <>
2003-02-14 10:08:37 +0000

    Some major progress, but still not completely working, mainly problems with including JSPs

--- ecommerce/webapp/templates/envsetup.js
+++ ecommerce/webapp/templates/envsetup.js
@@ -1,7 +1,9 @@
-importPackage(Packages.org.ofbiz.core.util);
 importPackage(Packages.java.lang);
 importPackage(Packages.java.net);
 importPackage(Packages.java.util);
+importPackage(Packages.org.ofbiz.core.util);
+importPackage(Packages.org.ofbiz.core.entity);
+importPackage(Packages.org.ofbiz.commonapp.product.catalog);
 
 var userLogin = session.getAttribute(SiteDefs.USER_LOGIN);
 if (userLogin != null) request.setAttribute("userLogin", userLogin);
@@ -15,3 +17,11 @@ layoutSettings.put("companySubtitle", UtilProperties.getPropertyValue(ecommerceP
 layoutSettings.put("headerImageUrl", UtilProperties.getPropertyValue(ecommercePropertiesUrl, "header.image.url"));
 layoutSettings.put("headerMiddleBackgroundUrl", UtilProperties.getPropertyValue(ecommercePropertiesUrl, "header.middle.background.url"));
 layoutSettings.put("headerRightBackgroundUrl", UtilProperties.getPropertyValue(ecommercePropertiesUrl, "header.right.background.url"));
+
+var prodCatalog = CatalogWorker.getProdCatalog(request);
+if (prodCatalog != null) {
+    var catalogStyleSheet = prodCatalog.get("styleSheet");
+    if (catalogStyleSheet != null) request.setAttribute("catalogStyleSheet", catalogStyleSheet);
+    var catalogHeaderLogo = prodCatalog.get("headerLogo");
+    if (catalogHeaderLogo != null) request.setAttribute("catalogHeaderLogo", catalogHeaderLogo);
+}
