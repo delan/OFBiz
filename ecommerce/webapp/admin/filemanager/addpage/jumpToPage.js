f48acd93bec4d7f43ac219e2d3691509537cf0bb
jonesde <>
2003-02-26 00:12:26 +0000

    Removing WSP admin files, need too many changes and will work better in a separate application, like the Content Manager; will work through file references in other webapps

--- ecommerce/webapp/admin/filemanager/addpage/jumpToPage.js
+++
@@ -1,6 +0,0 @@
-importPackage( Packages.java.lang );
-
-var dest = request.getParameter( "destinationPath" );
-System.out.println( "Sending : " + dest );
-
-context.put("redirect", dest);
--- ecommerce/webapp/admin/logic/logout.js
+++
@@ -1,7 +0,0 @@
-// FIXME: I don't think these first two are necessary anymore.
-
-session.removeAttribute("admin")
-session.removeAttribute("account")
-session.removeAttribute("editMode")
-session.removeAttribute("username")
-url_util.redirect("/")
