de5dc8d53eb59d00d1d91412313d2039e0bbf5be
azeneski <>
2003-03-01 01:11:57 +0000

    working on the new view renderer

--- core/src/webapp/org/ofbiz/core/view/GenericViewRenderer.java
+++ core/src/webapp/org/ofbiz/core/view/GenericViewRenderer.java
@@ -24,6 +24,8 @@
  */
 package org.ofbiz.core.view;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -31,18 +33,27 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpSession;
 
 import org.jpublish.JPublishContext;
 import org.jpublish.Page;
+import org.jpublish.Repository;
 import org.jpublish.SiteContext;
+import org.jpublish.page.PageInstance;
 import org.jpublish.view.ViewRenderException;
 import org.jpublish.view.ViewRenderer;
+import org.ofbiz.core.entity.GenericValue;
 import org.ofbiz.core.ftl.FreeMarkerViewRenderer;
+import org.ofbiz.core.util.Debug;
 
 import com.anthonyeden.lib.config.Configuration;
 import com.anthonyeden.lib.config.ConfigurationException;
+import com.anthonyeden.lib.config.XMLConfiguration;
+import com.anthonyeden.lib.util.IOUtilities;
 
 /**
  * Generic JPublish View Renderer - This is in testing; for use in wrapping other renderers
@@ -53,24 +64,29 @@ import com.anthonyeden.lib.config.ConfigurationException;
  */
 public class GenericViewRenderer implements ViewRenderer {
         
-    public static final String module = GenericViewRenderer.class.getName();        
+    public static final String module = GenericViewRenderer.class.getName();  
+    public static final String DEFAULT_RENDERER = "freemarker";
+    public Map renderers = null;      
         
-    protected SiteContext siteContext;
+    protected SiteContext siteContext = null;
 
     /**
      * @see org.jpublish.view.ViewRenderer#setSiteContext(org.jpublish.SiteContext)
      */
     public void setSiteContext(SiteContext siteContext) {
         this.siteContext = siteContext;
-        
+        this.renderers = new HashMap();
+        try {
+            loadCustom();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }                
     }
 
     /* (non-Javadoc)
      * @see org.jpublish.view.ViewRenderer#init()
      */
-    public void init() throws Exception {
-        // TODO Auto-generated method stub
-        
+    public void init() throws Exception {                         
     }
 
     /**
@@ -78,15 +94,19 @@ public class GenericViewRenderer implements ViewRenderer {
      */
     public void render(JPublishContext context, String path, Reader in, Writer out) throws IOException, ViewRenderException {
         HttpSession session = context.getSession();
-        Page page = context.getPage();
-        // locate the page renderer from page.getProperty("page-renderer");
-        // load the renderer class; somehow this needs to be mapped name/class
-        FreeMarkerViewRenderer renderer = new FreeMarkerViewRenderer(); // this is a test
-        renderer.setSiteContext(siteContext);
+        GenericValue userLogin = (GenericValue) session.getAttribute("userLogin");
         
-        // do some user/security checking for page edits       
-        //out.write("<b>" + page.getPath()  + "</b>");   // write the page editor links, etc
+        if (Debug.verboseOn()) Debug.logVerbose("Getting renderer for: " + path, module);
+        // locate the page renderer from page.getProperty("page-renderer");        
+        ViewRenderer renderer = getPathRenderer(path);        
         
+        Debug.logVerbose("Checking security", module);
+        // do some user/security checking for page edits -- this test just checks for admin
+        if (userLogin != null && userLogin.getString("userLoginId").equals("admin")) {    
+            out.write("<a href='/content/control/editContent?filePath=" + path + "'>*</a>");
+        }
+        
+        Debug.logVerbose("Calling render");
         // call the renderer to render the rest of the page.
         renderer.render(context, path, in, out);                   
     }
@@ -98,12 +118,42 @@ public class GenericViewRenderer implements ViewRenderer {
         render(context, path, new InputStreamReader(in), new OutputStreamWriter(out));               
     }
 
-    /* (non-Javadoc)
+    /**
      * @see org.jpublish.view.ViewRenderer#loadConfiguration(com.anthonyeden.lib.config.Configuration)
      */
-    public void loadConfiguration(Configuration config) throws ConfigurationException {
-        // TODO Auto-generated method stub
-        
+    public void loadConfiguration(Configuration config) throws ConfigurationException {               
     }
+    
+    private ViewRenderer getPathRenderer(String path) throws ViewRenderException {
+        String rendererName = DEFAULT_RENDERER;
+        try {        
+            PageInstance page = siteContext.getPageManager().getPage(path.substring(path.lastIndexOf(":")));
+            rendererName = page.getProperty("page-renderer");
+        } catch (Exception e) {}
+        //Debug.logInfo("Using renderer: " + rendererName, module);
+        if (rendererName == null) {
+            return (ViewRenderer) renderers.get(DEFAULT_RENDERER);
+        } else {
+            return (ViewRenderer) renderers.get(rendererName);
+        }       
+    }
+    
+    private void loadCustom() throws Exception {
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        InputStream in = new FileInputStream(siteContext.getConfigurationFile());
+        Configuration configuration = new XMLConfiguration(in);
         
+        Iterator renderElements = configuration.getChildren("page-renderer").iterator();
+        while (renderElements.hasNext()) {
+            Configuration viewRendererConfiguration = (Configuration) renderElements.next();
+            String renderName = viewRendererConfiguration.getAttribute("name");
+            String className = viewRendererConfiguration.getAttribute("classname");
+            ViewRenderer renderer = (ViewRenderer) cl.loadClass(className).newInstance();
+            renderer.setSiteContext(siteContext);
+            renderer.loadConfiguration(viewRendererConfiguration);
+            renderer.init();
+            Debug.logInfo("Added renderer [" + renderName + "] - [" + className + "]", module);
+            renderers.put(renderName, renderer);           
+        }                   
+    }       
 }
