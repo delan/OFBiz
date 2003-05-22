d2a681941f39fb7c31530cb304fe8ed44e3dedeb
jonesde <>
2003-05-22 03:27:36 +0000

    Added fix for condition expression bug found by Tristan Austin

--- commonapp/src/org/ofbiz/commonapp/security/login/LoginServices.java
+++ commonapp/src/org/ofbiz/commonapp/security/login/LoginServices.java
@@ -332,7 +332,7 @@ public class LoginServices {
 
         // security: don't create a user login if the specified partyId (if not empty) already exists
         // unless the logged in user has permission to do so (same partyId or PARTYMGR_CREATE)
-        if (partyId != null || partyId.length() > 0) {
+        if (partyId != null && partyId.length() > 0) {
             GenericValue party = null;
 
             try {
