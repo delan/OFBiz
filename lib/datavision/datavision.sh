15ad9a741414cd8e6e4fda6ac733674ec0c83fb9
jonesde <>
2003-05-26 19:03:38 +0000

    Updated to DataVision 0.7.8, figured it was about time to update.

--- lib/datavision/datavision.sh
+++ lib/datavision/datavision.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-classpath="$CLASSPATH":DataVision.jar:MinML.jar:jcalendar.jar
+classpath="$CLASSPATH":lib/DataVision.jar:lib/MinML2.jar:lib/jcalendar.jar:lib/jruby.jar:lib/gnu-regexp-1.1.4.jar
 
 cd `dirname $0`
 java -classpath $classpath jimm.datavision.DataVision $*
