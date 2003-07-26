415d7505eeabbe0702f8b0f488fa35765a8da99a
jonesde <>
2003-07-26 02:17:54 +0000

    Skeleton for UPS confirm and accept services; mostly gathered example XML and code data

---
+++ commonapp/src/org/ofbiz/commonapp/thirdparty/ups/UpsServices.java
@@ -0,0 +1,359 @@
+/*
+ * $Id$
+ *
+ *  Copyright (c) 2003 The Open For Business Project - www.ofbiz.org
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ *  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ *  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ofbiz.commonapp.thirdparty.ups;
+
+import java.util.*;
+
+import org.ofbiz.core.entity.*;
+import org.ofbiz.core.service.*;
+import org.ofbiz.core.util.*;
+
+/**
+ * ShipmentServices
+ *
+ * @author     <a href="mailto:jaz@ofbiz.org">Andy Zeneski</a> 
+ * @version    $Revision$
+ * @since      2.0
+ */
+public class UpsServices {
+
+    public static Map upsShipmentConfirm(DispatchContext dctx, Map context) {
+        Map result = new HashMap();
+        GenericDelegator delegator = dctx.getDelegator();
+        String shipmentId = (String) context.get("shipmentId");
+
+        try {
+            GenericValue shipment = delegator.findByPrimaryKey("Shipment", UtilMisc.toMap("shipmentId", shipmentId));
+        } catch (GenericEntityException e) {
+            Debug.logError(e);
+            ServiceUtil.returnError("Error reading or writing Shipment data for UPS Shipment Confirm: " + e.toString());
+        }
+
+        result.put(ModelService.RESPONSE_MESSAGE, ModelService.RESPOND_SUCCESS);
+        return result;
+    }
+    
+    public static Map upsShipmentAccept(DispatchContext dctx, Map context) {
+        Map result = new HashMap();
+        GenericDelegator delegator = dctx.getDelegator();
+        String shipmentId = (String) context.get("shipmentId");
+
+        try {
+            GenericValue shipment = delegator.findByPrimaryKey("Shipment", UtilMisc.toMap("shipmentId", shipmentId));
+        } catch (GenericEntityException e) {
+            Debug.logError(e);
+            ServiceUtil.returnError("Error reading or writing Shipment data for UPS Shipment Accept: " + e.toString());
+        }
+
+        result.put(ModelService.RESPONSE_MESSAGE, ModelService.RESPOND_SUCCESS);
+        return result;
+    }
+}
+
+/*
+ * UPS Code Reference
+
+Package Type Code
+00 Unknown
+01 UPS Letter
+02 Package
+03 UPS Tube
+04 UPS Pak
+21 UPS Express Box
+24 UPS 25KG Box
+25 UPS 10KG Box
+
+Pickup Types
+01 Daily Pickup
+03 Customer Counter
+06 One Time Pickup
+07 On Call Air Pickup
+19 Letter Center
+20 Air Service Center
+
+UPS Service Codes
+US Origin
+01 UPS Next Day Air
+02 UPS 2nd Day Air
+03 UPS Ground
+07 UPS Worldwide Express
+08 UPS Worldwide Expedited
+11 UPS Standard
+12 UPS 3-Day Select
+13 UPS Next Day Air Saver
+14 UPS Next Day Air Early AM
+54 UPS Worldwide Express Plus
+59 UPS 2nd Day Air AM
+64 N/A
+65 UPS Express Saver
+
+Reference Number Codes
+AJ Acct. Rec. Customer Acct.
+AT Appropriation Number
+BM Bill of Lading Number
+9V COD Number
+ON Dealer Order Number
+DP Department Number
+EI Employer's ID Number
+3Q FDA Product Code
+TJ Federal Taxpayer ID Number
+IK Invoice Number
+MK Manifest Key Number
+MJ Model Number
+PM Part Number
+PC Production Code
+PO Purchase Order No.
+RQ Purchase Request No.
+RZ Return Authorization No.
+SA Salesperson No.
+SE Serial No.
+SY Social Security No.
+ST Store No.
+TN Transaction Ref. No. 
+
+ */
+
+
+/*
+ * Sample XML documents:
+ *  
+<?xml version="1.0"?>
+<AccessRequest xml:lang="en-US">
+   <AccessLicenseNumber>TEST262223144CAT</AccessLicenseNumber>
+   <UserId>REG111111</UserId>
+   <Password>REG111111</Password>
+</AccessRequest>
+
+=======================================
+Shipment Confirm Request/Response
+=======================================
+
+<?xml version="1.0"?>
+<ShipmentConfirmRequest xml:lang="en-US">
+    <Request>
+        <TransactionReference>
+            <CustomerContext>Ship Confirm / nonvalidate</CustomerContext>
+            <XpciVersion>1.0001</XpciVersion>
+        </TransactionReference>
+        <RequestAction>ShipConfirm</RequestAction>
+        <RequestOption>nonvalidate</RequestOption>
+    </Request>
+    <LabelSpecification>
+        <LabelPrintMethod>
+            <Code>GIF</Code>
+        </LabelPrintMethod>
+        <HTTPUserAgent>Mozilla/5.0</HTTPUserAgent>
+        <LabelImageFormat>
+            <Code>GIF</Code>
+        </LabelImageFormat>
+    </LabelSpecification>
+    <Shipment>
+        <Description>DescriptionofGoodsTest</Description>
+        <Shipper>
+            <Name>ShipperName</Name>
+            <AttentionName>ShipperName</AttentionName>
+            <PhoneNumber>2226267227</PhoneNumber>
+            <ShipperNumber>12345E</ShipperNumber>
+            <Address>
+                <AddressLine1>123 ShipperStreet</AddressLine1>
+                <City>ShipperCity</City>
+                <PostalCode>03570</PostalCode>
+                <CountryCode>DE</CountryCode>
+            </Address>
+        </Shipper>
+        <ShipTo>
+            <CompanyName>ShipToCompanyName</CompanyName>
+            <AttentionName>ShipToAttnName</AttentionName>
+            <PhoneNumber>3336367336</PhoneNumber>
+            <Address>
+                <AddressLine1>123 ShipToStreet</AddressLine1>
+                <PostalCode>DT09</PostalCode>
+                <City>Trent</City>
+                <CountryCode>GB</CountryCode>
+            </Address>
+        </ShipTo>
+        <ShipFrom>
+            <CompanyName>ShipFromCompanyName</CompanyName>
+            <AttentionName>ShipFromAttnName</AttentionName>
+            <PhoneNumber>7525565064</PhoneNumber>
+            <Address>
+                <AddressLine1>123 ShipFromStreet</AddressLine1>
+                <City>Berlin</City>
+                <PostalCode>03570</PostalCode>
+                <CountryCode>DE</CountryCode>
+            </Address>
+        </ShipFrom>
+        <PaymentInformation>
+            <Prepaid>
+                <BillShipper>
+                    <AccountNumber>12345E</AccountNumber>
+                </BillShipper>
+            </Prepaid>
+        </PaymentInformation>
+        <Service>
+            <Code>07</Code>
+        </Service>
+        <Package>
+            <PackagingType>
+                <Code>02</Code>
+            </PackagingType>
+            <Dimensions>
+                <UnitOfMeasurement>
+                    <Code>CM</Code>
+                </UnitOfMeasurement>
+                <Length>60</Length>
+                <Width>7</Width>
+                <Height>5</Height>
+            </Dimensions>
+            <PackageWeight>
+                <UnitOfMeasurement>
+                    <Code>KGS</Code>
+                </UnitOfMeasurement>
+                <Weight>3.0</Weight>
+            </PackageWeight>
+        </Package>
+    </Shipment>
+</ShipmentConfirmRequest>
+
+=======================================
+
+<?xml version="1.0"?>
+<ShipmentConfirmResponse>
+    <Response>
+        <TransactionReference>
+            <CustomerContext>ShipConfirmUS</CustomerContext>
+            <XpciVersion>1.0001</XpciVersion>
+        </TransactionReference>
+        <ResponseStatusCode>1</ResponseStatusCode>
+        <ResponseStatusDescription>Success</ResponseStatusDescription>
+    </Response>
+    <ShipmentCharges>
+        <TransportationCharges>
+            <CurrencyCode>USD</CurrencyCode>
+            <MonetaryValue>31.38</MonetaryValue>
+        </TransportationCharges>
+        <ServiceOptionsCharges>
+            <CurrencyCode>USD</CurrencyCode>
+            <MonetaryValue>7.75</MonetaryValue>
+        </ServiceOptionsCharges>
+        <TotalCharges>
+            <CurrencyCode>USD</CurrencyCode>
+            <MonetaryValue>39.13</MonetaryValue>
+        </TotalCharges>
+    </ShipmentCharges>
+    <BillingWeight>
+        <UnitOfMeasurement>
+            <Code>LBS</Code>
+        </UnitOfMeasurement>
+        <Weight>4.0</Weight>
+    </BillingWeight>
+    <ShipmentIdentificationNumber>1Z12345E1512345676</ShipmentIdentificationNumber>
+    <ShipmentDigest>INSERT SHIPPING DIGEST HERE</ShipmentDigest>
+</ShipmentConfirmResponse>
+
+=======================================
+Shipment Accept Request/Response
+=======================================
+
+<?xml version="1.0"?>
+<ShipmentAcceptRequest>
+    <Request>
+        <TransactionReference>
+            <CustomerContext>TR01</CustomerContext>
+            <XpciVersion>1.0001</XpciVersion>
+        </TransactionReference>
+        <RequestAction>ShipAccept</RequestAction>
+        <RequestOption>01</RequestOption>
+    </Request>
+    <ShipmentDigest>INSERT SHIPPING DIGEST HERE</ShipmentDigest>
+</ShipmentAcceptRequest>
+
+=======================================
+
+<?xml version="1.0"?>
+<ShipmentAcceptResponse>
+    <Response>
+        <TransactionReference>
+            <CustomerContext>TR01</CustomerContext>
+            <XpciVersion>1.0001</XpciVersion>
+        </TransactionReference>
+        <ResponseStatusCode>1</ResponseStatusCode>
+        <ResponseStatusDescription>Success</ResponseStatusDescription>
+    </Response>
+    <ShipmentResults>
+        <ShipmentCharges>
+            <TransportationCharges>
+                <CurrencyCode>USD</CurrencyCode>
+                <MonetaryValue>31.38</MonetaryValue>
+            </TransportationCharges>
+            <ServiceOptionsCharges>
+                <CurrencyCode>USD</CurrencyCode>
+                <MonetaryValue>7.75</MonetaryValue>
+            </ServiceOptionsCharges>
+            <TotalCharges>
+                <CurrencyCode>USD</CurrencyCode>
+                <MonetaryValue>39.13</MonetaryValue>
+            </TotalCharges>
+        </ShipmentCharges>
+        <BillingWeight>
+            <UnitOfMeasurement>
+                <Code>LBS</Code>
+            </UnitOfMeasurement>
+            <Weight>4.0</Weight>
+        </BillingWeight>
+        <ShipmentIdentificationNumber>1Z12345E1512345676</ShipmentIdentificationNumber>
+        <PackageResults>
+            <TrackingNumber>1Z12345E1512345676</TrackingNumber>
+            <ServiceOptionsCharges>
+                <CurrencyCode>USD</CurrencyCode>
+                <MonetaryValue>0.00</MonetaryValue>
+            </ServiceOptionsCharges>
+            <LabelImage>
+                <LabelImageFormat>
+                    <Code>epl</Code>
+                </LabelImageFormat>
+                <GraphicImage>INSERT GRAPHIC IMAGE HERE</GraphicImage>
+            </LabelImage>
+        </PackageResults>
+        <PackageResults>
+            <TrackingNumber>1Z12345E1512345686</TrackingNumber>
+            <ServiceOptionsCharges>
+                <CurrencyCode>USD</CurrencyCode>
+                <MonetaryValue>7.75</MonetaryValue>
+            </ServiceOptionsCharges>
+            <LabelImage>
+                <LabelImageFormat>
+                    <Code>epl</Code>
+                </LabelImageFormat>
+                <GraphicImage>INSERT GRAPHIC IMAGE HERE</GraphicImage>
+            </LabelImage>
+        </PackageResults>
+    </ShipmentResults>
+</ShipmentAcceptResponse>
+
+
+ */
+
+ 
+ 
