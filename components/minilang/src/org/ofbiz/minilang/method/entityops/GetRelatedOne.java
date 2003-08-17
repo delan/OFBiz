a586165648298253c2a95037f473bd7c5e52ff9f
ajzeneski <ajzeneski@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2003-08-17 06:06:14 +0000

    added minilang component
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@37 deaff617-5be1-0310-a1b7-ec6878f92ba9

---
+++ components/minilang/src/org/ofbiz/minilang/method/entityops/GetRelatedOne.java
@@ -0,0 +1,83 @@
+/*
+ * $Id: GetRelatedOne.java,v 1.1 2003/08/17 06:06:11 ajzeneski Exp $
+ *
+ *  Copyright (c) 2001, 2002 The Open For Business Project - www.ofbiz.org
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ *  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ *  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ofbiz.minilang.method.entityops;
+
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.minilang.SimpleMethod;
+import org.ofbiz.minilang.method.ContextAccessor;
+import org.ofbiz.minilang.method.MethodContext;
+import org.ofbiz.minilang.method.MethodOperation;
+import org.w3c.dom.Element;
+
+/**
+ * Gets a list of related entity instance according to the specified relation-name
+ *
+ * @author     <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
+ * @version    $Revision: 1.1 $
+ * @since      2.0
+ */
+public class GetRelatedOne extends MethodOperation {
+    
+    public static final String module = GetRelatedOne.class.getName();
+    
+    ContextAccessor valueAcsr;
+    ContextAccessor toValueAcsr;
+    String relationName;
+    String useCacheStr;
+
+    public GetRelatedOne(Element element, SimpleMethod simpleMethod) {
+        super(element, simpleMethod);
+        valueAcsr = new ContextAccessor(element.getAttribute("value-name"));
+        toValueAcsr = new ContextAccessor(element.getAttribute("to-value-name"));
+        relationName = element.getAttribute("relation-name");
+        useCacheStr = element.getAttribute("use-cache");
+    }
+
+    public boolean exec(MethodContext methodContext) {
+        String relationName = methodContext.expandString(this.relationName);
+        String useCacheStr = methodContext.expandString(this.useCacheStr);
+        boolean useCache = "true".equals(useCacheStr);
+
+        GenericValue value = (GenericValue) valueAcsr.get(methodContext);
+        if (value == null) {
+            Debug.logWarning("Value not found with name: " + valueAcsr + ", not getting related...", module);
+            return true;
+        }
+        try {
+            if (useCache) {
+                toValueAcsr.put(methodContext, value.getRelatedOneCache(relationName));
+            } else {
+                toValueAcsr.put(methodContext, value.getRelatedOne(relationName));
+            }
+        } catch (GenericEntityException e) {
+            String errMsg = "ERROR: Could not complete the " + simpleMethod.getShortDescription() + " process [problem getting related one from entity with name " + value.getEntityName() + " for the relation-name: " + relationName + ": " + e.getMessage() + "]";
+            Debug.logError(e, errMsg, module);
+            methodContext.setErrorReturn(errMsg, simpleMethod);
+            return false;
+        }
+        return true;
+    }
+}
---
+++ components/minilang/src/org/ofbiz/minilang/method/ifops/IfHasPermission.java
@@ -0,0 +1,105 @@
+/*
+ * $Id: IfHasPermission.java,v 1.1 2003/08/17 06:06:13 ajzeneski Exp $
+ *
+ *  Copyright (c) 2001, 2002 The Open For Business Project - www.ofbiz.org
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ *  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ *  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ofbiz.minilang.method.ifops;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.ofbiz.base.util.UtilXml;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.minilang.SimpleMethod;
+import org.ofbiz.minilang.method.MethodContext;
+import org.ofbiz.minilang.method.MethodOperation;
+import org.ofbiz.security.Security;
+import org.w3c.dom.Element;
+
+/**
+ * Iff the user has the specified permission, process the sub-operations. Otherwise
+ * process else operations if specified.
+ *
+ * @author     <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
+ * @version    $Revision: 1.1 $
+ * @since      2.0
+ */
+public class IfHasPermission extends MethodOperation {
+
+    List subOps = new LinkedList();
+    List elseSubOps = null;
+
+    String permission;
+    String action;
+
+    public IfHasPermission(Element element, SimpleMethod simpleMethod) {
+        super(element, simpleMethod);
+        this.permission = element.getAttribute("permission");
+        this.action = element.getAttribute("action");
+
+        SimpleMethod.readOperations(element, subOps, simpleMethod);
+
+        Element elseElement = UtilXml.firstChildElement(element, "else");
+        if (elseElement != null) {
+            elseSubOps = new LinkedList();
+            SimpleMethod.readOperations(elseElement, elseSubOps, simpleMethod);
+        }
+    }
+
+    public boolean exec(MethodContext methodContext) {
+        // if conditions fails, always return true; if a sub-op returns false 
+        // return false and stop, otherwise return true
+        // return true;
+
+        // only run subOps if element is empty/null
+        boolean runSubOps = false;
+
+        // if no user is logged in, treat as if the user does not have permission: do not run subops
+        GenericValue userLogin = methodContext.getUserLogin();
+        if (userLogin != null) {
+            String permission = methodContext.expandString(this.permission);
+            String action = methodContext.expandString(this.action);
+            
+            Security security = methodContext.getSecurity();
+            if (action != null && action.length() > 0) {
+                // run hasEntityPermission
+                if (security.hasEntityPermission(permission, action, userLogin)) {
+                    runSubOps = true;
+                }
+            } else {
+                // run hasPermission
+                if (security.hasPermission(permission, userLogin)) {
+                    runSubOps = true;
+                }
+            }
+        }
+
+        if (runSubOps) {
+            return SimpleMethod.runSubOps(subOps, methodContext);
+        } else {
+            if (elseSubOps != null) {
+                return SimpleMethod.runSubOps(elseSubOps, methodContext);
+            } else {
+                return true;
+            }
+        }
+    }
+}
