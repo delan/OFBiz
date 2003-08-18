4b7052487f283518e18c90acad7f8927588180f0
jonesde <>
2003-08-18 08:49:29 +0000

    Removed various resources that have been moved to the new env in common and security, and entityext

--- ofbtmp/commonapp/src/org/ofbiz/commonapp/common/JavaScriptTest.js
+++
@@ -1,9 +0,0 @@
-
-// - BSF JavaScript test script
-
-x = bsf.lookupBean("message");
-m = bsf.lookupBean("response");
-m.put("result", x);
-
-bsf.registerBean("response", m);
-java.lang.System.out.println("BSF - JS: " + x);
