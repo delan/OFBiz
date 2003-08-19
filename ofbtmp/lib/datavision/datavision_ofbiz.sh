0e811c77e97c4a9a7b2b44589f7599e3066ee3d2
azeneski <>
2003-08-19 06:35:04 +0000

    *** empty log message ***

--- ofbtmp/lib/datavision/datavision_ofbiz.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-classpath="$CLASSPATH":../common/postgresql.jar:../common/hsqldb.jar:DataVision.jar:MinML.jar:jcalendar.jar
-
-cd `dirname $0`
-$JAVA_HOME/bin/java -classpath $classpath jimm.datavision.DataVision $*
-
