da49a411f0183e03baf76cac830c71cb3ea09f23
ajzeneski <ajzeneski@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2003-08-25 20:16:12 +0000

    refactor so we can call the calc shipping without a cart (need to move to service)
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@213 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/order/src/org/ofbiz/order/shoppingcart/shipping/ShippingEvents.java
+++ components/order/src/org/ofbiz/order/shoppingcart/shipping/ShippingEvents.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ShippingEvents.java,v 1.1 2003/08/18 17:59:53 jonesde Exp $
+ * $Id: ShippingEvents.java,v 1.2 2003/08/25 20:16:12 ajzeneski Exp $
  *
  *  Copyright (c) 2001, 2002 The Open For Business Project - www.ofbiz.org
  *
@@ -39,6 +39,7 @@ import org.ofbiz.base.util.UtilValidate;
 import org.ofbiz.entity.GenericDelegator;
 import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericValue;
+import org.ofbiz.order.order.OrderReadHelper;
 import org.ofbiz.order.shoppingcart.ShoppingCart;
 import org.ofbiz.service.ModelService;
 import org.ofbiz.service.ServiceUtil;
@@ -47,7 +48,7 @@ import org.ofbiz.service.ServiceUtil;
  * ShippingEvents - Events used for processing shipping fees
  *
  * @author     <a href="mailto:jaz@ofbiz.org">Andy Zeneski</a>
- * @version    $Revision: 1.1 $
+ * @version    $Revision: 1.2 $
  * @since      2.0
  */
 public class ShippingEvents {
@@ -74,11 +75,8 @@ public class ShippingEvents {
         // all done
         return "success";
     }
+    
     public static Map getShipEstimate(GenericDelegator delegator, ShoppingCart cart, String shippingMethod) {
-        String standardMessage = "A problem occurred calculating shipping. Fees will be calculated offline.";
-        List errorMessageList = new ArrayList();                             
-        StringBuffer errorMessage = new StringBuffer();
-        
         String shipmentMethodTypeId = null;
         String carrierPartyId = null;
         if (UtilValidate.isNotEmpty(shippingMethod)) {
@@ -90,26 +88,49 @@ public class ShippingEvents {
         } else {
             shipmentMethodTypeId = cart.getShipmentMethodTypeId();
             carrierPartyId = cart.getCarrierPartyId();            
-        }        
-                       
+        }         
+        return getShipEstimate(delegator, cart.getOrderType(), shipmentMethodTypeId, carrierPartyId, cart.getShippingContactMechId(), cart.getProductStoreId(), cart.getShippableWeight(), cart.getShippableQuantity(), cart.getShippableTotal());                               
+    }
+    
+    public static Map getShipEstimate(GenericDelegator delegator, OrderReadHelper orh) {
+        String shippingMethod = orh.getShippingMethodCode();
+        String shipmentMethodTypeId = null;
+        String carrierPartyId = null;
+        if (UtilValidate.isNotEmpty(shippingMethod)) {
+            int delimiterPos = shippingMethod.indexOf('@');                        
+            if (delimiterPos > 0) {
+                shipmentMethodTypeId = shippingMethod.substring(0, delimiterPos);
+                carrierPartyId = shippingMethod.substring(delimiterPos + 1);
+            }                      
+        }
+        GenericValue shipAddr = orh.getShippingAddress();
+        String contactMechId = shipAddr.getString("contactMechId");    
+        return getShipEstimate(delegator, orh.getOrderTypeId(), shipmentMethodTypeId, carrierPartyId, contactMechId, orh.getProductStoreId(), orh.getShippableWeight(), orh.getShippableQuantity(), orh.getShippableTotal());    
+    }
+    
+    public static Map getShipEstimate(GenericDelegator delegator, String orderTypeId, String shipmentMethodTypeId, String carrierPartyId, String shippingContactMechId, String productStoreId, double shippableWeight, double shippableQuantity, double shippableTotal) {
+        String standardMessage = "A problem occurred calculating shipping. Fees will be calculated offline.";
+        List errorMessageList = new ArrayList();                             
+        StringBuffer errorMessage = new StringBuffer();
+                                               
         if (shipmentMethodTypeId == null || carrierPartyId == null) {
-            if (cart.getOrderType().equals("SALES_ORDER")) {
+            if ("SALES_ORDER".equals(orderTypeId)) {
                 errorMessageList.add("Please Select Your Shipping Method.");
                 return ServiceUtil.returnError(errorMessageList);                
             } else {
                 return ServiceUtil.returnSuccess();
             }
         }
-        
-        String shippingContactMechId = cart.getShippingContactMechId();
+                
         if (shippingContactMechId == null) {
             errorMessageList.add("Please Select Your Shipping Address.");
             return ServiceUtil.returnError(errorMessageList);
         }
         
-        // get the store        
-        String productStoreId = cart.getProductStoreId();
-
+        Debug.log("Shippable Weight : " + shippableWeight, module);
+        Debug.log("Shippable Qty : " + shippableQuantity, module);
+        Debug.log("Shippable Total : " + shippableTotal, module);
+        
         // Get the ShipmentCostEstimate(s)
         Collection estimates = null;
 
@@ -117,8 +138,10 @@ public class ShippingEvents {
             Map fields = UtilMisc.toMap("productStoreId", productStoreId, "shipmentMethodTypeId", shipmentMethodTypeId, "carrierPartyId", carrierPartyId, "carrierRoleTypeId", "CARRIER");
 
             estimates = delegator.findByAnd("ShipmentCostEstimate", fields);
+            Debug.set(Debug.VERBOSE, true);
             if (Debug.verboseOn()) Debug.logVerbose("Estimate fields: " + fields, module);
             if (Debug.verboseOn()) Debug.logVerbose("Estimate(s): " + estimates, module);
+            Debug.set(Debug.VERBOSE, false);
         } catch (GenericEntityException e) {
             Debug.logError("[ShippingEvents.getShipEstimate] Cannot get shipping estimates.", module);            
             return ServiceUtil.returnSuccess(standardMessage);                 
@@ -139,11 +162,6 @@ public class ShippingEvents {
             Debug.logError("[ShippingEvents.getShipEstimate] Cannot get shipping address entity.", module);                   
             return ServiceUtil.returnSuccess(standardMessage);
         }
-
-        // Get some needed data from the cart.
-        double cartWeight = cart.getShippableWeight();
-        double cartQty = cart.getShippableQuantity();
-        double cartTotal = cart.getShippableTotal();
 
         // Get the possible estimates.
         ArrayList estimateList = new ArrayList();
@@ -190,7 +208,7 @@ public class ShippingEvents {
                             min = wv.getDouble("fromQuantity").doubleValue();
                             max = wv.getDouble("thruQuantity").doubleValue();
                         } catch (Exception e) {}
-                        if (cartWeight >= min && (max == 0 || cartWeight <= max))
+                        if (shippableWeight >= min && (max == 0 || shippableWeight <= max))
                             weightValid = true;
                     }
                     if (qv != null) {
@@ -202,7 +220,7 @@ public class ShippingEvents {
                             min = qv.getDouble("fromQuantity").doubleValue();
                             max = qv.getDouble("thruQuantity").doubleValue();
                         } catch (Exception e) {}
-                        if (cartQty >= min && (max == 0 || cartQty <= max))
+                        if (shippableQuantity >= min && (max == 0 || shippableQuantity <= max))
                             qtyValid = true;
                     }
                     if (pv != null) {
@@ -214,7 +232,7 @@ public class ShippingEvents {
                             min = pv.getDouble("fromQuantity").doubleValue();
                             max = pv.getDouble("thruQuantity").doubleValue();
                         } catch (Exception e) {}
-                        if (cartTotal >= min && (max == 0 || cartTotal <= max))
+                        if (shippableTotal >= min && (max == 0 || shippableTotal <= max))
                             priceValid = true;
                     }
                     // Now check the tests.
@@ -295,12 +313,12 @@ public class ShippingEvents {
         if (estimate.getDouble("priceUnitPrice") != null)
             priceUnit = estimate.getDouble("priceUnitPrice").doubleValue();
 
-        double weightAmount = cartWeight * weightUnit;
-        double quantityAmount = cartQty * qtyUnit;
-        double priceAmount = cartTotal * priceUnit;
+        double weightAmount = shippableWeight * weightUnit;
+        double quantityAmount = shippableQuantity * qtyUnit;
+        double priceAmount = shippableTotal * priceUnit;
 
-        double itemFlatAmount = cartQty * orderItemFlat;
-        double orderPercentage = cartTotal * (orderPercent / 100);
+        double itemFlatAmount = shippableQuantity * orderItemFlat;
+        double orderPercentage = shippableTotal * (orderPercent / 100);
 
         double shippingTotal = weightAmount + quantityAmount + priceAmount + orderFlat + itemFlatAmount + orderPercentage;
 
