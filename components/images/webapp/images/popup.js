ebb923f1d33150ddcc708cbc6e3a9a9339a67e39
ajzeneski <ajzeneski@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2003-11-21 22:46:01 +0000

    removed popup.js moved to selectall.js added additional js for hidden divs, intention is to rename this file
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@935 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/images/webapp/images/popup.js
+++
@@ -1,7 +0,0 @@
-
-// popup windows functions
-
-function popUp(url, name, height, width) {
-    popupWindow = window.open(url, name, 'location=no,scrollbars,width=' + width + ',height=' + height);
-}
-
