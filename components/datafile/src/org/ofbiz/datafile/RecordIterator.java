ec9fc65492d3fff69eac6bbc0c10486ef54a8c07
jonesde <jonesde@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2003-12-03 06:09:00 +0000

    Fixed pretty bad bug, was always passing null or the wrong value when creating a record
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@1052 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/datafile/src/org/ofbiz/datafile/RecordIterator.java
+++ components/datafile/src/org/ofbiz/datafile/RecordIterator.java
@@ -1,5 +1,5 @@
 /*
- * $Id: RecordIterator.java,v 1.1 2003/12/03 05:12:10 jonesde Exp $
+ * $Id: RecordIterator.java,v 1.2 2003/12/03 06:09:00 jonesde Exp $
  *
  * Copyright (c) 2001-2003 The Open For Business Project - www.ofbiz.org
  *
@@ -37,7 +37,7 @@ import java.util.Stack;
  *  Record Iterator for reading large files
  *
  * @author     <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
- * @version    $Revision: 1.1 $
+ * @version    $Revision: 1.2 $
  * @since      3.0
  */
 
@@ -120,8 +120,8 @@ public class RecordIterator {
         
         if (nextLine != null) {
             nextLineNum++;
-            ModelRecord modelRecord = findModelForLine(curLine, nextLineNum, modelDataFile);
-            this.nextRecord = Record.createRecord(curLine, nextLineNum, modelRecord);
+            ModelRecord modelRecord = findModelForLine(nextLine, nextLineNum, modelDataFile);
+            this.nextRecord = Record.createRecord(nextLine, nextLineNum, modelRecord);
             return true;
         } else {
             this.close();
