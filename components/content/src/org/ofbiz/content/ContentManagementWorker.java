9f95072437a9c12e33aaa1cb73950575f8f668e9
byersa <byersa@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2003-12-05 21:46:52 +0000

    Issue number: Obtained from: Submitted by: Reviewed by:
    I put some services and methods that deal with both Content and DataResource
    at this level. Not happy about it, but not so sure where else to put them.
    Worker has the "most recently used" code and Services adds a
    persistContentAndAssoc service that is an improvement on another called
    createContentAndAssoc.
    The MapProcessors has some converters to take the prefixed names that come
    from views and put them in std entity format. There are ones for
    Content, DataResource and ContentAssoc.
    
    
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@1114 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/content/src/org/ofbiz/content/ContentManagementWorker.java
+++ components/content/src/org/ofbiz/content/ContentManagementWorker.java
@@ -3,19 +3,49 @@ package org.ofbiz.content;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
+import java.util.Locale;
+import java.io.IOException;
 
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.UtilDateTime;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.base.util.UtilProperties;
+import org.ofbiz.base.util.UtilValidate;
 import org.ofbiz.base.util.UtilCache;
+import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.GenericEntity;
 import org.ofbiz.entity.GenericPK;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityConditionList;
+import org.ofbiz.entity.condition.EntityExpr;
+import org.ofbiz.entity.condition.EntityOperator;
+import org.ofbiz.entity.util.ByteWrapper;
+import org.ofbiz.security.Security;
+import org.ofbiz.service.DispatchContext;
+import org.ofbiz.service.ServiceUtil;
+import org.ofbiz.service.GenericServiceException;
+import org.ofbiz.service.LocalDispatcher;
+import org.ofbiz.service.ModelService;
+import org.ofbiz.content.content.ContentServices;
+import org.ofbiz.content.data.DataServices;
+import org.ofbiz.content.content.ContentWorker;
 
 /**
  * ContentManagementWorker Class
  *
  * @author     <a href="mailto:byersa@automationgroups.com">Al Byers</a>
- * @version    $Revision: 1.2 $
+ * @version    $Revision: 1.3 $
  * @since      3.0
  *
  * 
@@ -24,45 +54,150 @@ public class ContentManagementWorker {
 
     public static final String module = ContentManagementWorker.class.getName();
 
+    public static void mruAdd(HttpServletRequest request, GenericEntity pk, String suffix ) {
+        HttpSession session = request.getSession();
+        mruAdd(session, pk, suffix );
+    }
 
-    public static void lruAdd(HttpSession session, GenericPK pk ) {
+    public static void mruAdd(HttpServletRequest request, GenericEntity pk ) {
+        HttpSession session = request.getSession();
+        mruAdd(session, pk, null );
+    }
+
+    public static void mruAdd(HttpSession session, GenericEntity pk ) {
+        mruAdd(session, pk, null );
+    }
+
+    public static void mruAdd(HttpSession session, GenericEntity pk, String suffix ) {
+
+        if (pk == null) return;
 
         Map lookupCaches = (Map)session.getAttribute("lookupCaches");
         if(lookupCaches == null){
             lookupCaches = new HashMap();
             session.setAttribute("lookupCaches", lookupCaches);
         }    
-Debug.logInfo("in lruAdd, lookupCaches:" + lookupCaches, "");
+            //Debug.logInfo("in mruAdd, lookupCaches:" + lookupCaches, "");
         String entityName = pk.getEntityName();
-Debug.logInfo("in lruAdd, entityName:" + entityName, "");
-        UtilCache lkupCache = (UtilCache)lookupCaches.get(entityName);
-Debug.logInfo("in lruAdd, lkupCache:" + lkupCache, "");
+            //Debug.logInfo("in mruAdd, entityName:" + entityName, "");
+            //Debug.logInfo("in mruAdd, suffix:" + suffix, "");
+        if (entityName.indexOf("DataResource") >= 0) {
+            GenericDelegator delegator = pk.getDelegator();
+      
+            // Force all view variations to DataResource
+            GenericValue p = delegator.makeValue("DataResourceContentView", null);
+            //Debug.logInfo("in mruAdd, p:" + p, "");
+            String s = null;
+            try {
+                s = (String)pk.get("dataResourceId");
+                if (UtilValidate.isEmpty(s))  {
+                    s = (String)pk.get("drDataResourceId");
+                }
+                p.set("dataResourceId", s);
+                s = (String)pk.get("contentId");
+                if (UtilValidate.isEmpty(s))  {
+                    s = (String)pk.get("coContentId");
+                }
+                p.set("coContentId", s);
+            } catch(IllegalArgumentException e) { 
+                // ignore 
+            }
+            //Debug.logInfo("in mruAdd, s:" + s, "");
+            if (UtilValidate.isNotEmpty(s))  {
+                mruAddByEntityName( "DataResourceContentView", null, p, lookupCaches);
+                if (suffix != null && suffix.length() > 0) {
+                    mruAddByEntityName( "DataResourceContentView", suffix, p, lookupCaches);
+                }
+            }
+
+        } else {
+            mruAddByEntityName( entityName, null, pk, lookupCaches);
+            if (suffix != null && suffix.length() > 0) {
+                mruAddByEntityName( entityName, suffix, pk, lookupCaches);
+            }
+        }
+        return;
+    }
+
+   /**
+    * Makes an entry in the "most recently used" cache. It picks the cache
+    * by the entity name and builds a signature from the primary key values.
+    *
+    * @param entityName 
+    * @param suffix 
+    * @param pk either a GenericValue or GenericPK - populated
+    */
+    public static void mruAddByEntityName(String entityName, String suffix, 
+                                          GenericEntity pk, Map lookupCaches) {
+
+            //Debug.logInfo("in mruAddByEntityName, pk:" + pk, "");
+        String cacheEntityName = entityName;
+        if (UtilValidate.isNotEmpty(suffix)) {
+            cacheEntityName = entityName + suffix;
+        }
+            //Debug.logInfo("in mruAddByEntityName, cacheEntityName:" + cacheEntityName, "");
+        UtilCache lkupCache = (UtilCache)lookupCaches.get(cacheEntityName);
+            //Debug.logInfo("in mruAddByEntityName, lkupCache:" + lkupCache, "");
         if(lkupCache == null){
-            lkupCache	= new UtilCache(entityName,10,0);
-            lookupCaches.put(entityName, lkupCache);
+            lkupCache	= new UtilCache(cacheEntityName,10,0);
+            lookupCaches.put(cacheEntityName, lkupCache);
         }    
-Debug.logInfo("in lruAdd, lkupCache(2):" + lkupCache, "");
-Debug.logInfo("in lruAdd, pk:" + pk, "");
         
-        String idSig = buildPKSig(pk);
-Debug.logInfo("in lruAdd, idSig:" + idSig, "");
-        lkupCache.put(idSig,pk);
+        String idSig = buildPKSig(pk, null);
+            //Debug.logInfo("in mruAddByEntityName, idSig:" + idSig, "");
+        GenericPK p = pk.getPrimaryKey();
+            //Debug.logInfo("in mruAddByEntityName, p:" + p, "");
+        lkupCache.put(idSig,p);
         return;
     }
 
 
-    public static String buildPKSig( GenericPK pk ) {
+   /**
+    * Builds a string signature from a GenericValue or GenericPK.
+    *
+    * @param pk either a populated GenericValue or GenericPK.
+    * @param suffix a string that can be used to distinguish the signature (probably not used).
+    */
+    public static String buildPKSig( GenericEntity pk, String suffix ) {
 
         String sig = "";
-        Collection keyColl = pk.getAllKeys();
-        Iterator it = keyColl.iterator();
+        Collection keyColl = pk.getPrimaryKey().getAllKeys();
+        List keyList = new ArrayList(keyColl);
+        Collections.sort(keyList);
+        Iterator it = keyList.iterator();
         while (it.hasNext()) {
-            if (sig.length() > 0) sig += "_";
             String ky = (String)it.next();
             String val = (String)pk.get(ky);
-            if (val == null) val = "";
-            sig += val;
+            //Debug.logInfo("in buildPKSig, ky:" + ky + " val:" + val, "");
+            if (val != null && val.length() > 0) {
+                if (sig.length() > 0) sig += "_";
+                sig += val;
+            }
+        }
+        if (suffix != null && suffix.length() > 0) {
+            if (sig.length() > 0) sig += "_";
+            sig += suffix;
         }
         return sig;
+    }
+
+
+    public static void setCurrentEntityMap(HttpServletRequest request, GenericEntity ent) {
+     
+        String entityName = ent.getEntityName();
+        setCurrentEntityMap(request, entityName, ent);
+    }
+
+    public static void setCurrentEntityMap(HttpServletRequest request,
+                                 String entityName, GenericEntity ent) {
+        HttpSession session = request.getSession();
+        Map currentEntityMap = (Map)session.getAttribute("currentEntityMap");
+        if(currentEntityMap == null){
+            currentEntityMap     = new HashMap();
+            session.setAttribute("currentEntityMap", currentEntityMap);
+        }
+
+        currentEntityMap.put(entityName, ent);
+        //Debug.logInfo("in setCurrentEntityMap, ent:" + ent,"");
     }
 }
