5d27c722916590e5f076a4210897348a6016ec09
byersa <byersa@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2003-12-05 21:36:00 +0000

    Issue number: Obtained from: Submitted by: Reviewed by:
    I added a method getPage to this class, but I can't determine why.
    I would like to get things checked in, but I will come back and take care
    of this once I can do a compile.
    
    
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@1110 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/content/src/org/ofbiz/content/webapp/ftl/FreeMarkerViewRenderer.java
+++ components/content/src/org/ofbiz/content/webapp/ftl/FreeMarkerViewRenderer.java
@@ -1,5 +1,5 @@
 /*
- * $Id: FreeMarkerViewRenderer.java,v 1.1 2003/08/17 08:40:13 ajzeneski Exp $
+ * $Id: FreeMarkerViewRenderer.java,v 1.2 2003/12/05 21:36:00 byersa Exp $
  *
  * Copyright (c) 2003 The Open For Business Project - www.ofbiz.org
  *
@@ -39,16 +39,21 @@ import org.jpublish.view.ViewRenderException;
 import org.ofbiz.base.util.Debug;
 
 import freemarker.ext.beans.BeansWrapper;
+import freemarker.ext.beans.MapModel;
 import freemarker.template.SimpleHash;
 import freemarker.template.Template;
 import freemarker.template.WrappingTemplateModel;
+import freemarker.template.TemplateHashModel;
+
+import org.jpublish.page.PageInstance;
+import org.jpublish.SiteContext;
 
 /**
  * JPublish View Renderer For Freemarker Template Engine
  *
  * @author     <a href="mailto:jaz@ofbiz.org">Andy Zeneski</a>
  * @author     <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
- * @version    $Revision: 1.1 $
+ * @version    $Revision: 1.2 $
  * @since      2.1
  */
 public class FreeMarkerViewRenderer extends org.jpublish.view.freemarker.FreeMarkerViewRenderer {
@@ -85,6 +90,7 @@ public class FreeMarkerViewRenderer extends org.jpublish.view.freemarker.FreeMar
                  //Debug.logInfo("Key: " + key + " Value: " + value + ":" + (value == null ? "null" : value.getClass().getName()), module);
              }
              root.put("context", wrapper.wrap(contextMap));                          
+             root.put("jpublishContext", wrapper.wrap(context));                          
              FreeMarkerViewHandler.prepOfbizRoot(root, request, response);                     
          } catch (Exception e) {
              throw new ViewRenderException(e);            
@@ -94,9 +100,10 @@ public class FreeMarkerViewRenderer extends org.jpublish.view.freemarker.FreeMar
 
     public void render(JPublishContext context, String path, Reader in, Writer out) throws IOException, ViewRenderException{
         if (Debug.verboseOn()) Debug.logVerbose("render(" + path + ")", module);
-        try{
+        try {
             Page page = (Page)context.get(JPublishContext.JPUBLISH_PAGE);
             Object viewContext = createViewContext(context, path);
+
             Template template = fmConfig.getTemplate(path, page.getLocale());
             template.setObjectWrapper(BeansWrapper.getDefaultInstance());
             template.process(viewContext, out);
@@ -107,4 +114,16 @@ public class FreeMarkerViewRenderer extends org.jpublish.view.freemarker.FreeMar
             throw new ViewRenderException(e);
         }
     }
+
+    private Page getPage(String path, JPublishContext context) {
+        Page page = null;
+        try {
+            SiteContext siteContext = (SiteContext)context.get("site");
+            PageInstance pi = siteContext.getPageManager().getPage(path.substring(path.lastIndexOf(":")+1));
+            if (pi != null)
+                page = new Page(pi);
+        } catch (Exception e) {}
+        return page;
+    }
+
 }
