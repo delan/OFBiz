480e04e18441a15960ae8316dc6d3e313f49f113
holivier <holivier@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2003-12-09 20:56:22 +0000

    the first version of the productionRunCreate transaction
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@1192 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/manufacturing/src/org/ofbiz/manufacturing/jobshopmgt/ProductionRunServices.java
+++ components/manufacturing/src/org/ofbiz/manufacturing/jobshopmgt/ProductionRunServices.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ProductionRunServices.java,v 1.1 2003/11/27 15:38:03 holivier Exp $
+ * $Id: ProductionRunServices.java,v 1.2 2003/12/09 20:49:17 holivier Exp $
  *
  * Copyright (c) 2001, 2002, 2003 The Open For Business Project - www.ofbiz.org
  *
@@ -52,11 +52,13 @@ import org.ofbiz.service.ServiceUtil;
 import org.ofbiz.service.LocalDispatcher;
 import org.ofbiz.service.GenericServiceException;
 
+import org.ofbiz.manufacturing.techdata.TechDataServices;
+
 /**
  * Services for Production Run maintenance
  *
  * @author     <a href="mailto:olivier.heintz@nereide.biz">Olivier Heintz</a>
- * @version    $Revision: 1.1 $
+ * @version    $Revision: 1.2 $
  * @since      3.0
  */
 public class ProductionRunServices {
@@ -73,28 +75,28 @@ public class ProductionRunServices {
     public static Map deleteProductionRun(DispatchContext ctx, Map context) {
 
         /*
-         * pretty serious operation, would delete:
-         * - Party
-         * - PartyRole
-         * - PartyRelationship: from and to
-         * - PartyDataObject
-         * - Person or PartyGroup
-         * - PartyContactMech, but not ContactMech itself
-         * - PartyContactMechPurpose
-         * - Order?
+         * pretty serious operation, status depending would delete:
+         * - task
+         * - goods
+         * - inventoryItem
+         * - ...
          *
-         * We may want to not allow this, but rather have some sort of delete flag for it if it's REALLY that big of a deal...
+         * The analyse is plan but I don't know when ;-) ...
          */
-        return ServiceUtil.returnError("Cannot delete party, operation not yet implemented");
+        return ServiceUtil.returnError("Cannot delete productionRun , operation not yet implemented");
     }
 
     /**
      * Creates a Production Run.
      *  - check if routing - product link exist
      *  - check if product have a Bill Of Material
-     *  - chedk if routing have routingTask
+     *  - check if routing have routingTask
+     *  - create the workEffort for ProductionRun
+     *  - for each valid routingTask of the routing create a workeffort-task
+     *  - for the first routingTask, create for all the valid productIdTo with no associateRoutingTask  a WorkEffortGoodStandard
+     *  - for each valid routingTask of the routing and valid productIdTo associate with this RoutingTask create a WorkEffortGoodStandard
      * @param ctx The DispatchContext that this service is operating in.
-     * @param context Map containing the input parameters, productId routingId, quantity, startDate.
+     * @param context Map containing the input parameters, productId, routingId, pRQuantity, startDate, workEffortName, description
      * @return Map with the result of the service, the output parameters.
      */
     public static Map createProductionRun(DispatchContext ctx, Map context) {
@@ -103,11 +105,10 @@ public class ProductionRunServices {
 		LocalDispatcher dispatcher = ctx.getDispatcher();        
        	Security security = ctx.getSecurity();
         Timestamp now = UtilDateTime.nowTimestamp();
-		List toBeStored = new LinkedList();
-        List msgResult = new LinkedList();
+	    List msgResult = new LinkedList();
         Locale locale = (Locale) context.get("locale");
        	GenericValue userLogin = (GenericValue) context.get("userLogin");
-/* TODO: security management  
+/* TODO: security management  and finishing cleaning (ex copy from PartyServices.java)
 		if (!security.hasEntityPermission(secEntity, secOperation, userLogin)) {
 			result.put(ModelService.RESPONSE_MESSAGE, ModelService.RESPOND_ERROR);
 			result.put(ModelService.ERROR_MESSAGE, "You do not have permission to perform this operation for this party");
@@ -116,146 +117,121 @@ public class ProductionRunServices {
 */
         String productId = (String) context.get("productId");
 		String workEffortId = (String) context.get("routingId");
-		String statusId = "ROU_PROD_TEMPLATE";
-		GenericValue workEffortProduct = null, routing = null;
-		List productBom = null, routingTask = null ;
+		java.sql.Timestamp  startDate =  (java.sql.Timestamp) context.get("startDate");
+		String statusId = "PRUN_CREATED";
+		GenericValue routing = null, product = null;
+		List workEffortProducts = null, productBoms = null, routingTaskAssocs = null ;
 
 		try {
-			workEffortProduct = delegator.findByPrimaryKey("WorkEffortGoodStandard", UtilMisc.toMap("productId", productId,"workEffortId",workEffortId));
+			workEffortProducts = delegator.findByAnd("WorkEffortGoodStandard", UtilMisc.toMap("productId", productId,"workEffortId",workEffortId));
 			routing = delegator.findByPrimaryKey("WorkEffort", UtilMisc.toMap("workEffortId",workEffortId));
-// TODO: sort by seq and filter by date
-			productBom = delegator.findByAnd("ProductAssoc", UtilMisc.toMap("productId", productId,"productAssocTypeId","MANUF_COMPONENT"));
-// TODO: sort by seq and filter by date
-			routingTask = delegator.findByAnd("WorkEffortAssoc", UtilMisc.toMap("workEffortIdFrom", productId,"workEffortAssocTypeId","ROUTING_COMPONENT"));
+			product = delegator.findByPrimaryKey("Product", UtilMisc.toMap("productId", productId));
+// TODO: sort by seq and filter by date, pour ne prendre que les composants valide à la date de lancement de l'OF
+			productBoms = delegator.findByAnd("ProductAssoc", UtilMisc.toMap("productId", productId,"productAssocTypeId","MANUF_COMPONENT"));
+// TODO:   filter by date, pour ne prendre que les opérations valide à la date de lancement de l'OF
+			routingTaskAssocs = delegator.findByAnd("WorkEffortAssoc", UtilMisc.toMap("workEffortIdFrom", workEffortId,"workEffortAssocTypeId","ROUTING_COMPONENT"), 
+			                                                                                                   UtilMisc.toList("sequenceNum","fromDate"));
 		} catch (GenericEntityException e) {
 			Debug.logWarning(e.getMessage(), module);
 		}
-		if (workEffortProduct == null) 
+//TODO: test sur quantité vide et choix de la bonne gamme
+		if (workEffortProducts == null) 
 				return ServiceUtil.returnError(UtilProperties.getMessage(resource, "ManufacturingProductRoutingNotExist", locale));
-		if (productBom == null) 
+		if (productBoms == null) 
 				return ServiceUtil.returnError(UtilProperties.getMessage(resource, "ManufacturingProductHasNoBom", locale));
-		if (routingTask == null) 
+		if (routingTaskAssocs == null) 
 				return ServiceUtil.returnError(UtilProperties.getMessage(resource, "ManufacturingRoutingHasNoRoutingTask", locale));
 
-		Map serviceContext = UtilMisc.toMap("workEffortTypeId", "ROUTING", "workEffortName", routing.getString("workEffortName"),"currentStatusId","ROU_ACTIVE", "userLogin", userLogin);
+// ProductionRun header creation,   
+//TODO: Calculate the estimatedCompletionDate 
+        String workEffortName = (String) context.get("workEffortName");
+		String description = (String) context.get("description");
+		Double pRQuantity = (Double) context.get("pRQuantity");
+        if (workEffortName== null) workEffortName = product.getString("productName") + "-" + routing.getString("workEffortName");
+		Map serviceContext = new HashMap();
+		serviceContext.put("workEffortTypeId", "PROD_ORDER_HEADER");
+		serviceContext.put("workEffortPurposeTypeId","WEPT_PRODUCTION_RUN");
+		serviceContext.put("currentStatusId","PRUN_CREATED");
+		serviceContext.put("workEffortName", workEffortName);
+        serviceContext.put("description",description);
+		serviceContext.put("estimatedStartDate",startDate);
+		serviceContext.put("quantityToProduce", pRQuantity);
+		serviceContext.put("userLogin", userLogin);
 		Map resultService = null;
 		try {
 			resultService = dispatcher.runSync("createWorkEffort", serviceContext);
 		} catch (GenericServiceException e) {
 			Debug.logError(e, "Problem calling the createWorkEffort service", module);
 		}
+		String productionRunId = (String) resultService.get("workEffortId");
+		if (Debug.infoOn()) Debug.logInfo("ProductioRun created: " + productionRunId, module);
 
-		msgResult.add(resultService.get("workEffortId"));
+ // Multi creation ProductionRunTask and GoodAssoc
+		Iterator  rt = routingTaskAssocs.iterator();
+		boolean first = true;
+ 		while (rt.hasNext()) {
+	 		GenericValue routingTaskAssoc = (GenericValue) rt.next();
+			if (TechDataServices.routingTaskAssocIsValid(routingTaskAssoc, startDate)) {
+				GenericValue routingTask = null;
+				try {
+				  routingTask = routingTaskAssoc.getRelatedOne("ToWorkEffort");
+				} catch (GenericEntityException e) {
+				Debug.logError(e.getMessage(),  module);
+			    }   
+//				TODO: Calculate the estimatedCompletionDate 
+				serviceContext.clear();
+				serviceContext.put("workEffortTypeId", "PROD_ORDER_TASK");
+				serviceContext.put("workEffortPurposeTypeId", routingTask.getString("workEffortPurposeTypeId"));
+				serviceContext.put("currentStatusId","PRUN_CREATED");
+				serviceContext.put("workEffortParentId", productionRunId);
+				serviceContext.put("workEffortName", routingTask.getString("workEffortName"));
+				serviceContext.put("description",routingTask.getString("description"));
+				serviceContext.put("estimatedStartDate",startDate);
+				serviceContext.put("quantityToProduce", pRQuantity);
+				serviceContext.put("userLogin", userLogin);
+				resultService = null;
+				try {
+					resultService = dispatcher.runSync("createWorkEffort", serviceContext);
+				} catch (GenericServiceException e) {
+					Debug.logError(e, "Problem calling the createWorkEffort service", module);
+				}
+				String productionRunTaskId = (String) resultService.get("workEffortId");
+				if (Debug.infoOn()) Debug.logInfo("ProductionRunTaskId created: " + productionRunTaskId, module);
+				if ( first ) {
+					first = false;
+					Iterator  pb = productBoms.iterator();
+					while (pb.hasNext()) {
+						GenericValue productBom = (GenericValue) pb.next();
+						if (TechDataServices.productBomIsValid(productBom, startDate) 
+					    && productBom.getString("routingWorkEffortId") == null ) {
+							serviceContext.clear();
+							serviceContext.put("workEffortId", productionRunTaskId);
+							serviceContext.put("productId", productBom.getString("productIdTo"));
+							serviceContext.put("statusId","WIP_INCOMING_FULFIL");
+							serviceContext.put("fromDate",productBom.getTimestamp("fromDate"));
+							double scrapFactor = (productBom.get("scrapFactor") != null)? productBom.getDouble("scrapFactor").doubleValue() : 0;
+							serviceContext.put("estimatedQuantity", new Double(Math.floor((productBom.getDouble("quantity").doubleValue() * pRQuantity.doubleValue() / (1-(scrapFactor / 100))) + 0.5)));
+							serviceContext.put("userLogin", userLogin);
+							resultService = null;
+							try {
+								resultService = dispatcher.runSync("createWorkEffortGoodStandard", serviceContext);
+							} catch (GenericServiceException e) {
+								Debug.logError(e, "Problem calling the createWorkEffortGoodStandard service", module);
+							}
+							if (Debug.infoOn()) Debug.logInfo("ProductLink created for productId: " + productBom.getString("productIdTo"), module);
+						}
+					}
+				}
+//				TODO: generate the WorkEffortGoodStandard if there are some BOM at this RoutingTask pour ça il faut une jointure de table BOM et ROU
+//				TODO: startDate = estimatedCompletionDate
+	 		}
+ 		}
+//		TODO: out param must be productionRunId and not list of message
+ 		msgResult.add("réussi");
 
-        // message example
-//		in properties files there is: cart.add_category_defaults=Added ${totalQuantity} items to the cart.
-//		Map messageMap = UtilMisc.toMap("totalQuantity", UtilFormatOut.formatQuantity(totalQuantity) );
-//	   request.setAttribute("_EVENT_MESSAGE_", UtilProperties.getMessage(resource, "cart.add_category_defaults",
-//									 messageMap, locale ));
-//       if (partyId != null && Character.isDigit(partyId.charAt(0))) {
-//            return ServiceUtil.returnError(UtilProperties.getMessage(resource, "party.id_is_digit", locale));
-//        }
-
-        // partyId might be empty, so check it and get next seq party id if empty
-/*        if (partyId == null || partyId.length() == 0) {
-           Long newId = delegator.getNextSeqId("Party");
-
-            if (newId == null) {
-                return ServiceUtil.returnError(UtilProperties.getMessage(resource, "party.id_generation_failure", locale));
-            } else {
-                partyId = newId.toString();
-            }
-        
-
-            // create a party if one doesn't already exist
-            Map newPartyMap = UtilMisc.toMap("partyId", partyId, "partyTypeId", "PERSON", "createdDate", now, "lastModifiedDate", now);
-            if (userLogin != null) {
-                newPartyMap.put("createdByUserLogin", userLogin.get("userLoginId"));
-                newPartyMap.put("lastModifiedByUserLogin", userLogin.get("userLoginId"));
-            }
-            party = delegator.makeValue("Party", newPartyMap);
-            toBeStored.add(party);
-        }
-
-        GenericValue person = null;
-
-        try {
-            person = delegator.findByPrimaryKey("Person", UtilMisc.toMap("partyId", partyId));
-        } catch (GenericEntityException e) {
-            Debug.logWarning(e.getMessage(), module);
-        }
-
-        if (person != null) {
-            return ServiceUtil.returnError(UtilProperties.getMessage(resource, "person.create.person_exists", locale));
-        }
-
-        person = delegator.makeValue("Person", UtilMisc.toMap("partyId", partyId));
-        person.setNonPKFields(context);
-        toBeStored.add(person);
-
-        try {
-            delegator.storeAll(toBeStored);
-        } catch (GenericEntityException e) {
-            Debug.logWarning(e.getMessage(), module);
-            return ServiceUtil.returnError(UtilProperties.getMessage(resource, "person.create.db_error", new Object[] { e.getMessage() }, locale));
-        } 
-        person.setNonPKFields(context);
-            person.store();
-            if (Debug.verboseOn()) Debug.logVerbose("List: " + c, module);
-            if (Debug.infoOn()) Debug.logInfo("PartyFromEmail number found: " + c.size(), module);
-            exprs.add(new EntityExpr("infoString", true, EntityOperator.LIKE, "%" + email.toUpperCase() + "%", true));
-            List c = EntityUtil.filterByDate(delegator.findByAnd("PartyAndContactMech", exprs, UtilMisc.toList("infoString")), true);
-*/
-		
         result.put("msgResult", msgResult);
-        result.put(ModelService.RESPONSE_MESSAGE, ModelService.RESPOND_SUCCESS);
+		result.put(ModelService.SUCCESS_MESSAGE, UtilProperties.getMessage(resource, "ManufacturingProductionRunCreated",UtilMisc.toMap("productionRunId", productionRunId), locale));
         return result;
     }
 
-    /**
-     * Updates a Person.
-     * <b>security check</b>: userLogin partyId must equal partyId, or must have PARTYMGR_UPDATE permission.
-     * @param ctx The DispatchContext that this service is operating in.
-     * @param context Map containing the input parameters.
-     * @return Map with the result of the service, the output parameters.
-     */
-/*    public static Map updatePerson(DispatchContext ctx, Map context) {
-        Map result = new HashMap();
-        GenericDelegator delegator = ctx.getDelegator();
-        Security security = ctx.getSecurity();
-        GenericValue userLogin = (GenericValue) context.get("userLogin");
-        Locale locale = (Locale) context.get("locale");
-
-        String partyId = ServiceUtil.getPartyIdCheckSecurity(userLogin, security, context, result, "PARTYMGR", "_UPDATE");
-
-        if (result.size() > 0)
-            return result;
-
-        GenericValue person = null;
-
-        try {
-            person = delegator.findByPrimaryKey("Person", UtilMisc.toMap("partyId", partyId));
-        } catch (GenericEntityException e) {
-            Debug.logWarning(e, module);
-            return ServiceUtil.returnError(UtilProperties.getMessage(resource, "person.update.read_failure", new Object[] { e.getMessage() }, locale));
-        }
-
-        if (person == null) {
-            return ServiceUtil.returnError(UtilProperties.getMessage(resource, "person.update.not_found", locale));
-        }
-
-        person.setNonPKFields(context);
-
-        try {
-            person.store();
-        } catch (GenericEntityException e) {
-            Debug.logWarning(e.getMessage(), module);
-            return ServiceUtil.returnError(UtilProperties.getMessage(resource, "person.update.write_failure", new Object[] { e.getMessage() }, locale));
-        }
-
-        result.put(ModelService.RESPONSE_MESSAGE, ModelService.RESPOND_SUCCESS);
-        result.put(ModelService.SUCCESS_MESSAGE, UtilProperties.getMessage(resource, "person.update.success", locale));
-        return result;
-    }
-*/
 }
