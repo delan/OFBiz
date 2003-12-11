a698dde5ca0bfb98e2a8868285d3ee498f10faa0
jonesde <jonesde@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2003-12-11 01:58:19 +0000

    Added code to auto create tx stamp indexes, uses some new (and complex) code to shorten db names to make unique index names, had to change one entity name to make it work
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@1211 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/entity/src/org/ofbiz/entity/model/ModelEntity.java
+++ components/entity/src/org/ofbiz/entity/model/ModelEntity.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ModelEntity.java,v 1.5 2003/12/04 20:54:53 jonesde Exp $
+ * $Id: ModelEntity.java,v 1.6 2003/12/11 01:58:19 jonesde Exp $
  *
  * Copyright (c) 2001, 2002 The Open For Business Project - www.ofbiz.org
  *
@@ -35,7 +35,7 @@ import org.ofbiz.base.util.*;
  *
  * @author     <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
  * @author     <a href="mailto:jaz@ofbiz.org">Andy Zeneski</a>
- * @version    $Revision: 1.5 $
+ * @version    $Revision: 1.6 $
  * @since      2.0
  */
 public class ModelEntity implements Comparable {
@@ -136,6 +136,12 @@ public class ModelEntity implements Comparable {
             ModelField newField = reader.createModelField(STAMP_TX_FIELD, "date-time", null, false);
             newField.setIsAutoCreatedInternal(true);
             this.fields.add(newField);
+            
+            // also add an index for this field
+            String indexName = ModelUtil.shortenDbName(this.tableName + "_TXSTMP", 18);
+            ModelIndex txIndex = new ModelIndex(this, indexName, false);
+            txIndex.addIndexField(ModelEntity.STAMP_TX_FIELD);
+            indexes.add(txIndex);
         }
 
         if (utilTimer != null) utilTimer.timerString("  createModelEntity: before prim-keys");
--- components/entity/src/org/ofbiz/entity/model/ModelUtil.java
+++ components/entity/src/org/ofbiz/entity/model/ModelUtil.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ModelUtil.java,v 1.1 2003/08/16 22:05:48 ajzeneski Exp $
+ * $Id: ModelUtil.java,v 1.2 2003/12/11 01:58:19 jonesde Exp $
  *
  *  Copyright (c) 2001, 2002 The Open For Business Project - www.ofbiz.org
  *
@@ -31,11 +31,13 @@ import org.ofbiz.base.util.*;
  * Generic Entity - General Utilities
  *
  * @author     <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
- * @version    $Revision: 1.1 $
+ * @version    $Revision: 1.2 $
  * @since      2.0
  */
 public class ModelUtil {
-
+    
+    public static final String module = ModelUtil.class.getName();
+    
     /** 
      * Changes the first letter of the passed String to upper case.
      * @param string The passed String
@@ -141,6 +143,108 @@ public class ModelUtil {
         }
 
         return dbName.toString();
+    }
+
+    public static String vowelBag = "aeiouyAEIOUY";
+    /**  Start by removing all vowels, then pull 1 letter at a time off the end of each _ separated segment, go until it is less than or equal to the desired length 
+     * 
+     * @param dbName
+     * @param desiredLength
+     * @return shortened String
+     */
+    public static String shortenDbName(String dbName, int desiredLength) {
+        StringBuffer dbBuf = new StringBuffer(dbName);
+        if (dbBuf.length() > desiredLength) {
+            // remove one vowel at a time, starting at beginning
+            for (int i = dbBuf.length() - 1; i > 0; i--) {
+                // don't remove vowels that are at the beginning of the string (taken care of by the i > 0) or right after an underscore
+                if (dbBuf.charAt(i - 1) == '_') {
+                    continue;
+                }
+                
+                char curChar = dbBuf.charAt(i);
+                if (vowelBag.indexOf(curChar) > 0) {
+                    dbBuf.deleteCharAt(i);
+                }
+            }
+        }
+        
+        // remove all double underscores
+        while (dbBuf.indexOf("__") > 0) {
+            dbBuf.deleteCharAt(dbBuf.indexOf("__"));
+        }
+        
+        while (dbBuf.length() > desiredLength) {
+            boolean removedChars = false;
+            
+            int usIndex = dbBuf.lastIndexOf("_");
+            while (usIndex > 0 && dbBuf.length() > desiredLength) {
+                // if this is the first word in the group, don't pull letters off unless it is 4 letters or more
+                int prevUsIndex = dbBuf.lastIndexOf("_", usIndex - 1);
+                if (prevUsIndex < 0 && usIndex < 4) {
+                    break;
+                }
+                
+                // don't remove characters to reduce the size two less than three characters between underscores
+                if (prevUsIndex >= 0 && (usIndex - prevUsIndex) <= 4) {
+                    usIndex = prevUsIndex;
+                    continue;
+                }
+                
+                // delete the second to last character instead of the last, better chance of being unique
+                dbBuf.deleteCharAt(usIndex - 2);
+                removedChars = true;
+                if (usIndex > 2) {
+                    usIndex = dbBuf.lastIndexOf("_", usIndex - 2);
+                } else {
+                    break;
+                }
+            }
+            
+            // now delete the char at the end of the string if necessary
+            if (dbBuf.length() > desiredLength) {
+                int removeIndex = dbBuf.length() - 1;
+                int prevRemoveIndex = dbBuf.lastIndexOf("_", removeIndex - 1);
+                // don't remove characters to reduce the size two less than two characters between underscores
+                if (prevRemoveIndex < 0 || (removeIndex - prevRemoveIndex) >= 3) {
+                    // delete the second to last character instead of the last, better chance of being unique
+                    dbBuf.deleteCharAt(removeIndex - 1);
+                    removedChars = true;
+                }
+            }
+            
+            // remove all double underscores
+            while (dbBuf.indexOf("__") > 0) {
+                dbBuf.deleteCharAt(dbBuf.indexOf("__"));
+                removedChars = true;
+            }
+            
+            // if we didn't remove anything break out to avoid an infinite loop
+            if (!removedChars) {
+                break;
+            }
+        }
+        
+        // remove all double underscores
+        while (dbBuf.indexOf("__") > 0) {
+            dbBuf.deleteCharAt(dbBuf.indexOf("__"));
+        }
+        
+        while (dbBuf.length() > desiredLength) {
+            // still not short enough, get more aggressive
+            // don't remove the first segment, just remove the second over and over until we are short enough
+            int firstUs = dbBuf.indexOf("_");
+            if (firstUs > 0) {
+                int nextUs = dbBuf.indexOf("_", firstUs + 1);
+                if (nextUs > 0) {
+                    //Debug.logInfo("couldn't shorten enough normally, removing second segment from " + dbBuf, module);
+                    dbBuf.delete(firstUs, nextUs);
+                }
+            }
+        }
+            
+        //Debug.logInfo("Shortened " + dbName + " to " + dbBuf.toString(), module);
+        return dbBuf.toString();
     }
 
     /** 
