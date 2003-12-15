703b6cc95300e37a7b5b8fc38c97f418250d2aac
byersa <byersa@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2003-12-15 11:58:36 +0000

    Issue number: Obtained from: Submitted by: Reviewed by:
    Many changes to accommodate the non-JPublish and servlet resource requirement
    on the render transforms.
    
    Separated renderDataResourceAsText into that and renderDataResourceAsHtml.
    Added services and methods to store FILE resources.
    
    
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@1265 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/content/src/org/ofbiz/content/data/DataServices.java
+++ components/content/src/org/ofbiz/content/data/DataServices.java
@@ -8,6 +8,8 @@ import java.util.Locale;
 import java.util.Map;
 import java.io.IOException;
 import java.io.Writer;
+import java.io.FileWriter;
+import java.io.File;
 
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilDateTime;
@@ -15,17 +17,21 @@ import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.base.util.UtilValidate;
 import org.ofbiz.entity.GenericDelegator;
 import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.service.GenericServiceException;
 import org.ofbiz.entity.GenericValue;
 import org.ofbiz.service.DispatchContext;
 import org.ofbiz.service.LocalDispatcher;
 import org.ofbiz.service.ModelService;
 import org.ofbiz.service.ServiceUtil;
+import org.ofbiz.content.webapp.ftl.FreeMarkerWorker;
+
+import freemarker.template.SimpleHash;
 
 /**
  * DataServices Class
  *
  * @author     <a href="mailto:byersa@automationgroups.com">Al Byers</a>
- * @version    $Revision: 1.4 $
+ * @version    $Revision: 1.5 $
  * @since      3.0
  *
  * 
@@ -115,6 +121,7 @@ public class DataServices {
                 return ServiceUtil.returnError(e.getMessage());
             }
             result.put("dataResourceId", dataResourceId);
+            result.put("dataResource", dataResource);
         }
         return result;
     }
@@ -152,6 +159,81 @@ Debug.logInfo("in create ETextMethod permissionStatus:" + permissionStatus, null
                     result.put("dataResourceId", dataResourceId);
                 } catch(GenericEntityException e) {
                     return ServiceUtil.returnError(e.getMessage());
+                }
+            }
+        }
+        
+        return result;
+    }
+
+    /**
+     * A service wrapper for the createFileMethod method.
+     * Forces permissions to be checked.
+     */
+    public static Map createFile(DispatchContext dctx, Map context) {
+        context.put("entityOperation", "_CREATE");
+        List targetOperations = new ArrayList();
+        targetOperations.add("CREATE_CONTENT");
+        context.put("targetOperationList", targetOperations);
+        context.put("skipPermissionCheck", null);
+        Map result = null;
+        try {
+            result = createFileMethod(dctx, context);
+        } catch(GenericServiceException e) {
+            return ServiceUtil.returnError(e.getMessage());
+        }
+        return result;
+    }
+
+    public static Map createFileMethod(DispatchContext dctx, Map context) throws GenericServiceException {
+        HashMap result = new HashMap();
+        GenericDelegator delegator = dctx.getDelegator();
+        LocalDispatcher dispatcher = dctx.getDispatcher();
+Debug.logInfo("in create FileMethod context:" + context, null);
+        String permissionStatus = DataResourceWorker.callDataResourcePermissionCheck( delegator, dispatcher,
+                                      context );
+Debug.logInfo("in create FileMethod permissionStatus:" + permissionStatus, null);
+        if (permissionStatus != null && permissionStatus.equalsIgnoreCase("granted") ) {
+            GenericValue dataResource = (GenericValue)context.get("dataResource");
+            String dataResourceId = (String)dataResource.get("dataResourceId");
+            String dataResourceTypeId = (String)dataResource.get("dataResourceTypeId");
+            String objectInfo = (String)dataResource.get("objectInfo");
+            String textData = (String)context.get("textData");
+            String prefix = null;
+            File file = null;
+            if (textData != null && textData.length() > 0 ) {
+                String fileName = "";
+                String sep = "";
+                try {
+                    if (UtilValidate.isEmpty(dataResourceTypeId) || dataResourceTypeId.equals("LOCAL_FILE")) {
+                        file = new File(prefix + sep + objectInfo );
+                        if (!file.isAbsolute()) {
+                            throw new GenericServiceException("File: " + file + " is not absolute");
+                        }
+                    } else if (dataResourceTypeId.equals("OFBIZ_FILE")) {
+                        prefix = System.getProperty("ofbiz.home");
+                        if ( objectInfo.indexOf("/") != 0 
+                               && prefix.lastIndexOf("/") != (prefix.length() -1)) {
+                            sep = "/";
+                        }
+                        file = new File(prefix + sep + objectInfo );
+                    } else if (dataResourceTypeId.equals("CONTEXT_FILE")) {
+                        prefix = (String)context.get( "rootDir");
+                        if ( objectInfo.indexOf("/") != 0 
+                               && prefix.lastIndexOf("/") != (prefix.length() -1)) {
+                            sep = "/";
+                        }
+                        file = new File(prefix + sep + objectInfo );
+                    }
+                    if (file == null) {
+                        throw new IOException("File: " + file + " is null");
+                    }
+                    FileWriter out = new FileWriter( file);
+                    out.write(textData);
+                    out.close();
+                } catch(IOException e) {
+                    Debug.logWarning(e, module);
+                    throw new GenericServiceException(e.getMessage());
                 }
             }
         }
@@ -243,6 +325,7 @@ Debug.logInfo("in create ETextMethod permissionStatus:" + permissionStatus, null
                 return ServiceUtil.returnError(e.getMessage());
             }
         }
+        result.put("dataResource", dataResource);
         return result;
     }
 
@@ -287,30 +370,137 @@ Debug.logInfo("in create ETextMethod permissionStatus:" + permissionStatus, null
         return result;
     }
 
+
+    /**
+     * A service wrapper for the updateFileMethod method.
+     * Forces permissions to be checked.
+     */
+    public static Map updateFile(DispatchContext dctx, Map context) {
+        context.put("entityOperation", "_UPDATE");
+        List targetOperations = new ArrayList();
+        targetOperations.add("UPDATE_CONTENT");
+        context.put("targetOperationList", targetOperations);
+        context.put("skipPermissionCheck", null);
+        Map result = null;
+        try {
+            result = updateFileMethod(dctx, context);
+        } catch(GenericServiceException e) {
+            return ServiceUtil.returnError(e.getMessage());
+        }
+        return result;
+    }
+
+    public static Map updateFileMethod(DispatchContext dctx, Map context) throws GenericServiceException {
+        HashMap result = new HashMap();
+        GenericDelegator delegator = dctx.getDelegator();
+        LocalDispatcher dispatcher = dctx.getDispatcher();
+        GenericValue fileText = null;
+        Locale locale = (Locale)context.get("locale");
+        String permissionStatus = DataResourceWorker.callDataResourcePermissionCheck( delegator, dispatcher,
+                                      context );
+        if (permissionStatus != null && permissionStatus.equalsIgnoreCase("granted") ) {
+            GenericValue dataResource = (GenericValue)context.get("dataResource");
+            String dataResourceId = (String)dataResource.get("dataResourceId");
+            String dataResourceTypeId = (String)dataResource.get("dataResourceTypeId");
+            String objectInfo = (String)dataResource.get("objectInfo");
+            String textData = (String)context.get("textData");
+            String prefix = "";
+            File file = null;
+            String fileName = "";
+            String sep = "";
+            try {
+                if (UtilValidate.isEmpty(dataResourceTypeId) || dataResourceTypeId.equals("LOCAL_FILE")) {
+                    fileName = prefix + sep + objectInfo; 
+                    file = new File(fileName);
+                    if (file == null) {
+                        throw new GenericServiceException("File: " + fileName + " is null.");
+                    }
+                    if (!file.isAbsolute()) {
+                        throw new GenericServiceException("File: " + fileName + " is not absolute.");
+                    }
+                } else if (dataResourceTypeId.equals("OFBIZ_FILE")) {
+                    prefix = System.getProperty("ofbiz.home");
+                    if ( objectInfo.indexOf("/") != 0 
+                           && prefix.lastIndexOf("/") != (prefix.length() -1)) {
+                        sep = "/";
+                    }
+                    file = new File(prefix + sep + objectInfo );
+                } else if (dataResourceTypeId.equals("CONTEXT_FILE")) {
+                    prefix = (String)context.get( "rootDir");
+                    if ( objectInfo.indexOf("/") != 0 
+                           && prefix.lastIndexOf("/") != (prefix.length() -1)) {
+                        sep = "/";
+                    }
+                    file = new File(prefix + sep + objectInfo );
+                }
+                if (file == null) {
+                    throw new IOException("File: " + file + " is null");
+                }
+                FileWriter out = new FileWriter( file);
+                out.write(textData);
+                out.close();
+            } catch(IOException e) {
+                Debug.logWarning(e, module);
+                throw new GenericServiceException(e.getMessage());
+            }
+        }
+        
+        return result;
+    }
+
     public static void renderDataResourceAsText(DispatchContext dctx, Map context) throws IOException {
 
         Map results = new HashMap();
         GenericDelegator delegator = dctx.getDelegator();
         LocalDispatcher dispatcher = dctx.getDispatcher();
         Writer out = (Writer) context.get("outWriter"); 
-        Map templateContext = (Map) context.get("templateContext"); 
+        SimpleHash templateContext = (SimpleHash) context.get("templateContext"); 
         GenericValue userLogin = (GenericValue)context.get("userLogin");
         String dataResourceId = (String) context.get("dataResourceId"); 
         if (templateContext != null && UtilValidate.isEmpty(dataResourceId)) {
-            dataResourceId = (String)templateContext.get("dataResourceId");
+            dataResourceId = (String)FreeMarkerWorker.get(templateContext, "dataResourceId");
         }
         String mimeTypeId = (String) context.get("mimeTypeId"); 
         if (templateContext != null && UtilValidate.isEmpty(mimeTypeId)) {
-            mimeTypeId = (String)templateContext.get("mimeTypeId");
+            mimeTypeId = (String)FreeMarkerWorker.get(templateContext, "mimeTypeId");
         }
 
         Locale locale = (Locale) context.get("locale"); 
 
         if (templateContext == null)
-            templateContext = new HashMap();
+            templateContext = new SimpleHash();
 
         GenericValue view = (GenericValue)context.get("subContentDataResourceView");
         DataResourceWorker.renderDataResourceAsText(delegator, dataResourceId, out, 
+                          templateContext, view, locale, mimeTypeId);
+        return;
+
+    }
+
+    public static void renderDataResourceAsHtml(DispatchContext dctx, Map context) throws IOException {
+
+        Map results = new HashMap();
+        GenericDelegator delegator = dctx.getDelegator();
+        LocalDispatcher dispatcher = dctx.getDispatcher();
+        Writer out = (Writer) context.get("outWriter"); 
+        SimpleHash templateContext = (SimpleHash) context.get("templateContext"); 
+        GenericValue userLogin = (GenericValue)context.get("userLogin");
+        String dataResourceId = (String) context.get("dataResourceId"); 
+        if (templateContext != null && UtilValidate.isEmpty(dataResourceId)) {
+            dataResourceId = (String)FreeMarkerWorker.get(templateContext, "dataResourceId");
+        }
+        String mimeTypeId = (String) context.get("mimeTypeId"); 
+        if (templateContext != null && UtilValidate.isEmpty(mimeTypeId)) {
+            mimeTypeId = (String)FreeMarkerWorker.get(templateContext, "mimeTypeId");
+        }
+
+        Locale locale = (Locale) context.get("locale"); 
+
+        if (templateContext == null)
+            templateContext = new SimpleHash();
+
+        GenericValue view = (GenericValue)context.get("subContentDataResourceView");
+        DataResourceWorker.renderDataResourceAsHtml(delegator, dataResourceId, out, 
                           templateContext, view, locale, mimeTypeId);
         return;
 
