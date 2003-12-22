1bc2af846d1890c5b91d56ba6de32858689c0812
ajzeneski <ajzeneski@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2003-12-22 21:00:34 +0000

    no longer call makeValid when the service had validate set to false
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@1372 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/service/src/org/ofbiz/service/ServiceXaWrapper.java
+++ components/service/src/org/ofbiz/service/ServiceXaWrapper.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ServiceXaWrapper.java,v 1.6 2003/12/13 23:50:35 ajzeneski Exp $
+ * $Id: ServiceXaWrapper.java,v 1.7 2003/12/22 21:00:34 ajzeneski Exp $
  *
  * Copyright (c) 2003 The Open For Business Project - www.ofbiz.org
  *
@@ -40,7 +40,7 @@ import org.ofbiz.entity.transaction.GenericTransactionException;
  * ServiceXaWrapper - XA Resource wrapper for running services on commit() or rollback()
  *
  * @author     <a href="mailto:jaz@ofbiz.org">Andy Zeneski</a>
- * @version    $Revision: 1.6 $
+ * @version    $Revision: 1.7 $
  * @since      3.0
  */
 public class ServiceXaWrapper extends GenericXaResource {
@@ -139,7 +139,10 @@ public class ServiceXaWrapper extends GenericXaResource {
             boolean serviceError = false;
             try {
                 ModelService model = dctx.getModelService(this.commitService);
-                Map thisContext = model.makeValid(this.commitContext, ModelService.IN_PARAM);
+                Map thisContext = this.commitContext;
+                if (model.validate) {
+                    thisContext = model.makeValid(this.commitContext, ModelService.IN_PARAM);
+                }
                 dctx.getDispatcher().runSyncIgnore(this.commitService, thisContext);
             } catch (GenericServiceException e) {
                 Debug.logError(e, "Problem calling sync service : " + this.commitService + " / " + this.commitContext, module);
@@ -208,7 +211,10 @@ public class ServiceXaWrapper extends GenericXaResource {
             // invoke the service
             try {
                 ModelService model = dctx.getModelService(this.rollbackService);
-                Map thisContext = model.makeValid(this.rollbackContext, ModelService.IN_PARAM);
+                Map thisContext = this.rollbackContext;
+                if (model.validate) {
+                    thisContext = model.makeValid(this.rollbackContext, ModelService.IN_PARAM);
+                }
                 dctx.getDispatcher().runAsync(this.rollbackService, thisContext, true);
             } catch (GenericServiceException e) {
                 Debug.logError(e, "Problem calling async service : " + this.rollbackService + " / " + this.rollbackContext, module);
