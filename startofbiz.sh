135f31c057c71d4bbe10e773574e888afc577ddd
ajzeneski <ajzeneski@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2003-12-25 15:03:22 +0000

    updated unix start to run in background; log to console.log and support both JAVA_HOME path or non-java home path for OSX systems
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@1391 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- startofbiz.sh
+++ startofbiz.sh
@@ -1,2 +1,22 @@
+#!/bin/sh
 
-$JAVA_HOME/bin/java -Xmx128M -jar ofbiz.jar
+# console log file
+OFBIZ_LOG=logs/console.log
+
+# delete the last log
+rm -f $OFBIZ_LOG
+
+# VM args
+VMARGS=-Xmx128M
+
+# location of java executable
+if [ -e $JAVA_HOME ]; then
+  JAVA=$JAVA_HOME/bin/java
+else
+  JAVA=java
+fi
+
+# start ofbiz
+$JAVA $VMARGS -jar ofbiz.jar >>$OFBIZ_LOG 2>>$OFBIZ_LOG&
+exit 0
+
