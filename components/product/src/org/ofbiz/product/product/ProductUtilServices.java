17f58aad89effefe064983cb33b95a966f341ba2
jonesde <jonesde@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-01-27 09:08:42 +0000

    More fixes, mostly to handle the relateds better for that data
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@1642 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/product/src/org/ofbiz/product/product/ProductUtilServices.java
+++ components/product/src/org/ofbiz/product/product/ProductUtilServices.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ProductUtilServices.java,v 1.9 2004/01/27 09:06:19 jonesde Exp $
+ * $Id: ProductUtilServices.java,v 1.10 2004/01/27 09:08:42 jonesde Exp $
  *
  *  Copyright (c) 2002 The Open For Business Project (www.ofbiz.org)
  *  Permission is hereby granted, free of charge, to any person obtaining a
@@ -58,7 +58,7 @@ import org.ofbiz.service.ServiceUtil;
  *
  * @author     <a href="mailto:jaz@ofbiz.org">Andy Zeneski</a>
  * @author     <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
- * @version    $Revision: 1.9 $
+ * @version    $Revision: 1.10 $
  * @since      2.0
  */
 public class ProductUtilServices {
@@ -302,6 +302,8 @@ public class ProductUtilServices {
                     // ProductAssoc
                     duplicateRelated(product, "Main", "ProductAssoc", "productId", variantProductId, nowTimestamp, false, delegator);
                     duplicateRelated(product, "Assoc", "ProductAssoc", "productIdTo", variantProductId, nowTimestamp, false, delegator);
+
+                    product.removeRelated("ProductKeyword");
                     
                     numWithOneValid++;
                     if (numWithOneValid % 100 == 0) {
