eea8f94bf03b9cd311d44e44ce94a7a6b73c131d
jonesde <jonesde@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-05-01 13:21:48 +0000

    Added file from Adam Heath for java.net issue 126
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@2204 deaff617-5be1-0310-a1b7-ec6878f92ba9

---
+++ base/src/base/org/ofbiz/base/util/UtilObject.java
@@ -0,0 +1,58 @@
+/*
+ * $Id: UtilObject.java,v 1.1 2004/05/01 13:21:48 jonesde Exp $
+ *
+ *  Copyright (c) 2004 The Open For Business Project - www.ofbiz.org
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ *  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ *  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ofbiz.base.util;
+
+public class UtilObject
+{
+    public static boolean equalsHelper( Object o1, Object o2 ) {
+        if ( o1 == o2 ) {
+            // handles same-reference, or null
+            return true;
+        } else if ( o1 == null || o2 == null ) {
+            // either o1 or o2 is null, but not both
+            return false;
+        } else {
+            return o1.equals( o2 );
+        }
+    }
+
+    public static int compareToHelper( Comparable o1, Object o2 ) {
+        if ( o1 == o2 ) {
+            // handles same-reference, or null
+            return 0;
+        } else if ( o1 == null ) {
+            return -1;
+        } else if ( o2 == null ) {
+            // either o1 or o2 is null, but not both
+            return 1;
+        } else {
+            return o1.compareTo( o2 );
+        }
+    }
+
+    public static int doHashCode( Object o1 ) {
+        if ( o1 == null ) return 0;
+        return o1.hashCode();
+    }
+}
