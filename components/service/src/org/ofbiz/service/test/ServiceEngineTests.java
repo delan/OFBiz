85944270a1908b722c51454567d2398d20a2d96a
ajzeneski <ajzeneski@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-05-04 15:37:36 +0000

    added first (example) test case for service engine; hope to extend this to be more useful
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@2218 deaff617-5be1-0310-a1b7-ec6878f92ba9

---
+++ components/service/src/org/ofbiz/service/test/ServiceEngineTests.java
@@ -0,0 +1,47 @@
+/*
+ * $Id: ServiceEngineTests.java,v 1.1 2004/05/04 15:37:36 ajzeneski Exp $
+ *
+ */
+package org.ofbiz.service.test;
+
+import junit.framework.TestCase;
+
+import java.util.Map;
+
+import org.ofbiz.service.LocalDispatcher;
+import org.ofbiz.service.GenericDispatcher;
+import org.ofbiz.service.ModelService;
+import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.base.util.UtilMisc;
+
+/**
+ * 
+ * @author     <a href="mailto:jaz@ofbiz.org">Andy Zeneski</a>
+ * @version    $Revision: 1.1 $
+ * @since      May 4, 2004
+ */
+public class ServiceEngineTests extends TestCase {
+
+    public static final String DELEGATOR_NAME = "test";
+    public static final String DISPATCHER_NAME = "test-dispatcher";
+
+    private LocalDispatcher dispatcher = null;
+
+    public ServiceEngineTests(String name) {
+        super(name);
+    }
+
+    protected void setUp() throws Exception {
+        GenericDelegator delegator = GenericDelegator.getGenericDelegator(DELEGATOR_NAME);
+        dispatcher = new GenericDispatcher(DISPATCHER_NAME, delegator);
+    }
+
+    protected void tearDown() throws Exception {
+        dispatcher.deregister();
+    }
+
+    public void testBasicJavaInvocation() throws Exception {
+        Map result = dispatcher.runSync("testScv", UtilMisc.toMap("message", "Unit Test"));
+        assertEquals("Service result success", ModelService.RESPOND_SUCCESS, result.get(ModelService.RESPONSE_MESSAGE));
+    }
+}
