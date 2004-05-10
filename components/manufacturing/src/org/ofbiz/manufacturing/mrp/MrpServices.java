eb077f137b982ef888369b49c1209f45bbc9dde2
holivier <holivier@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-05-10 06:24:00 +0000

    Licence correction, from GPL to MIT, I have forgot to change before commit
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@2237 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/manufacturing/src/org/ofbiz/manufacturing/mrp/MrpServices.java
+++ components/manufacturing/src/org/ofbiz/manufacturing/mrp/MrpServices.java
@@ -1,13 +1,24 @@
 /*
- * $Id: MrpServices.java,v 1.1 2004/05/09 21:29:06 holivier Exp $
+ * $Id: MrpServices.java,v 1.2 2004/05/10 06:24:00 holivier Exp $
+ * Copyright (c) 2003, 2004 The Open For Business Project - www.ofbiz.org
  *
- * Copyright (c) 2003, 2004 Thierry GRAUSS - tgrauss@free.fr
- * Copyright (c) 2003, 2004 Nereide - www.nereide.biz
- * 
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
  
 package org.ofbiz.manufacturing.mrp;
@@ -48,7 +59,7 @@ import org.ofbiz.service.GenericServiceException;
  * Services for running MRP
  *
  * @author     <a href=mailto:thierry.grauss@etu.univ-tours.fr">Thierry GRAUSS</a>
- * @version    $Revision: 1.1 $
+ * @version    $Revision: 1.2 $
  * @since      0.1
  */
 public class MrpServices {
