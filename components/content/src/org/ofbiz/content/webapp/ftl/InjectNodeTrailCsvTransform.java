55c273c1c2b7aef4957d72c3951b7d43d35edaa0
byersa <byersa@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-05-11 14:45:50 +0000

    Issue number: Obtained from: Submitted by: Reviewed by:
    CheckPerm... Added code to take advantage of quickCheckContentId parameter.
    This code is used to check if a publish point has the right permissions.
    
    In InjectNode... added code to use breadcrumb code if needed.
    
    
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@2265 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/content/src/org/ofbiz/content/webapp/ftl/InjectNodeTrailCsvTransform.java
+++ components/content/src/org/ofbiz/content/webapp/ftl/InjectNodeTrailCsvTransform.java
@@ -1,5 +1,5 @@
 /*
- * $Id: InjectNodeTrailCsvTransform.java,v 1.8 2004/04/30 23:08:26 ajzeneski Exp $
+ * $Id: InjectNodeTrailCsvTransform.java,v 1.9 2004/05/11 14:45:50 byersa Exp $
  * 
  * Copyright (c) 2001-2003 The Open For Business Project - www.ofbiz.org
  * 
@@ -26,6 +26,9 @@ import javax.servlet.http.HttpServletRequest;
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilValidate;
 import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.content.content.ContentWorker;
 
 import freemarker.core.Environment;
 import freemarker.template.TemplateModelException;
@@ -36,7 +39,7 @@ import freemarker.template.TransformControl;
  * InjectNodeTrailCsvTransform - Freemarker Transform for URLs (links)
  * 
  * @author <a href="mailto:byersa@automationgroups.com">Al Byers</a>
- * @version $Revision: 1.8 $
+ * @version $Revision: 1.9 $
  * @since 3.0
  */
 public class InjectNodeTrailCsvTransform implements TemplateTransformModel {
@@ -69,6 +72,7 @@ public class InjectNodeTrailCsvTransform implements TemplateTransformModel {
         //FreeMarkerWorker.convertContext(templateCtx);
         final GenericDelegator delegator = (GenericDelegator) FreeMarkerWorker.getWrappedObject("delegator", env);
         final HttpServletRequest request = (HttpServletRequest) FreeMarkerWorker.getWrappedObject("request", env);
+        final GenericValue userLogin = (GenericValue) FreeMarkerWorker.getWrappedObject("userLogin", env);
         FreeMarkerWorker.getSiteParameters(request, templateCtx);
         FreeMarkerWorker.overrideWithArgs(templateCtx, args);
 
@@ -86,34 +90,62 @@ public class InjectNodeTrailCsvTransform implements TemplateTransformModel {
 
             public int onStart() throws TemplateModelException, IOException {
                 String csvTrail = null;
-                List trail = (List)templateCtx.get("globalNodeTrail");
-                //if (Debug.infoOn()) Debug.logInfo("in InjectNodeTrailCsv, trail:"+trail,module);
-                //if (Debug.infoOn()) Debug.logInfo("in InjectNodeTrailCsv, passedCsv:"+passedCsv,module);
-                if (UtilValidate.isNotEmpty(passedCsv)) {
-                    csvTrail = passedCsv;
-                    int lastComma = passedCsv.lastIndexOf(",");
-                    String lastPassedContentId = null;
-                    if (lastComma >= 0) { 
-                        lastPassedContentId = passedCsv.substring(lastComma + 1);
-                    } else {
-                        lastPassedContentId = passedCsv;
-                    }
 
-                    if (UtilValidate.isNotEmpty(lastPassedContentId)) {
-                        if (trail != null && trail.size() > 0) {
-                            Map nd = (Map)trail.get(0);
-                            String firstTrailContentId = (String)nd.get("contentId");
-                            if (UtilValidate.isNotEmpty(firstTrailContentId)
-                                && UtilValidate.isNotEmpty(lastPassedContentId)
-                                && firstTrailContentId.equals(lastPassedContentId) ) {
-                                csvTrail += "," + FreeMarkerWorker.nodeTrailToCsv(trail.subList(1, trail.size()));
-                            } else {
-                                csvTrail += "," + FreeMarkerWorker.nodeTrailToCsv(trail);
+                List trail = (List)templateCtx.get("globalNodeTrail");
+
+                if (Debug.infoOn()) Debug.logInfo("in InjectNodeTrailCsv(0), trail:"+trail,module);
+                // This will build a nodeTrail if none exists
+                // Maybe only contentId or subContentId are passed in
+                //GenericValue currentValue = getCurrentContent( delegator, trail,  userLogin, templateCtx, nullThruDatesOnly, contentAssocPredicateId);
+                String redo = (String)templateCtx.get("redo");
+
+                if (trail == null || trail.size() == 0 || (redo != null && redo.equalsIgnoreCase("true"))) {
+                    String thisContentId = null;
+                    String subContentId = (String)templateCtx.get("subContentId");
+                    if (Debug.infoOn()) Debug.logInfo("in InjectNodeTrailCsv(0), subContentId:"+subContentId,module);
+                    String contentId = (String)templateCtx.get("contentId");
+                    if (Debug.infoOn()) Debug.logInfo("in InjectNodeTrailCsv(0), contentId:"+contentId,module);
+                    String contentAssocTypeId = (String)templateCtx.get("contentAssocTypeId");
+                    if (Debug.infoOn()) Debug.logInfo("in InjectNodeTrailCsv(0), contentAssocTypeId:"+contentAssocTypeId,module);
+                    try {
+                        if (UtilValidate.isNotEmpty(subContentId))
+                            csvTrail = ContentWorker.getContentAncestryNodeTrailCsv(delegator, subContentId, contentAssocTypeId, "to");                     
+                        else if (UtilValidate.isNotEmpty(contentId))
+                            csvTrail = ContentWorker.getContentAncestryNodeTrailCsv(delegator, contentId, contentAssocTypeId, "to");                     
+                    } catch (GenericEntityException e) {
+                        throw new RuntimeException("Error getting current content. " + e.toString());
+                    }
+                    if (Debug.infoOn()) Debug.logInfo("in InjectNodeTrailCsv(0), csvTrail:"+csvTrail,module);
+                } else {
+                    // Build nodeTrail if one does not exist
+                    //if (Debug.infoOn()) Debug.logInfo("in InjectNodeTrailCsv, trail:"+trail,module);
+                    //if (Debug.infoOn()) Debug.logInfo("in InjectNodeTrailCsv, passedCsv:"+passedCsv,module);
+                    if (UtilValidate.isNotEmpty(passedCsv)) {
+                        csvTrail = passedCsv;
+                        int lastComma = passedCsv.lastIndexOf(",");
+                        String lastPassedContentId = null;
+                        if (lastComma >= 0) { 
+                            lastPassedContentId = passedCsv.substring(lastComma + 1);
+                        } else {
+                            lastPassedContentId = passedCsv;
+                        }
+    
+                        if (UtilValidate.isNotEmpty(lastPassedContentId)) {
+                            if (trail != null && trail.size() > 0) {
+                                Map nd = (Map)trail.get(0);
+                                String firstTrailContentId = (String)nd.get("contentId");
+                                if (UtilValidate.isNotEmpty(firstTrailContentId)
+                                    && UtilValidate.isNotEmpty(lastPassedContentId)
+                                    && firstTrailContentId.equals(lastPassedContentId) ) {
+                                    csvTrail += "," + FreeMarkerWorker.nodeTrailToCsv(trail.subList(1, trail.size()));
+                                } else {
+                                    csvTrail += "," + FreeMarkerWorker.nodeTrailToCsv(trail);
+                                }
                             }
                         }
+                    } else {
+                        csvTrail = FreeMarkerWorker.nodeTrailToCsv(trail);
                     }
-                } else {
-                    csvTrail = FreeMarkerWorker.nodeTrailToCsv(trail);
                 }
                 //if (Debug.infoOn()) Debug.logInfo("in InjectNodeTrailCsv, csvTrail:"+csvTrail,module);
                 templateCtx.put("nodeTrailCsv", csvTrail);
