e3609cc1cd305d75261814a523cb8ea51ba45a95
ajzeneski <ajzeneski@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-05-22 22:43:04 +0000

    now support configurable options; still some left to do but now in a working state (except for JSP pages rendered through Regions)
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@2339 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/catalina/src/org/ofbiz/catalina/container/CatalinaContainer.java
+++ components/catalina/src/org/ofbiz/catalina/container/CatalinaContainer.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CatalinaContainer.java,v 1.1 2004/05/22 21:14:48 ajzeneski Exp $
+ * $Id: CatalinaContainer.java,v 1.2 2004/05/22 22:43:04 ajzeneski Exp $
  *
  */
 package org.ofbiz.catalina.container;
@@ -8,6 +8,7 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
 import java.net.URL;
 import java.io.IOException;
 
@@ -34,6 +35,7 @@ import org.apache.catalina.Loader;
 
 import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.core.StandardWrapper;
+import org.apache.coyote.tomcat5.CoyoteServerSocketFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
@@ -41,7 +43,7 @@ import org.xml.sax.SAXException;
 /**
  * 
  * @author     <a href="mailto:jaz@ofbiz.org">Andy Zeneski</a>
- * @version    $Revision: 1.1 $
+ * @version    $Revision: 1.2 $
  * @since      May 21, 2004
  */
 public class CatalinaContainer implements Container {
@@ -51,50 +53,68 @@ public class CatalinaContainer implements Container {
     protected static Map mimeTypes = new HashMap();
 
     protected Embedded embedded = null;
-    protected Map servers = new HashMap();
+    protected Map engines = new HashMap();
     protected Map hosts = new HashMap();
 
     protected boolean enableDefaultMimeTypes = true;
 
     public boolean start(String configFileLocation) throws ContainerException {
-        // get the container config
-        ContainerConfig.Container cc = ContainerConfig.getContainer("catalina-container", configFileLocation);
-        String useNaming = cc.getProperty("useNaming").value;
-        String debug = cc.getProperty("debug").value;
-
         // set catalina_home
         System.setProperty("catalina.home", System.getProperty("ofbiz.home") + "/components/catalina");
 
+        // get the container config
+        ContainerConfig.Container cc = ContainerConfig.getContainer("catalina-container", configFileLocation);
+        if (cc == null) {
+            throw new ContainerException("No catalina-container configuration found in container config!");
+        }
+
+        // embedded properties
+        boolean useNaming = false;
+        ContainerConfig.Container.Property namingProp = cc.getProperty("useNaming");
+        if (namingProp != null) {
+            useNaming = "true".equalsIgnoreCase(namingProp.value);
+        }
+
+        int debug = 0;
+        ContainerConfig.Container.Property debugProp = cc.getProperty("debug");
+        if (debugProp != null) {
+            try {
+                debug = Integer.parseInt(debugProp.value);
+            } catch (Exception e) {
+                debug = 0;
+            }
+        }
+
         // create the instance of Embedded
         embedded = new Embedded();
-        embedded.setDebug(0);
-        embedded.setUseNaming("true".equalsIgnoreCase(useNaming));
-        embedded.setLogger(new SystemOutLogger());
+        embedded.setDebug(debug);
+        embedded.setUseNaming(useNaming);
+        embedded.setLogger(new SystemOutLogger()); // TODO Make this configurable
 
+        // create the engines
+        List engineProps = cc.getPropertiesWithValue("engine");
+        if (engineProps == null && engineProps.size() == 0) {
+            throw new ContainerException("Cannot load CatalinaContainer; no engines defined!");
+        }
+        Iterator ei = engineProps.iterator();
+        while (ei.hasNext()) {
+            ContainerConfig.Container.Property engineProp = (ContainerConfig.Container.Property) ei.next();
+            createEngine(engineProp);
+        }
 
+        // load the web applications
+        loadComponents();
 
-        String engineName = "default-server";
-        String hostName = "0.0.0.0";
-
-        // Create an engine
-        Engine engine = embedded.createEngine();
-        engine.setName(engineName);
-        engine.setDefaultHost(hostName);
-
-        // create a default virtual host; others will be created as needed
-        Host host = embedded.createHost(hostName, CATALINA_HOSTS_HOME);
-        hosts.put(engineName + "._DEFAULT", host);
-        engine.addChild(host);
-
-        // Add in all webapps (components)
-        loadComponents(embedded);
-
-        // Install the assembled container hierarchy
-        embedded.addEngine(engine);
-
-        // Assemble and install a default HTTP connector
-        Connector connector = embedded.createConnector("0.0.0.0", 8080, "http");
-        embedded.addConnector(connector);
+        // create the connectors
+        List connectorProps = cc.getPropertiesWithValue("connector");
+        if (connectorProps == null && connectorProps.size() == 0) {
+            throw new ContainerException("Cannot load CatalinaContainer; no engines defined!");
+        }
+        Iterator ci = connectorProps.iterator();
+        while (ci.hasNext()) {
+            ContainerConfig.Container.Property connectorProp = (ContainerConfig.Container.Property) ci.next();
+            createConnector(connectorProp);
+        }
 
         // Start the embedded server
         try {
@@ -106,29 +126,154 @@ public class CatalinaContainer implements Container {
         return true;
     }
 
-    protected Engine createServer(ContainerConfig.Container.Property serverConfig) throws ContainerException {
+    protected Engine createEngine(ContainerConfig.Container.Property engineConfig) throws ContainerException {
         if (embedded == null) {
             throw new ContainerException("Cannot create Engine without Embedded instance!");
         }
 
-        ContainerConfig.Container.Property defaultHostProp = serverConfig.getProperty("default-host");
+        ContainerConfig.Container.Property defaultHostProp = engineConfig.getProperty("default-host");
         if (defaultHostProp == null) {
             throw new ContainerException("default-host element of server property is required for catalina!");
         }
 
-        String engineName = serverConfig.name;
+        String engineName = engineConfig.name;
         String hostName = defaultHostProp.value;
 
         Engine engine = embedded.createEngine();
         engine.setName(engineName);
         engine.setDefaultHost(hostName);
+        engines.put(engine.getName(), engine);
 
-        // add connectors
+        // create a default virtual host; others will be created as needed
+        Host host = createHost(engine, hostName);
+        hosts.put(engineName + "._DEFAULT", host);
 
+        embedded.addEngine(engine);
         return engine;
     }
 
-    protected void loadComponents(Embedded embedded) {
+    protected Host createHost(Engine engine, String hostName) throws ContainerException {
+        if (embedded == null) {
+            throw new ContainerException("Cannot create Host without Embedded instance!");
+        }
+
+        Host host = embedded.createHost(hostName, CATALINA_HOSTS_HOME);
+        engine.addChild(host);
+        hosts.put(engine.getName() + hostName, host);
+
+        return host;
+    }
+
+    protected Connector createConnector(ContainerConfig.Container.Property connectorProp) throws ContainerException {
+        if (embedded == null) {
+            throw new ContainerException("Cannot create Connector without Embedded instance!");
+        }
+
+        String host = "0.0.0.0";
+        String type = "http";
+        int port = 8080;
+        int redirect = 0;
+        boolean lookup = false;
+
+        ContainerConfig.Container.Property hostProp = connectorProp.getProperty("host");
+        if (hostProp != null) {
+            host = hostProp.value;
+        }
+
+        ContainerConfig.Container.Property typeProp = connectorProp.getProperty("type");
+        if (typeProp != null) {
+            type = typeProp.value;
+        }
+
+        ContainerConfig.Container.Property portProp = connectorProp.getProperty("port");
+        if (portProp != null) {
+            try {
+                port = Integer.parseInt(portProp.value);
+            } catch (Exception e) {
+                Debug.logWarning("Invalid port number " + portProp.value + " setting connector [" + connectorProp.name + "] to 8080.", module);
+                port = 8080;
+            }
+        }
+
+        ContainerConfig.Container.Property lookupProp = connectorProp.getProperty("enable-lookups");
+        if (lookupProp != null) {
+            lookup = "true".equalsIgnoreCase(lookupProp.value);
+        }
+
+        ContainerConfig.Container.Property rePortProp = connectorProp.getProperty("redirect-port");
+        if (rePortProp != null) {
+            try {
+                redirect = Integer.parseInt(rePortProp.value);
+            } catch (Exception e) {
+                Debug.logWarning("Invalid redirect-port number " + portProp.value + " not setting connector [" + connectorProp.name + "].", module);
+                redirect = 0;
+            }
+        }
+
+        Connector connector = embedded.createConnector(host, port, type);
+        connector.setEnableLookups(lookup);
+        if (redirect > 0) {
+            connector.setRedirectPort(redirect);
+        }
+
+        if ("https".equals(type)) {
+            configureSsl(connectorProp, connector);
+        }
+        embedded.addConnector(connector);
+
+        return connector;
+    }
+
+    protected void configureSsl(ContainerConfig.Container.Property connectorProp, Connector connector) throws ContainerException {
+        if (connector == null || connectorProp == null || !connector.getSecure()) {
+            throw new ContainerException("Not an SSL (secure) connector!");
+        }
+
+        CoyoteServerSocketFactory sf = (CoyoteServerSocketFactory) connector.getFactory();
+
+        String keystore = null;
+        String storeType = null;
+        String password = null;
+        String keyAlias = null;
+
+        ContainerConfig.Container.Property keyStoreProp = connectorProp.getProperty("keystore");
+        if (keyStoreProp == null) {
+            throw new ContainerException("No keystore setting found for SSL connector!");
+        }
+        keystore = keyStoreProp.value;
+
+        ContainerConfig.Container.Property keyStoreType = connectorProp.getProperty("keystore-type");
+        if (keyStoreType != null) {
+            storeType = keyStoreType.value;
+        }
+
+        ContainerConfig.Container.Property passwordProp = connectorProp.getProperty("password");
+        if (passwordProp != null) {
+            password = passwordProp.value;
+        }
+
+        ContainerConfig.Container.Property aliasProp = connectorProp.getProperty("key-alias");
+        if (aliasProp != null) {
+            keyAlias = aliasProp.value;
+        }
+
+        sf.setKeystoreFile(keystore);
+        if (password != null) {
+            sf.setKeystorePass(password);
+        }
+        if (storeType != null) {
+            sf.setKeystoreType(storeType);
+        }
+        if (keyAlias != null) {
+            sf.setKeyAlias(keyAlias);
+        }
+        connector.setFactory(sf);
+    }
+
+    protected void loadComponents() throws ContainerException {
+        if (embedded == null) {
+            throw new ContainerException("Cannot load web applications without Embedded instance!");
+        }
 
         // load the applications
         Collection componentConfigs = ComponentConfig.getAllComponents();
@@ -139,7 +284,7 @@ public class CatalinaContainer implements Container {
                 Iterator appInfos = component.getWebappInfos().iterator();
                 while (appInfos.hasNext()) {
                     ComponentConfig.WebappInfo appInfo = (ComponentConfig.WebappInfo) appInfos.next();
-                    //List virtualHosts = appInfo.getVirtualHosts();
+                    List virtualHosts = appInfo.getVirtualHosts();
                     Map initParameters = appInfo.getInitParameters();
                     Host host = (Host) hosts.get(appInfo.server + "._DEFAULT");
                     if (host == null) {
