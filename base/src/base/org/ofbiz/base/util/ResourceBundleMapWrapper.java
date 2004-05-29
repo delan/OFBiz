5068418c02a06e1368b6e6a8d8f6a6bb61f438b0
jonesde <jonesde@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-05-29 07:49:42 +0000

    i18n related stuff: now uses a special object to support fallback to other properties files when a property isn't in a certain file, for example falls back to English if a French label is missing instead of blowing up; also changed a BUNCH of page-action tags to content-action because in a page-action the envsetup.bsh hasn't run yet and certain things are missing, this was causing problems for forms with localized labels
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@2387 deaff617-5be1-0310-a1b7-ec6878f92ba9

---
+++ base/src/base/org/ofbiz/base/util/ResourceBundleMapWrapper.java
@@ -0,0 +1,158 @@
+/*
+ * $Id: ResourceBundleMapWrapper.java,v 1.1 2004/05/29 07:48:51 jonesde Exp $
+ *
+ *  Copyright (c) 2001-2004 The Open For Business Project - www.ofbiz.org
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ *  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ *  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ofbiz.base.util;
+
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+
+/**
+ * Generic ResourceBundle Map Wrapper, given ResourceBundle allows it to be used as a Map
+ *
+ * @author     <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
+ * @version    $Revision: 1.1 $
+ * @since      3.1
+ */
+public class ResourceBundleMapWrapper implements Map {
+
+    protected ResourceBundle resourceBundle;
+    protected Map topLevelMap;
+    
+    public ResourceBundleMapWrapper(ResourceBundle resourceBundle) {
+        this.resourceBundle = resourceBundle;
+        topLevelMap = new HashMap();
+        // NOTE: this should return all keys, including keys from parent ResourceBundles, if not then something else must be done here...
+        Enumeration keyNum = resourceBundle.getKeys();
+        while (keyNum.hasMoreElements()) {
+            String key = (String) keyNum.nextElement();
+            //resourceBundleMap.put(key, bundle.getObject(key));
+            Object value = resourceBundle.getObject(key);
+            topLevelMap.put(key, value);
+        }
+        topLevelMap.put("_RESOURCE_BUNDLE_", resourceBundle);
+    }
+    
+    /* (non-Javadoc)
+     * @see java.util.Map#size()
+     */
+    public int size() {
+        // this is an approximate size, won't include elements from parent bundles
+        return topLevelMap.size() - 1;
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Map#isEmpty()
+     */
+    public boolean isEmpty() {
+        return topLevelMap.isEmpty();
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Map#containsKey(java.lang.Object)
+     */
+    public boolean containsKey(Object arg0) {
+        if (topLevelMap.containsKey(arg0)) {
+            return true;
+        } else {
+            if (this.resourceBundle.getObject((String) arg0) != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Map#containsValue(java.lang.Object)
+     */
+    public boolean containsValue(Object arg0) {
+        throw new RuntimeException("Not implemented for ResourceBundleMapWrapper");
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Map#get(java.lang.Object)
+     */
+    public Object get(Object arg0) {
+        Object value = this.topLevelMap.get(arg0);
+        if (value == null) {
+            value = this.resourceBundle.getObject((String) arg0);
+        }
+        if (value == null) {
+            value = this.resourceBundle.getString((String) arg0);
+        }
+        return value;
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Map#put(java.lang.Object, java.lang.Object)
+     */
+    public Object put(Object arg0, Object arg1) {
+        throw new RuntimeException("Not implemented for ResourceBundleMapWrapper");
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Map#remove(java.lang.Object)
+     */
+    public Object remove(Object arg0) {
+        throw new RuntimeException("Not implemented for ResourceBundleMapWrapper");
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Map#putAll(java.util.Map)
+     */
+    public void putAll(Map arg0) {
+        throw new RuntimeException("Not implemented for ResourceBundleMapWrapper");
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Map#clear()
+     */
+    public void clear() {
+        throw new RuntimeException("Not implemented for ResourceBundleMapWrapper");
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Map#keySet()
+     */
+    public Set keySet() {
+        return this.topLevelMap.keySet();
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Map#values()
+     */
+    public Collection values() {
+        return this.topLevelMap.values();
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Map#entrySet()
+     */
+    public Set entrySet() {
+        return this.topLevelMap.entrySet();
+    }
+}
