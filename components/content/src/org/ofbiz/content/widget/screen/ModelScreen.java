44fcc2dff78be386bce056e8ce9ad5cf44a37592
jonesde <jonesde@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-07-10 15:49:14 +0000

    Added initial skeleton for the screen widget
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@2748 deaff617-5be1-0310-a1b7-ec6878f92ba9

---
+++ components/content/src/org/ofbiz/content/widget/screen/ModelScreen.java
@@ -0,0 +1,93 @@
+/*
+ * $Id: ModelScreen.java,v 1.1 2004/07/10 15:49:14 jonesde Exp $
+ *
+ * Copyright (c) 2004 The Open For Business Project - www.ofbiz.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ofbiz.content.widget.screen;
+
+import java.util.Map;
+
+import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.service.LocalDispatcher;
+import org.w3c.dom.Element;
+
+/**
+ * Widget Library - Screen model class
+ *
+ * @author     <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
+ * @version    $Revision: 1.1 $
+ * @since      3.1
+ */
+public class ModelScreen {
+
+    public static final String module = ModelScreen.class.getName();
+
+    protected GenericDelegator delegator;
+    protected LocalDispatcher dispatcher;
+
+    protected String name;
+
+    // ===== CONSTRUCTORS =====
+    /** Default Constructor */
+    public ModelScreen() {}
+
+    /** XML Constructor */
+    public ModelScreen(Element screenElement, GenericDelegator delegator, LocalDispatcher dispatcher) {
+        this.delegator = delegator;
+        this.dispatcher = dispatcher;
+
+        this.name = screenElement.getAttribute("name");
+
+    }
+
+    /**
+     * Renders this screen to a String, i.e. in a text format, as defined with the
+     * ScreenStringRenderer implementation.
+     *
+     * @param buffer The StringBuffer that the screen text will be written to
+     * @param context Map containing the screen context; the following are
+     *   reserved words in this context: parameters (Map), isError (Boolean),
+     *   itemIndex (Integer, for lists only, otherwise null), bshInterpreter,
+     *   screenName (String, optional alternate name for screen, defaults to the
+     *   value of the name attribute)
+     * @param screenStringRenderer An implementation of the ScreenStringRenderer
+     *   interface that is responsible for the actual text generation for
+     *   different screen elements; implementing your own makes it possible to
+     *   use the same screen definitions for many types of screen UIs
+     */
+    public void renderScreenString(StringBuffer buffer, Map context, ScreenStringRenderer screenStringRenderer) {
+    }
+
+    public LocalDispatcher getDispacher() {
+        return this.dispatcher;
+    }
+
+    public GenericDelegator getDelegator() {
+        return this.delegator;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
