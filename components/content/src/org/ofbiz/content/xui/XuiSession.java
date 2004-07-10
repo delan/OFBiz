57c021af1c3316e6fed7bbcc049260a728f8a993
ajzeneski <ajzeneski@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-07-10 19:55:06 +0000

    implemented XUI session
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@2751 deaff617-5be1-0310-a1b7-ec6878f92ba9

---
+++ components/content/src/org/ofbiz/content/xui/XuiSession.java
@@ -0,0 +1,141 @@
+/*
+ * $Id: XuiSession.java,v 1.1 2004/07/10 19:55:06 ajzeneski Exp $
+ *
+ * Copyright (c) 2004 The Open For Business Project - www.ofbiz.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package org.ofbiz.content.xui;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.service.LocalDispatcher;
+import org.ofbiz.service.GenericServiceException;
+import org.ofbiz.service.ServiceUtil;
+import org.ofbiz.base.util.GeneralException;
+import org.ofbiz.base.util.UtilValidate;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.base.util.Debug;
+
+/**
+ * 
+ * @author     <a href="mailto:jaz@ofbiz.org">Andy Zeneski</a>
+ * @version    $Revision: 1.1 $
+ * @since      3.1
+ */
+public class XuiSession {
+
+    public static final String module = XuiSession.class.getName();
+
+    protected GenericDelegator delegator = null;
+    protected LocalDispatcher dispatcher = null;
+    protected GenericValue userLogin = null;
+    protected Map attributes = new HashMap();
+    protected String id = null;
+
+
+    public XuiSession(String id, GenericDelegator delegator, LocalDispatcher dispatcher) {
+        this.id = id;
+        this.delegator = delegator;
+        this.dispatcher = dispatcher;
+        Debug.logInfo("Created XuiSession [" + id + "]", module);
+    }
+
+    public GenericDelegator getDelegator() {
+        return this.delegator;
+    }
+
+    public LocalDispatcher getDispatcher() {
+        return this.dispatcher;
+    }
+
+    public GenericValue getUserLogin() {
+        return this.userLogin;
+    }
+
+    public void setAttribute(String name, Object value) {
+        this.attributes.put(name, value);
+    }
+
+    public Object getAttribute(String name) {
+        return this.attributes.get(name);
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void checkLogin(String username, String password) throws UserLoginFailure {
+        // if already logged in just return
+        if (this.userLogin != null) {
+            return;
+        }
+
+        // check the required parameters and objects
+        if (dispatcher == null) {
+            throw new UserLoginFailure("Unable to log in; POS not configured propertly");
+        }
+        if (UtilValidate.isEmpty(username)) {
+            throw new UserLoginFailure("Username is missing");
+        }
+        if (UtilValidate.isEmpty(password)) {
+            throw new UserLoginFailure("Password is missing");
+        }
+
+        // call the login service
+        Map result = null;
+        try {
+            result = dispatcher.runSync("userLogin", UtilMisc.toMap("login.username", username, "login.password", password));
+        } catch (GenericServiceException e) {
+            throw new UserLoginFailure(e);
+        }
+
+        // check for errors
+        if (ServiceUtil.isError(result)) {
+            throw new UserLoginFailure(ServiceUtil.getErrorMessage(result));
+        } else {
+            this.userLogin = (GenericValue) result.get("userLogin");
+            if (this.userLogin == null) {
+                throw new UserLoginFailure("UserLogin return was not valid (null)");
+            }
+        }
+    }
+
+    public class UserLoginFailure extends GeneralException {
+        public UserLoginFailure() {
+            super();
+        }
+
+        public UserLoginFailure(String str) {
+            super(str);
+        }
+
+        public UserLoginFailure(String str, Throwable nested) {
+            super(str, nested);
+        }
+
+        public UserLoginFailure(Throwable nested) {
+            super(nested);
+        }
+    }
+}
