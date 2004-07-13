c3daa4a8dd7250d917157d881be442ddb11ca015
jonesde <jonesde@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-07-13 11:29:46 +0000

    Changed cache prefix to entitycache for easier filtering in the cache management pages in UtilCache
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@2766 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/entity/src/org/ofbiz/entity/cache/AbstractCache.java
+++ components/entity/src/org/ofbiz/entity/cache/AbstractCache.java
@@ -1,6 +1,29 @@
+/*
+ * $Id: AbstractCache.java,v 1.2 2004/07/13 11:29:46 jonesde Exp $
+ *
+ * Copyright (c) 2001-2004 The Open For Business Project - www.ofbiz.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
 package org.ofbiz.entity.cache;
 
-import java.util.Arrays;
 import org.ofbiz.base.util.UtilCache;
 import org.ofbiz.entity.GenericDelegator;
 
@@ -26,13 +49,13 @@ public abstract class AbstractCache {
     }
 
     public String getCacheNamePrefix() {
-        return "Cache." + id + "." + delegatorName + ".";
+        return "entitycache." + id + "." + delegatorName + ".";
     }
 
     public String[] getCacheNamePrefixes() {
         return new String[] {
-            "Cache." + id + ".${delegator-name}.",
-            "Cache." + id + "." + delegatorName + "."
+            "entitycache." + id + ".${delegator-name}.",
+            "entitycache." + id + "." + delegatorName + "."
         };
     }
 
