9a7668a0b9ecb2fb558a743b9846d6fa5b69d5f2
jonesde <jonesde@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-07-13 11:29:46 +0000

    Changed cache prefix to entitycache for easier filtering in the cache management pages in UtilCache
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@2766 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/entity/src/org/ofbiz/entity/cache/AbstractEntityConditionCache.java
+++ components/entity/src/org/ofbiz/entity/cache/AbstractEntityConditionCache.java
@@ -1,7 +1,29 @@
+/*
+ * $Id: AbstractEntityConditionCache.java,v 1.2 2004/07/13 11:29:46 jonesde Exp $
+ *
+ * Copyright (c) 2001-2004 The Open For Business Project - www.ofbiz.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
 package org.ofbiz.entity.cache;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -10,11 +32,10 @@ import java.util.Map;
 import org.ofbiz.base.util.UtilCache;
 import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.entity.GenericEntity;
-import org.ofbiz.entity.GenericValue;
 import org.ofbiz.entity.GenericPK;
+import org.ofbiz.entity.GenericValue;
 import org.ofbiz.entity.condition.EntityCondition;
 import org.ofbiz.entity.model.ModelEntity;
-import org.ofbiz.entity.util.EntityUtil;
 
 public abstract class AbstractEntityConditionCache extends AbstractCache {
 
