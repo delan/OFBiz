aedcfe9026350456ef331d75e16078724a07c628
jonesde <jonesde@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-07-15 02:11:58 +0000

    Implemented set action and service action in screen widget
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@2789 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/content/src/org/ofbiz/content/widget/screen/ModelScreenAction.java
+++ components/content/src/org/ofbiz/content/widget/screen/ModelScreenAction.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ModelScreenAction.java,v 1.2 2004/07/11 09:30:01 jonesde Exp $
+ * $Id: ModelScreenAction.java,v 1.3 2004/07/15 02:11:58 jonesde Exp $
  *
  * Copyright (c) 2004 The Open For Business Project - www.ofbiz.org
  *
@@ -23,6 +23,7 @@
  */
 package org.ofbiz.content.widget.screen;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -31,14 +32,21 @@ import java.util.Map;
 import org.ofbiz.base.util.BshUtil;
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.GeneralException;
+import org.ofbiz.base.util.UtilFormatOut;
+import org.ofbiz.base.util.UtilValidate;
 import org.ofbiz.base.util.UtilXml;
+import org.ofbiz.base.util.collections.FlexibleMapAccessor;
+import org.ofbiz.base.util.collections.OrderedMap;
+import org.ofbiz.base.util.string.FlexibleStringExpander;
+import org.ofbiz.service.GenericServiceException;
+import org.ofbiz.service.ModelService;
 import org.w3c.dom.Element;
 
 /**
  * Widget Library - Screen model class
  *
  * @author     <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
- * @version    $Revision: 1.2 $
+ * @version    $Revision: 1.3 $
  * @since      3.1
  */
 public abstract class ModelScreenAction {
@@ -59,11 +67,12 @@ public abstract class ModelScreenAction {
         Iterator actionElementIter = actionElementList.iterator();
         while (actionElementIter.hasNext()) {
             Element actionElement = (Element) actionElementIter.next();
-            // script | service | entity-one | entity-and | entity-condition
-            if ("script".equals(actionElement.getNodeName())) {
+            if ("set".equals(actionElement.getNodeName())) {
+                actions.add(new SetField(modelScreen, actionElement));
+            } else if ("script".equals(actionElement.getNodeName())) {
                 actions.add(new Script(modelScreen, actionElement));
             } else if ("service".equals(actionElement.getNodeName())) {
-                // TODO: implement this
+                actions.add(new Service(modelScreen, actionElement));
             } else if ("entity-one".equals(actionElement.getNodeName())) {
                 // TODO: implement this
             } else if ("entity-and".equals(actionElement.getNodeName())) {
@@ -84,10 +93,34 @@ public abstract class ModelScreenAction {
         }
     }
     
+    public static class SetField extends ModelScreenAction {
+        protected FlexibleMapAccessor field;
+        protected FlexibleMapAccessor fromField;
+        protected FlexibleStringExpander value;
+        
+        public SetField(ModelScreen modelScreen, Element setElement) {
+            super (modelScreen, setElement);
+            this.field = new FlexibleMapAccessor(setElement.getAttribute("field"));
+            this.fromField = UtilValidate.isNotEmpty(setElement.getAttribute("from-field")) ? new FlexibleMapAccessor(setElement.getAttribute("from-field")) : null;
+            this.value = UtilValidate.isNotEmpty(setElement.getAttribute("value")) ? new FlexibleStringExpander(setElement.getAttribute("value")) : null;
+            if (this.fromField != null && this.value != null) {
+                throw new IllegalArgumentException("Cannot specify a from-field [" + setElement.getAttribute("from-field") + "] and a value [" + setElement.getAttribute("value") + "] on the set action in a screen widget");
+            }
+        }
+        
+        public void runAction(Map context) {
+            if (this.fromField != null) {
+                this.field.put(context, this.fromField.get(context));
+            } else if (this.value != null) {
+                this.field.put(context, this.value.expandString(context));
+            }
+        }
+    }
+    
     public static class Script extends ModelScreenAction {
         protected String location;
         
-        public Script (ModelScreen modelScreen, Element scriptElement) {
+        public Script(ModelScreen modelScreen, Element scriptElement) {
             super (modelScreen, scriptElement);
             this.location = scriptElement.getAttribute("location");
         }
@@ -103,6 +136,74 @@ public abstract class ModelScreenAction {
                 }
             } else {
                 throw new IllegalArgumentException("For screen script actions the script type is not yet support for location:" + location);
+            }
+        }
+    }
+
+    public static class Service extends ModelScreenAction {
+        protected FlexibleStringExpander serviceName;
+        protected FlexibleMapAccessor resultMapName;
+        protected FlexibleStringExpander autoFieldMap;
+        protected Map fieldMap;
+        
+        public Service(ModelScreen modelScreen, Element setElement) {
+            super (modelScreen, setElement);
+            this.serviceName = new FlexibleStringExpander(setElement.getAttribute("service-name"));
+            this.resultMapName = UtilValidate.isNotEmpty(setElement.getAttribute("result-map-name")) ? new FlexibleMapAccessor(setElement.getAttribute("result-map-name")) : null;
+            this.autoFieldMap = new FlexibleStringExpander(setElement.getAttribute("auto-field-map"));
+            
+            List fieldMapElementList = UtilXml.childElementList(setElement, "field-map");
+            if (fieldMapElementList.size() > 0) {
+                this.fieldMap = new HashMap();
+                Iterator fieldMapElementIter = fieldMapElementList.iterator();
+                while (fieldMapElementIter.hasNext()) {
+                    Element fieldMapElement = (Element) fieldMapElementIter.next();
+                    // set the env-name for each field-name, noting that if no field-name is specified it defaults to the env-name
+                    this.fieldMap.put(
+                            new FlexibleMapAccessor(UtilFormatOut.checkEmpty(fieldMapElement.getAttribute("field-name"), fieldMapElement.getAttribute("env-name"))), 
+                            new FlexibleMapAccessor(fieldMapElement.getAttribute("env-name")));
+                }
+            }
+        }
+        
+        public void runAction(Map context) {
+            String serviceNameExpanded = this.serviceName.expandString(context);
+            if (UtilValidate.isEmpty(serviceNameExpanded)) {
+                throw new IllegalArgumentException("Service name was empty, expanded from: " + this.serviceName.getOriginal());
+            }
+            
+            String autoFieldMapString = this.autoFieldMap.expandString(context);
+            boolean autoFieldMapBool = !"false".equals(autoFieldMapString);
+            
+            try {
+                Map serviceContext = null;
+                if (autoFieldMapBool) {
+                    serviceContext = this.modelScreen.getDispacher().getDispatchContext().makeValidContext(serviceNameExpanded, ModelService.IN_PARAM, context);
+                } else {
+                    serviceContext = new HashMap();
+                }
+                
+                if (this.fieldMap != null) {
+                    Iterator fieldMapEntryIter = this.fieldMap.entrySet().iterator();
+                    while (fieldMapEntryIter.hasNext()) {
+                        Map.Entry entry = (Map.Entry) fieldMapEntryIter.next();
+                        FlexibleMapAccessor serviceContextFieldAcsr = (FlexibleMapAccessor) entry.getKey();
+                        FlexibleMapAccessor contextEnvAcsr = (FlexibleMapAccessor) entry.getValue();
+                        serviceContextFieldAcsr.put(serviceContext, contextEnvAcsr.get(context));
+                    }
+                }
+                
+                Map result = this.modelScreen.getDispacher().runSync(serviceNameExpanded, serviceContext);
+                
+                if (this.resultMapName != null) {
+                    this.resultMapName.put(context, result);
+                } else {
+                    context.putAll(result);
+                }
+            } catch (GenericServiceException e) {
+                String errMsg = "Error calling service with name " + serviceNameExpanded + ": " + e.toString();
+                Debug.logError(e, errMsg, module);
+                throw new IllegalArgumentException(errMsg);
             }
         }
     }
