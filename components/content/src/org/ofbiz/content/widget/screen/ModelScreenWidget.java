bcba9f77655bd25e558ba94da389427b73d9867b
byersa <byersa@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-07-16 18:53:24 +0000

    Issue number: Obtained from: Submitted by: Reviewed by:
    Filled in some of the pieces that are easy - Menu, Content and SubContent
    widgets. Made changes to MenuFactory to mimic those in FormFactory.
    
    
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@2797 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/content/src/org/ofbiz/content/widget/screen/ModelScreenWidget.java
+++ components/content/src/org/ofbiz/content/widget/screen/ModelScreenWidget.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ModelScreenWidget.java,v 1.2 2004/07/15 22:25:00 jonesde Exp $
+ * $Id: ModelScreenWidget.java,v 1.3 2004/07/16 18:53:24 byersa Exp $
  *
  * Copyright (c) 2004 The Open For Business Project - www.ofbiz.org
  *
@@ -29,28 +29,39 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Locale;
+import java.sql.Timestamp;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilFormatOut;
 import org.ofbiz.base.util.UtilXml;
+import org.ofbiz.base.util.UtilDateTime;
 import org.ofbiz.base.util.collections.MapStack;
 import org.ofbiz.base.util.string.FlexibleStringExpander;
 import org.ofbiz.content.widget.form.FormFactory;
 import org.ofbiz.content.widget.form.FormStringRenderer;
 import org.ofbiz.content.widget.form.ModelForm;
 import org.ofbiz.content.widget.html.HtmlFormRenderer;
+import org.ofbiz.content.content.ContentWorker;
+import org.ofbiz.content.widget.menu.MenuFactory;
+import org.ofbiz.content.widget.menu.MenuStringRenderer;
+import org.ofbiz.content.widget.menu.ModelMenu;
+import org.ofbiz.content.widget.html.HtmlMenuRenderer;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
+import org.ofbiz.base.util.GeneralException;
+import org.ofbiz.entity.GenericValue;
 
 /**
  * Widget Library - Screen model class
  *
  * @author     <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
- * @version    $Revision: 1.2 $
+ * @version    $Revision: 1.3 $
  * @since      3.1
  */
 public abstract class ModelScreenWidget {
@@ -89,13 +100,13 @@ public abstract class ModelScreenWidget {
             } else if ("form".equals(subElement.getNodeName())) {
                 subWidgets.add(new Form(modelScreen, widgetElement));
             } else if ("menu".equals(subElement.getNodeName())) {
-                // TODO: implement this
+                subWidgets.add(new Menu(modelScreen, widgetElement));
             } else if ("tree".equals(subElement.getNodeName())) {
-                // TODO: implement this
+                subWidgets.add(new ModelTree(modelScreen, widgetElement));
             } else if ("content".equals(subElement.getNodeName())) {
-                // TODO: implement this
+                subWidgets.add(new Content(modelScreen, widgetElement));
             } else if ("sub-content".equals(subElement.getNodeName())) {
-                // TODO: implement this
+                subWidgets.add(new SubContent(modelScreen, widgetElement));
             } else if ("platform-specific".equals(subElement.getNodeName())) {
                 subWidgets.add(new PlatformSpecific(modelScreen, widgetElement));
             } else {
@@ -348,6 +359,166 @@ public abstract class ModelScreenWidget {
 
         public void renderWidgetString(Writer writer, Map context, ScreenStringRenderer screenStringRenderer) {
             subWidget.renderWidgetString(writer, context, screenStringRenderer);
+        }
+    }
+
+    public static class Content extends ModelScreenWidget {
+        
+        protected FlexibleStringExpander contentId;
+        
+        public Content(ModelScreen modelScreen, Element subContentElement) {
+            super(modelScreen, subContentElement);
+
+            // put the text attribute first, then the pcdata under the element, if both are there of course
+            String contentId = UtilFormatOut.checkNull(subContentElement.getAttribute("content-id"));
+
+        }
+
+        public void renderWidgetString(Writer writer, Map context, ScreenStringRenderer screenStringRenderer) {
+            Locale locale = Locale.getDefault();
+            Boolean nullThruDatesOnly = new Boolean(false);
+            String mimeTypeId = "text/html";
+            Map map = null;
+            String expandedContentId = contentId.expandString(context);
+            try {
+                ContentWorker.renderContentAsTextCache(this.modelScreen.delegator, expandedContentId, writer, map, null, locale, mimeTypeId);
+
+            } catch(GeneralException e) {
+                String errMsg = "Error rendering included content with id [" + contentId + "] : " + e.toString();
+                Debug.logError(e, errMsg, module);
+                throw new RuntimeException(errMsg);
+            } catch(IOException e2) {
+                String errMsg = "Error rendering included content with id [" + contentId + "] : " + e2.toString();
+                Debug.logError(e2, errMsg, module);
+                throw new RuntimeException(errMsg);
+            }
+
+
+        }
+        
+        public String getContentId(Map context) {
+            return this.contentId.expandString(context);
+        }
+        
+    }
+
+
+    public static class SubContent extends ModelScreenWidget {
+        
+        protected FlexibleStringExpander contentId;
+        protected FlexibleStringExpander assocName;
+        
+        public SubContent(ModelScreen modelScreen, Element subContentElement) {
+            super(modelScreen, subContentElement);
+
+            // put the text attribute first, then the pcdata under the element, if both are there of course
+            String contentId = UtilFormatOut.checkNull(subContentElement.getAttribute("content-id"));
+            String assocName = UtilFormatOut.checkNull(subContentElement.getAttribute("assoc-name"));
+
+        }
+
+        public void renderWidgetString(Writer writer, Map context, ScreenStringRenderer screenStringRenderer) {
+            Locale locale = Locale.getDefault();
+            Boolean nullThruDatesOnly = new Boolean(false);
+            Timestamp fromDate = UtilDateTime.nowTimestamp();
+            String mimeTypeId = "text/html";
+            GenericValue userLogin = null;
+            Map map = null;
+            String expandedContentId = contentId.expandString(context);
+            String expandedAssocName = assocName.expandString(context);
+            HttpServletRequest request = (HttpServletRequest) context.get("request");
+            if (request != null) {
+                HttpSession session = request.getSession();
+                userLogin = (GenericValue)session.getAttribute("userLogin");
+            }
+            try {
+                ContentWorker.renderSubContentAsTextCache(this.modelScreen.delegator, expandedContentId, writer, expandedAssocName, null, map, locale, mimeTypeId, userLogin, fromDate, nullThruDatesOnly);
+            } catch(GeneralException e) {
+                String errMsg = "Error rendering included content with id [" + contentId + "] and assoc [" + assocName + "]: " + e.toString();
+                Debug.logError(e, errMsg, module);
+                throw new RuntimeException(errMsg);
+            } catch(IOException e2) {
+                String errMsg = "Error rendering included content with id [" + contentId + "] and assoc [" + assocName + "]: " + e2.toString();
+                Debug.logError(e2, errMsg, module);
+                throw new RuntimeException(errMsg);
+            }
+
+
+        }
+        
+        public String getContentId(Map context) {
+            return this.contentId.expandString(context);
+        }
+        
+        public String getAssocName(Map context) {
+            return this.assocName.expandString(context);
+        }
+        
+    }
+
+    public static class Menu extends ModelScreenWidget {
+        protected FlexibleStringExpander nameExdr;
+        protected FlexibleStringExpander locationExdr;
+        
+        public Menu(ModelScreen modelScreen, Element menuElement) {
+            super(modelScreen, menuElement);
+
+            this.nameExdr = new FlexibleStringExpander(menuElement.getAttribute("name"));
+            this.locationExdr = new FlexibleStringExpander(menuElement.getAttribute("location"));
+        }
+
+        public void renderWidgetString(Writer writer, Map context, ScreenStringRenderer screenStringRenderer) {
+            String name = this.getName(context);
+            String location = this.getLocation(context);
+            ModelMenu modelMenu = null;
+            try {
+                modelMenu = MenuFactory.getMenuFromLocation(this.getLocation(context), this.getName(context), this.modelScreen.getDelegator(), this.modelScreen.getDispacher());
+            } catch (IOException e) {
+                String errMsg = "Error rendering included menu named [" + name + "] at location [" + location + "]: " + e.toString();
+                Debug.logError(e, errMsg, module);
+                throw new RuntimeException(errMsg);
+            } catch (SAXException e) {
+                String errMsg = "Error rendering included menu named [" + name + "] at location [" + location + "]: " + e.toString();
+                Debug.logError(e, errMsg, module);
+                throw new RuntimeException(errMsg);
+            } catch (ParserConfigurationException e) {
+                String errMsg = "Error rendering included menu named [" + name + "] at location [" + location + "]: " + e.toString();
+                Debug.logError(e, errMsg, module);
+                throw new RuntimeException(errMsg);
+            }
+            
+            // try finding the menuStringRenderer by name in the context in case one was prepared and put there
+            MenuStringRenderer menuStringRenderer = (MenuStringRenderer) context.get("menuStringRenderer");
+            // if there was no menuStringRenderer put in place, now try finding the request/response in the context and creating a new one
+            if (menuStringRenderer == null) {
+                HttpServletRequest request = (HttpServletRequest) context.get("request");
+                HttpServletResponse response = (HttpServletResponse) context.get("response");
+                if (request != null && response != null) {
+                    menuStringRenderer = new HtmlMenuRenderer(request, response);
+                }
+            }
+            // still null, throw an error
+            if (menuStringRenderer == null) {
+                throw new IllegalArgumentException("Could not find a menuStringRenderer in the context, and could not find HTTP request/response objects need to create one.");
+            }
+            
+            StringBuffer renderBuffer = new StringBuffer();
+            modelMenu.renderMenuString(renderBuffer, context, menuStringRenderer);
+            try {
+                writer.write(renderBuffer.toString());
+            } catch (IOException e) {
+                String errMsg = "Error rendering included menu named [" + name + "] at location [" + location + "]: " + e.toString();
+                Debug.logError(e, errMsg, module);
+                throw new RuntimeException(errMsg);
+            }
+        }
+        
+        public String getName(Map context) {
+            return this.nameExdr.expandString(context);
+        }
+        
+        public String getLocation(Map context) {
+            return this.locationExdr.expandString(context);
         }
     }
 }
