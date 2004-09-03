f61371fd8878e8dfeb3de2a782bbd010609cda09
jonesde <jonesde@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-09-03 12:32:41 +0000

    Added license header, fixed unneeded exception catch, organized imports
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@3248 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/content/src/org/ofbiz/content/ContentManagementServices.java
+++ components/content/src/org/ofbiz/content/ContentManagementServices.java
@@ -1,3 +1,26 @@
+/*
+ * $Id$
+ *
+ *  Copyright (c) 2003-2004 The Open For Business Project - www.ofbiz.org
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ *  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ *  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package org.ofbiz.content;
 
 import java.sql.Timestamp;
@@ -35,8 +58,6 @@ import org.ofbiz.service.LocalDispatcher;
 import org.ofbiz.service.ModelService;
 import org.ofbiz.service.ServiceAuthException;
 import org.ofbiz.service.ServiceUtil;
-import org.ofbiz.entity.transaction.TransactionUtil;
-import org.ofbiz.entity.transaction.GenericTransactionException;
 
 
 /**
@@ -232,11 +253,7 @@ public class ContentManagementServices {
                     } else if (dataResourceTypeId.indexOf("_FILE") >=0) {
                         dataResource = (GenericValue)thisResult.get("dataResource");
                         context.put("dataResource", dataResource);
-                        try {
-                            thisResult = DataServices.createFileMethod(dctx, context);
-                        } catch(GenericServiceException e) {
-                            return ServiceUtil.returnError(e.getMessage());
-                        }
+                        thisResult = DataServices.createFileMethod(dctx, context);
                     } else if (dataResourceTypeId.equals("IMAGE_OBJECT")) {
                         ByteWrapper byteWrapper = (ByteWrapper)context.get("imageData");
                         if (byteWrapper != null) {
