f47f05a79d31bf320ed1f9126d08de9766264c77
jacopo <jacopo@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-09-23 08:27:24 +0000

    Modified code to handle configurable products. Fixed formatting issues.
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@3512 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/manufacturing/src/org/ofbiz/manufacturing/techdata/ProductHelper.java
+++ components/manufacturing/src/org/ofbiz/manufacturing/techdata/ProductHelper.java
@@ -21,9 +21,9 @@
  * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
  * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
  * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- * 
+ *
  */
- 
+
 package org.ofbiz.manufacturing.techdata;
 
 import java.sql.Timestamp;
@@ -64,48 +64,63 @@ import org.ofbiz.service.GenericServiceException;
 
 
 /**
- * Method to retrieve some manufacturing Product Information 
+ * Method to retrieve some manufacturing Product Information
  *
  * @author     <a href="mailto:olivier.heintz@nereide.biz">Olivier Heintz</a>
  * @author     <a href="mailto:nicolas@librenberry.net">Nicolas MALIN</a>
  * @author     <a href=mailto:tgrauss@free.fr">Thierry GRAUSS</a>
- * @version    $Rev:$
+ * @version    $Rev$
  * @since      3.0
  */
 public class ProductHelper {
-
+    
     public static final String module = ProductHelper.class.getName();
     public static final String resource = "ManufacturingUiLabels";
     
     
-	/**
-	  * Get the routing object for a product.
-  	  * @param product : the product for which the method return the routing
-   	  * @param quantity : the quantity to build
-	  * @param evenDate : the date used to filter the active routing
-   	  * @return GenericValue routing : the routing object
-	  **/
-    public static GenericValue getRouting(GenericValue product, double quantity, Timestamp eventDate ){
-		Debug.logInfo("getRouting called", module);
-		
-		//Looks for the routing associated with the product
+    /**
+     * Get the routing object for a product.
+     * @param product : the product for which the method return the routing
+     * @param quantity : the quantity to build
+     * @param evenDate : the date used to filter the active routing
+     * @return GenericValue routing : the routing object
+     **/
+    public static GenericValue getRouting(GenericValue product, double quantity, Timestamp eventDate, LocalDispatcher dispatcher){
+        Debug.logInfo("getRouting called", module);
+        
+        //Looks for the routing associated with the product
         List listRouting = null;
-		try{
-			listRouting = product.getRelated("WorkEffortGoodStandard",UtilMisc.toMap("statusId", "ROU_PROD_TEMPLATE"),UtilMisc.toList("estimatedQuantity DESC"));
+        try{
+            listRouting = product.getRelated("WorkEffortGoodStandard",UtilMisc.toMap("statusId", "ROU_PROD_TEMPLATE"),UtilMisc.toList("estimatedQuantity DESC"));
             if (listRouting.size()>0) listRouting = EntityUtil.filterByDate(listRouting,eventDate);
-		} catch (GenericEntityException e) {
-			Debug.logError(e,"Error : product.getRelated routing... productId="+product.getString("productId"), module);
-			return null;
-		}
-		if (listRouting == null || listRouting.size()==0) return null;
-		Iterator listRoutingIter = listRouting.iterator();
-        boolean find = false;
+        } catch (GenericEntityException e) {
+            Debug.logError(e,"Error : product.getRelated routing... productId="+product.getString("productId"), module);
+            return null;
+        }
+        if (listRouting == null || listRouting.size()==0) {
+            // if the product is a configuration, probably the routing is linked to the virtual product
+            Map serviceResponse = null;
+            try {
+                serviceResponse = dispatcher.runSync("getManufacturingComponents", UtilMisc.toMap("productId", product.getString("productId"), "quantity", new Double(quantity)));
+            } catch (GenericServiceException e) {
+                Debug.logError("Error : getManufacturingComponents for productId ="+product.getString("productId")+"--"+e.getMessage(), module);
+                return null;
+            }
+            String routingId = (String)serviceResponse.get("workEffortId");
+            try{
+                return product.getDelegator().findByPrimaryKey("WorkEffort", UtilMisc.toMap("workEffortId", routingId));
+            } catch (GenericEntityException e) {
+                return null;
+            }
+        }
+        Iterator listRoutingIter = listRouting.iterator();
+        boolean found = false;
         GenericValue routingProduct = null;
-		//Looks to determine which routing has a valid quantity
-		while (listRoutingIter.hasNext() && !find) {
+        //Looks to determine which routing has a valid quantity
+        while (listRoutingIter.hasNext() && !found) {
             routingProduct = (GenericValue) listRoutingIter.next();
             if (routingProduct.getDouble("estimatedQuantity")==null ||
-                 routingProduct.getDouble("estimatedQuantity").doubleValue() < quantity) find = true;
+            routingProduct.getDouble("estimatedQuantity").doubleValue() < quantity) found = true;
         }
         try{
             return routingProduct.getRelatedOneCache("WorkEffort");
@@ -114,60 +129,60 @@ public class ProductHelper {
             return null;
         }
     }
-
-	
-	/**
-	* test to know if the product is build or bought
-	* @param GenericValue product to test.
-	* @return <ul>
-	* <li>return true if the product is build</li>
-	* <li>return false if the product is bought</li></ul>
-	*/
-	public static boolean isBuild(GenericValue product) {
-		Debug.logInfo("isBuild called", module);
-		/* 
-		* TODO : we should ameliorate this. For the moment a component which has no subcomponent
-		* is always bought. In the real life, this is not always true (for example coal, gold, wood, and raw materials
-		* in general).
-		*/
-		//look for the list of subcomponents of the product
-		try{
+    
+    
+    /**
+     * test to know if the product is build or bought
+     * @param GenericValue product to test.
+     * @return <ul>
+     * <li>return true if the product is build</li>
+     * <li>return false if the product is bought</li></ul>
+     */
+    public static boolean isBuild(GenericValue product) {
+        Debug.logInfo("isBuild called", module);
+                /*
+                 * TODO : we should ameliorate this. For the moment a component which has no subcomponent
+                 * is always bought. In the real life, this is not always true (for example coal, gold, wood, and raw materials
+                 * in general).
+                 */
+        //look for the list of subcomponents of the product
+        try{
             List listBom = product.getRelatedByAndCache("MainProductAssoc",UtilMisc.toMap("productAssocTypeId","MANUF_COMPONENT"));
             if (listBom.size()>0) listBom = EntityUtil.filterByDate(listBom);
             if (listBom.size()>0) return true;
             else return false;
-		} catch (GenericEntityException e) {
-			Debug.logError("Error : listBomComponent", module);
-			return false;
-		}
-	}
-
+        } catch (GenericEntityException e) {
+            Debug.logError("Error : listBomComponent", module);
+            return false;
+        }
+    }
+    
     /**
      * Calcul the ATP Date of a list d'objet inventoryEventPlan
      * @param List The list of inventoryEventPlan that transmit to ftl
      * @return List of Double represant ATPDate
      */
-    public static List getVariationProduct(List inventoryList){
-	
-	Debug.logInfo("coucou de la methode", module);
-	ArrayList inventoryProductList;
+    public static List getVariationProduct(List inventoryList, LocalDispatcher dispatcher){
+        
+        Debug.logInfo("coucou de la methode", module);
+        ArrayList inventoryProductList;
         Map inventoryProductMap = new TreeMap();
         Map cumulativeAtpByEventMap = new TreeMap();
         GenericValue inventoryTmp;
         boolean firstOfList = true;
-
+        
         //regroupement des inventorys en fonction de l'article
         Iterator iter = inventoryList.iterator();
         while( iter.hasNext() ){
             inventoryTmp = (GenericValue) iter.next();
-	    inventoryProductList = (ArrayList)inventoryProductMap.get( inventoryTmp.getString("productId") );
-	    if( inventoryProductList == null ){
-		inventoryProductList = new ArrayList();
-		inventoryProductMap.put( inventoryTmp.getString("productId"), inventoryProductList);
-	    }
-	    inventoryProductList.add( inventoryTmp );
+            inventoryProductList = (ArrayList)inventoryProductMap.get( inventoryTmp.getString("productId") );
+            if( inventoryProductList == null ){
+                inventoryProductList = new ArrayList();
+                inventoryProductMap.put( inventoryTmp.getString("productId"), inventoryProductList);
+            }
+            inventoryProductList.add( inventoryTmp );
         }
-    
+        
         // iteration on the product found
         ArrayList keys = new ArrayList( inventoryProductMap.keySet() );
         for (Iterator iterMap = keys.iterator(); iterMap.hasNext(); ){
@@ -175,18 +190,18 @@ public class ProductHelper {
             inventoryProductList = (ArrayList)  inventoryProductMap.get(productId);
             double productAtp = 0;
             for (iter = inventoryProductList.iterator();iter.hasNext();){
-                //Acumulate all the InventoryEventPlanned.quantity 
+                //Acumulate all the InventoryEventPlanned.quantity
                 inventoryTmp = (GenericValue)iter.next();
                 if (firstOfList){
                     //Intinial ATP equal to the current product QOH
                     try {
-                         GenericValue product = inventoryTmp.getRelatedOneCache("Product");
-                         productAtp =  MrpServices.findProductMrpQoh(product);
-                     } catch (GenericEntityException e) {
-                             Debug.logError("Error : getRelatedOneCache Produc with productId="+inventoryTmp.getString("productId")+"--"+e.getMessage(), module);
-                             return null;
-                     }
-                     firstOfList = false;
+                        GenericValue product = inventoryTmp.getRelatedOneCache("Product");
+                        productAtp =  MrpServices.findProductMrpQoh(product, dispatcher);
+                    } catch (Exception e) {
+                        Debug.logError("Error : getRelatedOneCache Produc with productId="+inventoryTmp.getString("productId")+"--"+e.getMessage(), module);
+                        return null;
+                    }
+                    firstOfList = false;
                 }
                 Double doubleTmp = (Double)inventoryTmp.getDouble("eventQuantity");
                 productAtp += doubleTmp.doubleValue();
@@ -194,7 +209,7 @@ public class ProductHelper {
             }
             firstOfList = true;
         }
-
+        
         //construct the return list
         List eventPlannedAndCumulativeAtp = new LinkedList();
         iter = inventoryList.iterator();
@@ -204,6 +219,6 @@ public class ProductHelper {
             eventPlannedAndCumulativeAtp.add( productAtp );
         }
         return eventPlannedAndCumulativeAtp;
-
+        
     }
 }
