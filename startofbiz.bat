c0a0f67ab18ee413f66040a5634f073facf996c0
jonesde <jonesde@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-10-25 18:40:12 +0000

    Made some adjustments to this startup script, one recommended by Ismael Matos to handle spaces in the JAVA_HOME setting, also made it not run in a debugging mode by default, have that in a comment
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@3783 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- startofbiz.bat
+++ startofbiz.bat
@@ -1,2 +1,6 @@
 
-%JAVA_HOME%\bin\java -Xms256M -Xmx512M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar ofbiz.jar
+"%JAVA_HOME%\bin\java" -Xms256M -Xmx512M -jar ofbiz.jar
+
+REM This one is for more of a debugging mode
+REM "%JAVA_HOME%\bin\java" -Xms256M -Xmx512M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar ofbiz.jar
+
