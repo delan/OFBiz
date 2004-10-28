f4b1fef097860b5cde090e3aedd76e04c9484b99
jaz <jaz@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-10-28 20:54:34 +0000

    added first pass (not yet tested) of void order POS event
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@3808 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/pos/src/org/ofbiz/pos/component/Input.java
+++ components/pos/src/org/ofbiz/pos/component/Input.java
@@ -50,7 +50,7 @@ public class Input implements KeyboardReceiver, KeyListener {
     public static final String module = Input.class.getName();
     private static final String[] validFunc = { "LOGIN", "OPEN", "CLOSE", "UNLOCK", "MGRLOGIN", "PAID", "TOTAL",
                                                 "CREDIT", "GIFTCARD", "MSRINFO", "CHECK", "CHECKINFO", "REFNUM",
-                                                "QTY", "SHIFT" };
+                                                "QTY", "VOID", "SHIFT" };
 
     protected Stack functionStack = new Stack();
     protected Component[] pageComs = null;
