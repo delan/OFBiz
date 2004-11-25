1678c94e23c58b1b6870e3adc0438b4e7e5fad5d
jacopo <jacopo@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2004-11-25 11:35:04 +0000

    Bug fixes.
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@3979 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/manufacturing/src/org/ofbiz/manufacturing/bom/BOMHelper.java
+++ components/manufacturing/src/org/ofbiz/manufacturing/bom/BOMHelper.java
@@ -146,7 +146,7 @@ public class BOMHelper {
                 Debug.logError("Production Run for order item (" + orderItem.getString("orderId") + "/" + orderItem.getString("orderItemSeqId") + ") not created.", module);
                 continue;
             }
-            Map result = dispatcher.runSync("createProductionRunsForOrder", UtilMisc.toMap("productId", orderItem.getString("productId"), "quantity", shipmentPlan.getDouble("quantity"), "orderId", shipmentPlan.getString("orderId"), "orderItemSeqId", shipmentPlan.getString("orderItemSeqId"), "userLogin", userLogin));
+            Map result = dispatcher.runSync("createProductionRunsForOrder", UtilMisc.toMap("productId", orderItem.getString("productId"), "quantity", shipmentPlan.getDouble("quantity"), "orderId", shipmentPlan.getString("orderId"), "orderItemSeqId", shipmentPlan.getString("orderItemSeqId"), "shipmentId", shipmentId, "userLogin", userLogin));
         }
         } catch (Exception e) {
             // if there is an exception for either, the other probably wont work
