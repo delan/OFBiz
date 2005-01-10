98028f6d022eb6659455054eb0e1b1fb29b9fb11
jonesde <jonesde@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2005-01-10 14:04:19 +0000

    Added patches from Jacopo Cappellato for multi-field returns from lookup boxes to get IDs and descriptions back (descriptions typically for display only)
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@4232 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/images/webapp/images/calendar1.js
+++ components/images/webapp/images/calendar1.js
@@ -196,23 +196,27 @@ function cal_error (str_message) {
 	return null;
 }
 
+// ================= FIELD LOOKUP METHODS ============================
+
 function call_fieldlookup(target, viewName, formName,viewWidth,viewheight) {   
     var fieldLookup = new fieldLookup1(target);  
     if (! viewWidth) viewWidth = 250;
     if (! viewheight) viewheight = 200;
     fieldLookup.popup(viewName, formName, viewWidth, viewheight);
 }
-
 function call_fieldlookup2(target, viewName) {   
     var fieldLookup = new fieldLookup1(target);  
     fieldLookup.popup2(viewName);
 }
+function call_fieldlookup3(target, target2, viewName) {
+    var fieldLookup = new fieldLookup2(target, target2);
+    fieldLookup.popup2(viewName);
+}
 
 function fieldLookup1(obj_target) {
-
-	// assing methods
-	this.popup    = lookup_popup1;
-	this.popup2    = lookup_popup2;
+	// passing methods
+	this.popup = lookup_popup1;
+	this.popup2 = lookup_popup2;
 
 	// validate input parameters
 	if (!obj_target)
@@ -224,6 +228,29 @@ function fieldLookup1(obj_target) {
 	// register in global collections
 	this.id = lookups.length;
 	lookups[this.id] = this;
+}
+function fieldLookup2(obj_target, obj_target2) {
+    // passing methods
+    this.popup    = lookup_popup1;
+    this.popup2    = lookup_popup2;
+
+    // validate input parameters
+    if (!obj_target)
+        return lookup_error("Error calling the field lookup: no target control specified");
+    if (obj_target.value == null)
+        return cal_error("Error calling the field lookup: parameter specified is not valid tardet control");
+    this.target = obj_target;
+    // validate input parameters
+    if (!obj_target2)
+        return lookup_error("Error calling the field lookup: no target control specified");
+    if (obj_target2.value == null)
+        return cal_error("Error calling the field lookup: parameter specified is not valid tardet control");
+    this.target2 = obj_target2;
+
+
+    // register in global collections
+    this.id = lookups.length;
+    lookups[this.id] = this;
 }
 
 function lookup_popup1 (view_name, form_name, viewWidth, viewheight) {
