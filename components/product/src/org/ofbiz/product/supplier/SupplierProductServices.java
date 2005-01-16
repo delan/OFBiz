a32bb1ccb4da47b7983108699a9f869fe11c4aab
jaz <jaz@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2005-01-16 18:13:43 +0000

    JIRA patch OFBIZ-75
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@4288 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- components/product/src/org/ofbiz/product/supplier/SupplierProductServices.java
+++ components/product/src/org/ofbiz/product/supplier/SupplierProductServices.java
@@ -1,7 +1,9 @@
 package org.ofbiz.product.supplier;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import org.ofbiz.base.util.Debug;
@@ -13,7 +15,10 @@ import org.ofbiz.service.ServiceUtil;
 import org.ofbiz.entity.GenericDelegator;
 import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.condition.EntityExpr;
+import org.ofbiz.entity.condition.EntityOperator;
 import org.ofbiz.entity.util.EntityUtil;
+import org.ofbiz.product.product.ProductWorker;
 
 /**
  * Services for suppliers of products
@@ -28,8 +33,8 @@ public class SupplierProductServices {
     public static final String resource = "ProductUiLabels";
     
 /*
- * Parameters: productId, partyId
- * Result: a List of SupplierProduct entities for productId, filtered by date and optionally by partyId
+ * Parameters: productId, partyId, currencyUomId, quantity
+ * Result: a List of SupplierProduct entities for productId, filtered by date and optionally by partyId, ordered with lowest price first
  */
     public static Map getSuppliersForProduct(DispatchContext dctx, Map context) {
 		Map results = new HashMap();
@@ -38,9 +43,15 @@ public class SupplierProductServices {
 		GenericValue product = null;
 		String productId = (String) context.get("productId");
 		String partyId = (String) context.get("partyId");
-		
+                String currencyUomId = (String) context.get("currencyUomId");
+                Double quantity =(Double) context.get("quantity");
 		try {
 			product = delegator.findByPrimaryKeyCache("Product", UtilMisc.toMap("productId", productId));
+                        if (product==null) {
+                                results = ServiceUtil.returnSuccess();
+                                results.put("supplierProducts",null);
+                                return results;
+                        }
 			List supplierProducts = product.getRelatedCache("SupplierProduct");
 
 			// if there were no related SupplierProduct entities and the item is a variant, then get the SupplierProducts of the virtual parent product
@@ -57,13 +68,73 @@ public class SupplierProductServices {
 				supplierProducts = EntityUtil.filterByAnd(supplierProducts, UtilMisc.toMap("partyId", partyId));
 			}
 			
+                        // filter the list down by the currencyUomId if one is provided
+                        if (currencyUomId != null) {
+                           supplierProducts = EntityUtil.filterByAnd(supplierProducts, UtilMisc.toMap("currencyUomId", currencyUomId));
+                        }
+
+                        // filter the list down by the minimumOrderQuantity if one is provided
+                        if (quantity != null) {
+                                //minimumOrderQuantity
+                            supplierProducts = EntityUtil.filterByCondition(supplierProducts, new EntityExpr("minimumOrderQuantity", EntityOperator.LESS_THAN_EQUAL_TO, quantity));
+                        }
+
 			// filter the list down again by date before returning it
-			results = ServiceUtil.returnSuccess();
-			results.put("supplierProducts", EntityUtil.filterByDate(supplierProducts, UtilDateTime.nowTimestamp(), "availableFromDate", "availableThruDate", true));
-		} catch (GenericEntityException ex) {
+                        supplierProducts = EntityUtil.filterByDate(supplierProducts, UtilDateTime.nowTimestamp(), "availableFromDate", "availableThruDate", true);
+
+                        //sort resulting list of SupplierProduct entities by price in ASCENDING order
+                        supplierProducts = EntityUtil.orderBy(supplierProducts, UtilMisc.toList("lastPrice ASC"));
+
+                        results = ServiceUtil.returnSuccess();
+                        results.put("supplierProducts", supplierProducts);
+                } catch (GenericEntityException ex) {
+                      Debug.logError(ex, ex.getMessage(), module);
+                      return ServiceUtil.returnError(ex.getMessage());
+                }catch(Exception ex){
+                      Debug.logError(ex, ex.getMessage(), module);
+                      return ServiceUtil.returnError(ex.getMessage());
+                }
+         return results;
+    }
+
+    /*
+     * Parameters: partyId of a supplier and productFeatures, a Collection (usually List) of product features
+     * Service will convert each feature in the Collection, changing their idCode and description based on the
+     * SupplierProduct entity for that supplier party and feature, and return it as convertedProductFeatures
+     */
+    public static Map convertFeaturesForSupplier(DispatchContext dctx, Map context) {
+        Map results = new HashMap();
+        String partyId = (String) context.get("partyId");
+        Collection features = (Collection) context.get("productFeatures");
+
+        try {
+            if (partyId != null && features != null && features.size() > 0) {
+                // loop through all the features, find the related SupplierProductFeature for the given partyId, and
+                // substitue description and idCode
+                for (Iterator fI = features.iterator(); fI.hasNext(); ) {
+                    GenericValue nextFeature = (GenericValue) fI.next();
+                    List supplierFeatures = EntityUtil.filterByAnd(nextFeature.getRelated("SupplierProductFeature"),
+                            UtilMisc.toMap("partyId", partyId));
+                    GenericValue supplierFeature = null;
+
+                    if ((supplierFeatures != null) && (supplierFeatures.size() > 0)) {
+                        supplierFeature = (GenericValue) supplierFeatures.get(0);
+                        if (supplierFeature.get("description") != null) {
+                            nextFeature.put("description", supplierFeature.get("description"));
+                        }
+                        if (supplierFeature.get("idCode") != null) {
+                            nextFeature.put("idCode", supplierFeature.get("idCode"));
+                        }
+                        // TODO: later, do some kind of uom/quantity conoversion with the UomConversion entity
+                    }
+                }
+            }
+            results = ServiceUtil.returnSuccess();
+            results.put("convertedProductFeatures", features);
+        } catch (GenericEntityException ex) {
             Debug.logError(ex, ex.getMessage(), module);
             return ServiceUtil.returnError(ex.getMessage());
-		}
+        }
     	return results;
     }
 }
