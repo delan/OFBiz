7fc267e2dda4565bd2cbde745ee7c25bb6dfcf4d
jonesde <jonesde@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2005-04-02 15:34:19 +0000

    Minor import cleanup
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@4733 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- applications/order/src/org/ofbiz/order/order/OrderContentWrapper.java
+++ applications/order/src/org/ofbiz/order/order/OrderContentWrapper.java
@@ -42,8 +42,6 @@ import org.ofbiz.base.util.cache.UtilCache;
 import org.ofbiz.content.content.ContentWorker;
 import org.ofbiz.entity.GenericDelegator;
 import org.ofbiz.entity.GenericValue;
-import org.ofbiz.entity.model.ModelEntity;
-import org.ofbiz.entity.model.ModelUtil;
 import org.ofbiz.entity.util.EntityUtil;
 
 /**
