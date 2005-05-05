d03a0aca9fd1d46832d148ac85476584816123e0
sichen <sichen@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2005-05-05 19:50:46 +0000

    First tests which create and remove entities
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@4963 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- framework/entity/src/org/ofbiz/entity/test/EntityTestSuite.java
+++ framework/entity/src/org/ofbiz/entity/test/EntityTestSuite.java
@@ -25,10 +25,12 @@
 package org.ofbiz.entity.test;
 
 import java.util.List;
+import java.util.LinkedList;
 import java.util.Iterator;
 
 import junit.framework.TestCase;
 import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericValue;
 import org.ofbiz.entity.condition.EntityCondition;
 import org.ofbiz.entity.condition.EntityExpr;
@@ -56,8 +58,29 @@ public class EntityTestSuite extends TestCase {
         this.delegator = GenericDelegator.getGenericDelegator(DELEGATOR_NAME);
     }
 
-    // TODO: these tests should not expect seed data to exist; fix these to insert first and then run the tests
+    // TODO: create more find tests for all the testing entities
+    public void testMakeValue() throws Exception {
+        try {
+            // This method call directly stores a new value into the entity engine
+            delegator.create("TestingType", UtilMisc.toMap("testingTypeId", "TEST-1", "description", "Testing Type #4"));
+            
+            // This sequence creates the GenericValue entities first, puts them in a List, then calls the delegator to store them all
+            List newValues = new LinkedList();
+        
+            newValues.add(delegator.makeValue("TestingType", UtilMisc.toMap("testingTypeId", "TEST-2", "description", "Testing Type #2")));
+            newValues.add(delegator.makeValue("TestingType", UtilMisc.toMap("testingTypeId", "TEST-3", "description", "Testing Type #3")));
+            newValues.add(delegator.makeValue("TestingType", UtilMisc.toMap("testingTypeId", "TEST-4", "description", "Testing Type #4")));
+            delegator.storeAll(newValues);
+            
+            // finds a List of newly created values.  the second parameter specifies the fields to order results by.
+            List newlyCreatedValues = delegator.findAll("TestingType", UtilMisc.toList("testingTypeId"));
+            TestCase.assertEquals("4 TestingTypes found", newlyCreatedValues.size(), 4);
+        } catch (GenericEntityException ex) {
+            TestCase.fail(ex.getMessage());
+        }
+    }
     
+    // TODO: use a different testing entity here, one that was created above
     // test a simple find by and
     public void testFindByAnd() throws Exception {
         List values = delegator.findByAnd("UserLogin", UtilMisc.toMap("partyId", "admin"));
@@ -77,5 +100,20 @@ public class EntityTestSuite extends TestCase {
             Debug.logInfo("Testing ProductID - " + productId, module);
             TestCase.assertTrue("No product starting w/ GZ-", !productId.startsWith("GZ-"));
         }
+    }
+    
+    // TODO: remove all testing entities created above
+    public void testRemoveValue() throws Exception {
+        try {
+            // uses the delegator.removeAll method to remove all the entities.  Could have also used .removeValue, .remove____ methods 
+            List values = delegator.findAll("TestingType");
+            delegator.removeAll(values);
+            
+            // now make sure there are no more of these
+            values = delegator.findAll("TestingType");
+            TestCase.assertEquals("No more TestingTypes after remove all", values.size(), 0);
+        } catch (GenericEntityException ex) {
+            TestCase.fail(ex.getMessage());
+        }    
     }
 }
