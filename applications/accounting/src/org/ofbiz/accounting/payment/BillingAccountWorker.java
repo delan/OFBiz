1fcfb08ca2e746a7aa093151d52c89af598346f7
jonesde <jonesde@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2005-05-24 06:39:37 +0000

    Implemented feature to support use of billing accounts by agents of other customers; this includes demo data for all of this in the DemoOrderPeopleData.xml; this also includes some cleanups to put the logic to make a list of billing accounts for a party in a central method; some new seed data was also added for this; also added an ECA to ensure the PartyRole when associating a party to a BillingAccount
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@5026 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- applications/accounting/src/org/ofbiz/accounting/payment/BillingAccountWorker.java
+++ applications/accounting/src/org/ofbiz/accounting/payment/BillingAccountWorker.java
@@ -1,7 +1,7 @@
 /*
  * $Id$
  *
- *  Copyright (c) 2003 The Open For Business Project - www.ofbiz.org
+ *  Copyright (c) 2003-2005 The Open For Business Project - www.ofbiz.org
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a
  *  copy of this software and associated documentation files (the "Software"),
@@ -23,39 +23,94 @@
  */
 package org.ofbiz.accounting.payment;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
+
+import javolution.util.FastList;
 
 import org.ofbiz.accounting.invoice.InvoiceWorker;
 import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.GeneralException;
 import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.entity.GenericDelegator;
 import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityConditionList;
 import org.ofbiz.entity.condition.EntityExpr;
 import org.ofbiz.entity.condition.EntityOperator;
+import org.ofbiz.entity.util.EntityUtil;
 import org.ofbiz.order.order.OrderReadHelper;
 import org.ofbiz.service.DispatchContext;
+import org.ofbiz.service.LocalDispatcher;
 import org.ofbiz.service.ServiceUtil;
 
 /**
  * Worker methods for BillingAccounts
  *
  * @author     <a href="mailto:jaz@ofbiz.org">Andy Zeneski</a>
+ * @author     <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
  * @version    $Rev$
  * @since      2.1
  */
 public class BillingAccountWorker {
     
     public static final String module = BillingAccountWorker.class.getName();
+
+    public static List makePartyBillingAccountList(GenericValue userLogin, String currencyUomId, GenericDelegator delegator, LocalDispatcher dispatcher) throws GeneralException {
+        List billingAccountList = FastList.newInstance();
+        String partyId = userLogin.getString("partyId");
+
+        Map agentResult = dispatcher.runSync("getRelatedParties", UtilMisc.toMap("userLogin", userLogin, "partyIdFrom", partyId, 
+                "roleTypeIdFrom", "AGENT", "roleTypeIdTo", "CUSTOMER", "partyRelationshipTypeId", "AGENT", "includeFromToSwitched", "Y"));
+        if (ServiceUtil.isError(agentResult)) {
+            throw new GeneralException("Error while finding party BillingAccounts when getting Customers that this party is an agent of: " + ServiceUtil.getErrorMessage(agentResult));
+        }
+        List relatedPartyIdList = (List) agentResult.get("relatedPartyIdList");
+
+        EntityCondition barFindCond = new EntityConditionList(UtilMisc.toList(
+                new EntityExpr("partyId", EntityOperator.IN, relatedPartyIdList),
+                new EntityExpr("roleTypeId", EntityOperator.EQUALS, "BILL_TO_CUSTOMER")), EntityOperator.AND);
+        List billingAccountRoleList = delegator.findByCondition("BillingAccountRole", barFindCond, null, null);
+        billingAccountRoleList = EntityUtil.filterByDate(billingAccountRoleList);
+
+        if (billingAccountRoleList != null && billingAccountRoleList.size() > 0) {
+            double totalAvailable = 0.0;
+            TreeMap sortedAccounts = new TreeMap();
+            Iterator billingAcctIter = billingAccountRoleList.iterator();
+            while (billingAcctIter.hasNext()) {
+                GenericValue billingAccountRole = (GenericValue) billingAcctIter.next();        
+                GenericValue billingAccountVO = billingAccountRole.getRelatedOne("BillingAccount");
+                if (currencyUomId.equals(billingAccountVO.getString("accountCurrencyUomId"))) {
+                    double accountBalance = BillingAccountWorker.getBillingAccountBalance(billingAccountVO);
+                
+                    Map billingAccount = new HashMap(billingAccountVO);
+                    double accountLimit = 0.0;
+                    if (billingAccountVO.getDouble("accountLimit") != null) {
+                        accountLimit = billingAccountVO.getDouble("accountLimit").doubleValue();
+                    }
+                
+                    billingAccount.put("accountBalance", new Double(accountBalance)); 
+                    double accountAvailable = accountLimit - accountBalance;
+                    totalAvailable += accountAvailable;    
+                    sortedAccounts.put(new Double(accountAvailable), billingAccount);
+                }
+            }
+            
+            billingAccountList.addAll(sortedAccounts.values());
+        }
+        return billingAccountList;
+    }
     
-    public static double getBillingAccountBalance(GenericValue billingAccount) {
+    public static double getBillingAccountBalance(GenericValue billingAccount) throws GenericEntityException {
         return getBillingAccountBalance(billingAccount.getDelegator(), billingAccount.getString("billingAccountId"));
     }
         
-    public static double getBillingAccountBalance(GenericDelegator delegator, String billingAccountId) {
+    public static double getBillingAccountBalance(GenericDelegator delegator, String billingAccountId) throws GenericEntityException {
         double balance = 0.00;
         
         // first get all the pending orders (not cancelled, rejected or completed)
@@ -65,12 +120,9 @@ public class BillingAccountWorker {
         exprs1.add(new EntityExpr("statusId", EntityOperator.NOT_EQUAL, "ORDER_REJECTED"));
         exprs1.add(new EntityExpr("statusId", EntityOperator.NOT_EQUAL, "ORDER_CANCELLED"));
         exprs1.add(new EntityExpr("statusId", EntityOperator.NOT_EQUAL, "ORDER_COMPLETED"));
-        try {
-            orderHeaders = delegator.findByAnd("OrderHeader", exprs1);
-        } catch (GenericEntityException e) {
-            Debug.logError(e, "Trouble getting OrderHeader list", module);
-            return 0.01;
-        }
+
+        orderHeaders = delegator.findByAnd("OrderHeader", exprs1);
+
         if (orderHeaders != null) {
             Iterator ohi = orderHeaders.iterator();
             while (ohi.hasNext()) {
@@ -85,12 +137,9 @@ public class BillingAccountWorker {
         List exprs2 = new LinkedList();
         exprs2.add(new EntityExpr("billingAccountId", EntityOperator.EQUALS, billingAccountId));       
         exprs2.add(new EntityExpr("statusId", EntityOperator.NOT_EQUAL, "INVOICE_CANCELLED"));
-        try {
-            invoices = delegator.findByAnd("Invoice", exprs2);
-        } catch (GenericEntityException e) {
-            Debug.logError(e, "Trouble getting Invoice list", module);
-            return 0.01;
-        }
+
+        invoices = delegator.findByAnd("Invoice", exprs2);
+
         if (invoices != null) {
             Iterator ii = invoices.iterator();
             while (ii.hasNext()) {
@@ -103,12 +152,9 @@ public class BillingAccountWorker {
         List credits = null;
         List exprs3 = new LinkedList();
         exprs3.add(new EntityExpr("billingAccountId", EntityOperator.EQUALS, billingAccountId));       
-        try {
-            credits = delegator.findByAnd("PaymentApplication", exprs3);
-        } catch (GenericEntityException e) {
-            Debug.logError(e, "Trouble getting PaymentApplication list", module);
-            return 0.01;
-        }
+
+        credits = delegator.findByAnd("PaymentApplication", exprs3);
+
         if (credits != null) {
             Iterator ci = credits.iterator();
             while (ci.hasNext()) {
@@ -127,11 +173,13 @@ public class BillingAccountWorker {
         GenericDelegator delegator = dctx.getDelegator();
         String billingAccountId = (String) context.get("billingAccountId");
         GenericValue billingAccount = null;
+        Double accountBalance = null;
         try {
             billingAccount = delegator.findByPrimaryKey("BillingAccount", UtilMisc.toMap("billingAccountId", billingAccountId));
+            accountBalance = new Double(getBillingAccountBalance(delegator, billingAccountId));
         } catch (GenericEntityException e) {
             Debug.logError(e, module);
-            return ServiceUtil.returnError("Unable to locate billing account #" + billingAccountId);
+            return ServiceUtil.returnError("Error getting billing account or calculating balance for billing account #" + billingAccountId);
         }
         
         if (billingAccount == null) {
@@ -139,9 +187,8 @@ public class BillingAccountWorker {
         }
         
         Map result = ServiceUtil.returnSuccess();
-        result.put("accountBalance", new Double(getBillingAccountBalance(delegator, billingAccountId)));
+        result.put("accountBalance", accountBalance);
         result.put("billingAccount", billingAccount);
         return result;  
     }
- 
 }
