8e9499f345160908d2a316f4f78e7602509eee46
jonesde <jonesde@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2005-09-12 23:53:27 +0000

    Another fix from Al Byers to better handle parent/child forms
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@5715 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- framework/widget/src/org/ofbiz/widget/form/ModelForm.java
+++ framework/widget/src/org/ofbiz/widget/form/ModelForm.java
@@ -220,27 +220,23 @@ public class ModelForm {
                 this.defaultWidgetStyle = parent.defaultWidgetStyle;
                 this.defaultTooltipStyle = parent.defaultTooltipStyle;
                 this.itemIndexSeparator = parent.itemIndexSeparator;
-                this.fieldList = parent.fieldList;
-                this.fieldMap = parent.fieldMap;
+                //this.fieldList = parent.fieldList;
+                //this.fieldMap = parent.fieldMap;
                 this.separateColumns = parent.separateColumns;
                 this.targetType = parent.targetType;
                 this.defaultMapName = parent.defaultMapName;
                 this.targetWindowExdr = parent.targetWindowExdr;
                 this.hideHeader = parent.hideHeader;
                 
-                // TODO: amb, 2005-09-08
-                //This code should be removed because the parent child list could be 
-                // used by multiple forms and there is no point in setting the
-                // modelFields to any particular parent form
-                // This code is duplicated now in renderFormString, but I am
-                // not sure that a modelForm value isn't needed before then,
-                // so I hesitate to remove this code.
                 
-                // Update the parent's fields with this form
-                Iterator fieldListIter = this.fieldList.iterator();
+                // Create this fieldList/Map from clones of parent's
+                Iterator fieldListIter = parent.fieldList.iterator();
                 while (fieldListIter.hasNext()) {
-                    ModelFormField childField = (ModelFormField)fieldListIter.next();
-                    childField.setModelForm(this);
+                    ModelFormField parentChildField = (ModelFormField)fieldListIter.next();
+                    ModelFormField childField = new ModelFormField(this);
+                    childField.mergeOverrideModelFormField(parentChildField);
+                    this.fieldList.add(childField);
+                    this.fieldMap.put(childField.getName(), childField);
                 }
             }
         }
@@ -590,10 +586,6 @@ public class ModelForm {
             if (curPos > positions) {
                 positions = curPos;
             }
-            // This a hack made necessary because if two forms extend from the same
-            // extended resource, they use the same field list which has the 
-            // modelForm var set to the last one.
-            modelFormField.setModelForm(this);
        }
 
         if ("single".equals(this.type)) {
