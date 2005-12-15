f6be7f866528052c3ff56f2a85d13799d5ad9333
byersa <byersa@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2005-12-15 22:38:52 +0000

    Forgot to put copyright and disclaimer.
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@6343 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- applications/content/src/org/ofbiz/content/survey/PdfSurveyServices.java
+++ applications/content/src/org/ofbiz/content/survey/PdfSurveyServices.java
@@ -1,3 +1,26 @@
+/*
+ * $Id: PdfSurveyServices.java 5462 2005-08-05 18:35:48Z byersa $
+ *
+ *  Copyright (c) 2003-2005 The Open For Business Project - www.ofbiz.org
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ *  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ *  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package org.ofbiz.content.survey;
 
 import java.io.ByteArrayOutputStream;
@@ -47,6 +70,15 @@ import com.lowagie.text.pdf.PdfString;
 import com.lowagie.text.DocumentException;
 
 
+/**
+ * PdfSurveyServices Class
+ * 
+ * @author <a href="mailto:byersa@automationgroups.com">Al Byers</a>
+ * @version $Rev: 5462 $
+ * @since 3.2
+ * 
+ *  
+ */
 
 public class PdfSurveyServices {
 	
