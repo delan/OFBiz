15e07697ec546ec80b67219f8347243000948e9d
sichen <sichen@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2005-12-16 18:52:04 +0000

    Added rule based formatting method for specialized number formats, particularily spelled-out amounts for checks. This method uses ICU4J and is more flexible than UtilFormatOut.formatSpelledOutAmount. Specifically it allows full control over formatting behavior by locale. Plesae see the code comments for details on how to extend this.
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@6352 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- applications/accounting/src/org/ofbiz/accounting/util/UtilAccounting.java
+++ applications/accounting/src/org/ofbiz/accounting/util/UtilAccounting.java
@@ -30,7 +30,11 @@ package org.ofbiz.accounting.util;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.Locale;
 import java.math.BigDecimal;
+
+import com.ibm.icu.text.RuleBasedNumberFormat;
 
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilMisc;
@@ -262,5 +266,80 @@ public class UtilAccounting {
         else if ("ROUND_FLOOR".equals(value)) return BigDecimal.ROUND_FLOOR;
         else if ("ROUND_UNNECCESSARY".equals(value)) return BigDecimal.ROUND_UNNECESSARY;
         return -1;
+    }
+
+    // ICU4J rule sets for the en_US locale. To add more rules, expand this string.
+    // For reference, see the RbnfSampleRuleSets.java file distributed with ICU4J
+    public static final String ruleSet_en_US =
+        /*
+         * These rules format a number in one of the two styles often used
+         * on checks.  %dollars-and-hundredths formats cents as hundredths of
+         * a dollar (23.40 comes out as "twenty-three and 40/100 dollars").
+         * %dollars-and-cents formats in dollars and cents (23.40 comes out as
+         * "twenty-three dollars and forty cents")
+         */
+        "%dollars-and-cents:\n"
+        + "    x.0: << [and >%%cents>];\n"
+        + "    0.x: >%%cents>;\n"
+        + "    0: zero dollars; one dollar; =%%main= dollars;\n"
+        + "%%main:\n"
+        + "    zero; one; two; three; four; five; six; seven; eight; nine;\n"
+        + "    ten; eleven; twelve; thirteen; fourteen; fifteen; sixteen;\n"
+        + "        seventeen; eighteen; nineteen;\n"
+        + "    20: twenty[->>];\n"
+        + "    30: thirty[->>];\n"
+        + "    40: forty[->>];\n"
+        + "    50: fifty[->>];\n"
+        + "    60: sixty[->>];\n"
+        + "    70: seventy[->>];\n"
+        + "    80: eighty[->>];\n"
+        + "    90: ninety[->>];\n"
+        + "    100: << hundred[ >>];\n"
+        + "    1000: << thousand[ >>];\n"
+        + "    1,000,000: << million[ >>];\n"
+        + "    1,000,000,000: << billion[ >>];\n"
+        + "    1,000,000,000,000: << trillion[ >>];\n"
+        + "    1,000,000,000,000,000: =#,##0=;\n"
+        + "%%cents:\n"
+        + "    100: <%%main< cent[s];\n"
+        + "%dollars-and-hundredths:\n"
+        + "    x.0: <%%main< and >%%hundredths>/100;\n" // this used to end in 'dollars' but that should be added later
+        + "%%hundredths:\n"
+        + "    100: <00<;\n";
+
+    // hash map to store ICU4J rule sets keyed to Locale
+    public static HashMap rbnfRuleSets;
+    static {
+        rbnfRuleSets = new HashMap();
+        rbnfRuleSets.put(Locale.US, ruleSet_en_US);
+    }
+    
+    /**
+     * Method to format an amount using a custom rule set.
+     * Current rule sets available:
+     * 
+     * en_US
+     * %dollars-and-cents - 1,225.25 becomes "one thousand two hundred twenty five dollars and twenty five cents" (useful for checks)
+     * %dollars-and-hundreths - 1,225.25 becomes "one thousand two hundred twenty five and 25/00" (alternate for checks)
+     *
+     * @param   amount - the amount to format
+     * @param   rule - the name of the rule set to use (e.g., %dollars-and-hundredths)
+     * @param   locale - the Locale
+     * @return  formatted string or an empty string if there was an error
+     */
+    public static String formatRuleBasedAmount(double amount, String rule, Locale locale) {
+        String ruleSet = (String) rbnfRuleSets.get(locale);
+        if (ruleSet == null) {
+            Debug.logWarning("Cannot format rule based amount for locale " + locale.toString() + " because rule set for that locale does not exist", module);
+            return "";
+        }
+        RuleBasedNumberFormat formatter = new RuleBasedNumberFormat(ruleSet, locale);
+        String result = "";
+        try {
+            result = formatter.format(amount, rule);
+        } catch (Exception e) {
+            Debug.logError(e, "Failed to format amount " + amount + " using rule " + rule, module);
+        }
+        return result;
     }
 }
