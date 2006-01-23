7f55e30d7d128cb40c9807052f89df1f84ed14ea
jaz <jaz@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2006-01-23 07:36:14 +0000

    reverted back view handler to not use the FOPRender class, due to problems w/ barcode4j
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@6548 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- framework/widget/src/org/ofbiz/widget/screen/ScreenFopPdfViewHandler.java
+++ framework/widget/src/org/ofbiz/widget/screen/ScreenFopPdfViewHandler.java
@@ -28,13 +28,21 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.avalon.framework.logger.Log4JLogger;
+import org.apache.avalon.framework.logger.Logger;
+import org.apache.fop.apps.Driver;
+import org.apache.fop.image.FopImageFactory;
+import org.apache.fop.messaging.MessageHandler;
+import org.apache.fop.tools.DocumentInputSource;
 import org.ofbiz.base.util.Debug;
-import org.ofbiz.base.util.GeneralException;
-import org.ofbiz.webapp.view.FopRenderer;
+import org.ofbiz.base.util.UtilXml;
 import org.ofbiz.webapp.view.ViewHandlerException;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 /**
  * Uses XSL-FO formatted templates to generate PDF views
@@ -45,9 +53,9 @@ import org.ofbiz.webapp.view.ViewHandlerException;
  * @since      3.0
  */
 public class ScreenFopPdfViewHandler extends ScreenWidgetViewHandler {
-
+    
     public static final String module = ScreenFopPdfViewHandler.class.getName();
-
+    
     /**
      * @see org.ofbiz.content.webapp.view.ViewHandler#render(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
      */
@@ -64,25 +72,58 @@ public class ScreenFopPdfViewHandler extends ScreenWidgetViewHandler {
         if (Debug.verboseOn()) {
            // Debug.logVerbose("XSL-FO : " + writer.toString(), module);
         }
-
-        // render the byte array
-        ByteArrayOutputStream out = null;
+        
+        // configure logging for the FOP
+        Logger logger = new Log4JLogger(Debug.getLogger(module));
+        MessageHandler.setScreenLogger(logger);        
+                          
+        // load the FOP driver
+        Driver driver = new Driver();
+        driver.setRenderer(Driver.RENDER_PDF);
+        driver.setLogger(logger);
+                                        
+        /*
         try {
-            out = FopRenderer.render(writer);
-        } catch (GeneralException e) {
-            throw new ViewHandlerException(e.getMessage(), e.getNested());
+            String buf = writer.toString();
+            java.io.FileWriter fw = new java.io.FileWriter(new java.io.File("/tmp/xslfo.out"));
+            fw.write(buf.toString());
+            fw.close();
+        } catch (IOException e) {
+            throw new ViewHandlerException("Unable write to browser OutputStream", e);            
+        }                             
+        */
+        // read the XSL-FO XML Document
+        Document xslfo = null;
+        try {
+            xslfo = UtilXml.readXmlDocument(writer.toString());
+        } catch (Throwable t) {
+            throw new ViewHandlerException("Problems reading the parsed content to XML Document", t);
         }
+        
+        // create the output stream for the PDF
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        driver.setOutputStream(out);     
                 
+        // set the input source (XSL-FO) and generate the PDF        
+        InputSource is = new DocumentInputSource(xslfo);               
+        driver.setInputSource(is);        
+        try {
+            driver.run();
+            FopImageFactory.resetCache();
+        } catch (Throwable t) {
+            throw new ViewHandlerException("Unable to generate PDF from XSL-FO", t);
+        }
+                  
         // set the content type and length                    
-        response.setContentType("application/pdf");
+        response.setContentType("application/pdf");        
         response.setContentLength(out.size());
-
+        
         // write to the browser
         try {
             out.writeTo(response.getOutputStream());
             response.getOutputStream().flush();
         } catch (IOException e) {
-            throw new ViewHandlerException("Unable write to browser OutputStream", e);
-        }
+            throw new ViewHandlerException("Unable write to browser OutputStream", e);            
+        }                             
     }
 }
