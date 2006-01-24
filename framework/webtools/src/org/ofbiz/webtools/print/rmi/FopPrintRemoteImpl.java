010066aa0ed95fb93da25c23a0eb4c4765ba59fe
jaz <jaz@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2006-01-24 06:28:34 +0000

    updated remote interface; rendering for printing will now be handled by the applet. This just needs to obtain the rendered XSL-FO from FTL
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@6557 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- framework/webtools/src/org/ofbiz/webtools/print/rmi/FopPrintRemoteImpl.java
+++ framework/webtools/src/org/ofbiz/webtools/print/rmi/FopPrintRemoteImpl.java
@@ -24,8 +24,6 @@
  */
 package org.ofbiz.webtools.print.rmi;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.rmi.RemoteException;
@@ -35,17 +33,6 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.Locale;
 import java.util.Map;
 
-import org.apache.avalon.framework.logger.Log4JLogger;
-import org.apache.avalon.framework.logger.Logger;
-import org.apache.fop.apps.Driver;
-import org.apache.fop.image.FopImageFactory;
-import org.apache.fop.messaging.MessageHandler;
-import org.apache.fop.tools.DocumentInputSource;
-import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
-
-import org.ofbiz.base.util.Debug;
-import org.ofbiz.base.util.UtilXml;
 import org.ofbiz.service.DispatchContext;
 import org.ofbiz.widget.html.HtmlScreenRenderer;
 import org.ofbiz.widget.screen.ScreenRenderer;
@@ -57,9 +44,11 @@ import org.ofbiz.widget.screen.ScreenRenderer;
  * @version    $Rev$
  * @since      3.5
  */
+
 public class FopPrintRemoteImpl extends UnicastRemoteObject implements FopPrintRemote {
 
     public static final String module = FopPrintRemoteImpl.class.getName();
+    
     protected HtmlScreenRenderer htmlScreenRenderer = new HtmlScreenRenderer();
     protected DispatchContext dctx = null;
     protected Locale locale = null;
@@ -70,21 +59,7 @@ public class FopPrintRemoteImpl extends UnicastRemoteObject implements FopPrintR
         this.dctx = dctx;
     }
 
-    public byte[] getFopPdf(String screenUri, Map parameters) throws RemoteException {
-        // make sure the locale object is in the parameters
-        if (parameters.get("locale") == null) {
-            if (this.locale == null) {
-                this.locale = Locale.getDefault();
-            }
-            parameters.put("locale", locale);
-        }
-
-        Debug.log("Attempt to render screen [" + screenUri + "] using parameters: " + parameters, module);
-        return render(screenUri, parameters);
-
-    }
-
-    public byte[] render(String screen, Map parameters) throws RemoteException {
+    public byte[] getXslFo(String screen, Map parameters) throws RemoteException {
         // render and obtain the XSL-FO
         Writer writer = new StringWriter();
         try {
@@ -92,47 +67,8 @@ public class FopPrintRemoteImpl extends UnicastRemoteObject implements FopPrintR
             screens.populateContextForService(dctx, parameters);
             screens.render(screen);
         } catch (Throwable t) {
-            throw new RemoteException("Problems with the response writer/output stream", t);
+            throw new RemoteException("Problems rendering FOP XSL-FO", t);
         }
-
-        // configure logging for the FOP
-        Logger logger = new Log4JLogger(Debug.getLogger(module));
-        MessageHandler.setScreenLogger(logger);
-
-        // load the FOP driver
-        Driver driver = new Driver();
-        driver.setRenderer(Driver.RENDER_PDF);
-        driver.setLogger(logger);
-
-        // read the XSL-FO XML Document
-        Document xslfo = null;
-        try {
-            xslfo = UtilXml.readXmlDocument(writer.toString());
-        } catch (Throwable t) {
-            throw new RemoteException("Problems reading the parsed content to XML Document", t);
-        }
-
-        // create the output stream for the PDF
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        driver.setOutputStream(out);
-
-        // set the input source (XSL-FO) and generate the PDF
-        InputSource is = new DocumentInputSource(xslfo);
-        driver.setInputSource(is);
-        try {
-            driver.run();
-            FopImageFactory.resetCache();
-        } catch (Throwable t) {
-            throw new RemoteException("Unable to generate PDF from XSL-FO", t);
-        }
-
-        byte[] bytes = out.toByteArray();
-        try {
-            out.close();
-        } catch (IOException e) {
-            throw new RemoteException("Problem closing output stream", e);
-        }
-
-        return bytes;
+        return writer.toString().getBytes();
     }
 }
