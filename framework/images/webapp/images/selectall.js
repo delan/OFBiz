605b20c59e3f9675e24f6320f66f29d9f44d8c79
jonesde <jonesde@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2006-02-17 18:51:20 +0000

    Patch from Jacopo for OFBIZ-564 still not working (4 out of 4 hunks failed), so applied manually, also added license header
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@6766 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- framework/images/webapp/images/selectall.js
+++ framework/images/webapp/images/selectall.js
@@ -1,3 +1,24 @@
+/*
+ *  Copyright (c) 2003-2006 The Open For Business Project - www.ofbiz.org
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ *  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ *  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ *  THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 // Check Box Select/Toggle Functions for Select/Toggle All
 
@@ -5,7 +26,7 @@ function toggle(e) {
     e.checked = !e.checked;    
 }
 
-function checkToggle(e) {
+function checkToggleDefault(e) {
     checkToggle(e, "selectAllForm");
 }
 function checkToggle(e, formName) {
@@ -25,7 +46,7 @@ function checkToggle(e, formName) {
     }
 }
 
-function toggleAll(e) {
+function toggleAllDefault(e) {
     toggleAll(e, "selectAllForm");
 }
 function toggleAll(e, formName) {
@@ -39,7 +60,7 @@ function toggleAll(e, formName) {
     }
 }
 
-function selectAll() {
+function selectAllDefault() {
     selectAll("selectAllForm");
 }
 function selectAll(formName) {
@@ -53,7 +74,7 @@ function selectAll(formName) {
     }     
 }
 
-function removeSelected() {
+function removeSelectedDefault() {
     removeSelected("selectAllForm");
 }
 function removeSelected(formName) {
@@ -163,4 +184,3 @@ function confirmActionFormLink(msg, formName) {
         return false;
     }
 }
-
