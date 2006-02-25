88178835c221808ac22fb68ca61ca996c3858ece
jaz <jaz@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2006-02-25 01:53:26 +0000

    added file utilities
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@6844 deaff617-5be1-0310-a1b7-ec6878f92ba9

---
+++ base/src/base/org/ofbiz/base/util/FileUtil.java
@@ -0,0 +1,89 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2001-2006 The Open For Business Project - www.ofbiz.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package org.ofbiz.base.util;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * File Utilities
+ *
+ * @author     <a href="mailto:jaz@ofbiz.org">Andy Zeneski</a>
+ * @version    $Rev$
+ * @since      3.5
+ */
+public class FileUtil {
+
+    public static final String module = FileUtil.class.getName();
+
+    public static void writeString(String fileName, String s) throws IOException {
+        writeString(null, fileName, s);
+    }
+
+    public static void writeString(String path, String fileName, String s) throws IOException {
+        // make sure the export directory exists
+        if (UtilValidate.isNotEmpty(path)) {
+            path = path.replaceAll("\\\\", "/");
+            File parentDir = new File(path);
+            if (!parentDir.exists()) {
+                if (!parentDir.mkdir()) {
+                    throw new IOException("Cannot create directory for path: " + path);
+                }
+            }
+
+            // build the filename with the path
+            if (!path.endsWith("/")) {
+                path = path + "/";
+            }
+            if (fileName.startsWith("/")) {
+                fileName = fileName.substring(1);
+            }
+            fileName = path + fileName;
+        }
+
+        if (UtilValidate.isNotEmpty(fileName)) {
+            // write the string
+            Writer out = null;
+            try {
+                out = new BufferedWriter(new FileWriter(fileName));
+                out.write(s + System.getProperty("line.separator"));
+            } catch (IOException e) {
+                Debug.logError(e, module);
+                throw e;
+            } finally {
+                if (out != null) {
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        Debug.logError(e, module);
+                    }
+                }
+            }
+        }
+    }
+}
