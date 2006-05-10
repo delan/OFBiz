8e3b47326da75ad3a7f3b277449c794d520247f2
jonesde <jonesde@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2006-05-10 00:08:10 +0000

    Initial implementation of test runner in a new testtools component; supports specifying tests in the ofbiz-component.xml file instead of in the test-containers.xml file; entity and service tests are now moved over; already support JUnit tests, still some coding to do for service tests and Jython tests, which are intended to be from recorded web sessions using one of the various tools for this, still need to decide on one of those to centralize on as well as generic Jython scripts may not be sufficient for this, may be specific to tool generating them
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@7551 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- framework/testtools/src/org/ofbiz/testtools/TestRunContainer.java
+++ framework/testtools/src/org/ofbiz/testtools/TestRunContainer.java
@@ -1,47 +1,39 @@
 /*
- * $Id$
+ * $Id: $
  *
- * Copyright (c) 2004 The Open For Business Project - www.ofbiz.org
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
- * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
- * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
+ * Copyright 2006-2006 The Apache Software Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
-package org.ofbiz.base.container;
+package org.ofbiz.testtools;
 
 import java.util.Enumeration;
-import java.util.Iterator;
 
 import junit.framework.TestResult;
 import junit.framework.TestSuite;
-import org.ofbiz.base.util.Debug;
-import org.ofbiz.base.util.ObjectType;
 
+import org.ofbiz.base.container.Container;
+import org.ofbiz.base.container.ContainerException;
+import org.ofbiz.base.util.Debug;
 
 /**
+ * A Container implementation to run the tests configured through this testtools stuff.
  *
- * @author     <a href="mailto:jaz@ofbiz.org">Andy Zeneski</a>
- * @version    $Rev$
- * @since      3.1
+ * @author     <a href="mailto:jonesde@ofbiz.org">David E. Jones</a>
  */
-public class JunitContainer implements Container {
+public class TestRunContainer implements Container {
 
-    public static final String module = JunitContainer.class.getName();
+    public static final String module = TestRunContainer.class.getName();
     protected TestResult results = null;
     protected String configFile = null;
 
@@ -53,27 +45,14 @@ public class JunitContainer implements Container {
     }
 
     public boolean start() throws ContainerException {
-        ContainerConfig.Container jc = ContainerConfig.getContainer("junit-container", configFile);
+        //ContainerConfig.Container jc = ContainerConfig.getContainer("junit-container", configFile);
 
         // get the tests to run
-        Iterator ti = jc.properties.values().iterator();
-        if (ti == null) {
-            Debug.log("No tests to load", module);
-            return true;
-        }
+        JunitSuiteWrapper jsWrapper = new JunitSuiteWrapper();
 
         // load the tests into the suite
         TestSuite suite = new TestSuite();
-        while (ti.hasNext()) {
-            ContainerConfig.Container.Property prop = (ContainerConfig.Container.Property) ti.next();
-            Class clz = null;
-            try {
-                clz = ObjectType.loadClass(prop.value);
-                suite.addTestSuite(clz);
-            } catch (Exception e) {
-                Debug.logError(e, "Unable to load test suite class : " + prop.value, module);
-            }
-        }
+        jsWrapper.populateTestSuite(suite);
 
         // holder for the results
         results = new TestResult();
