0bd13d7fffab4d028cab6ea2e0e23b2368202082
jaz <jaz@deaff617-5be1-0310-a1b7-ec6878f92ba9>
2006-05-11 14:07:02 +0000

    added method to help find an existing party; will be expanding this to better handle variations in address (i.e. rd vs road, e vs east etc)
    git-svn-id: http://svn.ofbiz.org/svn/ofbiz/trunk@7562 deaff617-5be1-0310-a1b7-ec6878f92ba9

--- applications/party/src/org/ofbiz/party/party/PartyWorker.java
+++ applications/party/src/org/ofbiz/party/party/PartyWorker.java
@@ -29,17 +29,26 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.jsp.PageContext;
+
+import javolution.util.FastList;
+import javolution.util.FastSet;
 
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.base.util.UtilFormatOut;
 import org.ofbiz.base.util.UtilValidate;
+import org.ofbiz.base.util.GeneralException;
 import org.ofbiz.entity.GenericDelegator;
 import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.condition.EntityExpr;
+import org.ofbiz.entity.condition.EntityOperator;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityConditionList;
 import org.ofbiz.entity.model.ModelEntity;
 import org.ofbiz.entity.util.EntityUtil;
 
@@ -142,4 +151,119 @@ public class PartyWorker {
             return null;
         }
     }
+
+    public static String findPartyId(GenericDelegator delegator, String address1, String address2, String city,
+                            String stateProvinceGeoId, String postalCode, String postalCodeExt, String countryGeoId,
+                            String firstName, String middleName, String lastName) throws GeneralException {
+
+        // address information
+        if (firstName == null || lastName == null || address1 == null || city == null || postalCode == null) {
+            throw new IllegalArgumentException();
+        }
+
+        List addrExprs = FastList.newInstance();
+        if (stateProvinceGeoId != null) {
+            if ("**".equals(stateProvinceGeoId)) {
+                Debug.logWarning("Illegal state code passed!", module);
+            } else if ("NA".equals(stateProvinceGeoId)) {
+                addrExprs.add(new EntityExpr("stateProvinceGeoId", EntityOperator.EQUALS, "_NA_"));
+            } else {
+                addrExprs.add(new EntityExpr("stateProvinceGeoId", EntityOperator.EQUALS, stateProvinceGeoId.toUpperCase()));
+            }
+        }
+
+        if (!postalCode.startsWith("*")) {
+            if (postalCode.length() == 10 && postalCode.indexOf("-") != -1) {
+                String[] zipSplit = postalCode.split("-", 2);
+                postalCode = zipSplit[0];
+                postalCodeExt = zipSplit[1];
+            }
+            addrExprs.add(new EntityExpr("postalCode", EntityOperator.EQUALS, postalCode));
+        }
+
+        if (postalCodeExt != null) {
+            addrExprs.add(new EntityExpr("postalCodeExt", EntityOperator.EQUALS, postalCodeExt));
+        }
+
+        city = city.replaceAll("'", "\\\\'");
+        addrExprs.add(new EntityExpr("city", true, EntityOperator.EQUALS, city, true));
+
+        if (countryGeoId != null) {
+            addrExprs.add(new EntityExpr("countryGeoId", EntityOperator.EQUALS, countryGeoId.toUpperCase()));
+        }
+
+        List sort = UtilMisc.toList("-fromDate");
+        EntityCondition addrCond = new EntityConditionList(addrExprs, EntityOperator.AND);
+        List addresses = EntityUtil.filterByDate(delegator.findByCondition("PartyAndPostalAddress", addrCond, null, sort));
+        //Debug.log("Checking for matching address: " + addrCond.toString() + "[" + addresses.size() + "]", module);
+
+        Set validParty = FastSet.newInstance();
+        if (UtilValidate.isNotEmpty(addresses)) {
+            // check the address line
+            Iterator v = addresses.iterator();
+            while (v.hasNext()) {
+                GenericValue address = (GenericValue) v.next();
+
+                // address 1 field
+                String addr1Source = address1.toUpperCase().replaceAll("\\W", "");
+                String addr1Target = address.getString("address1");
+
+                if (addr1Target != null) {
+                    addr1Target = addr1Target.toUpperCase().replaceAll("\\W", "");
+                    Debug.log("Comparing address1 : " + addr1Source + " / " + addr1Target, module);
+                    if (addr1Target.equals(addr1Source)) {
+
+                        // address 2 field
+                        if (address2 != null) {
+                            String addr2Source = address2.toUpperCase().replaceAll("\\W", "");
+                            String addr2Target = address.getString("address2");
+                            if (addr2Target != null) {
+                                addr2Target = addr2Target.toUpperCase().replaceAll("\\W", "");
+                                Debug.log("Comparing address2 : " + addr2Source + " / " + addr2Target, module);
+
+                                if (addr2Source.equals(addr2Target)) {
+                                    Debug.log("Matching address2; adding valid address", module);
+                                    validParty.add(address.getString("partyId"));
+                                }
+                            }
+                        } else {
+                            if (address.get("address2") == null) {
+                                Debug.log("No address2; adding valid address", module);
+                                validParty.add(address.getString("partyId"));
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (UtilValidate.isNotEmpty(validParty)) {
+                Iterator a = validParty.iterator();
+                while (a.hasNext()) {
+                    String partyId = (String) a.next();
+                    if (UtilValidate.isNotEmpty(partyId)) {
+                        GenericValue p = delegator.findByPrimaryKey("Person", UtilMisc.toMap("partyId", partyId));
+                        if (p != null) {
+                            String fName = p.getString("firstName");
+                            String lName = p.getString("lastName");
+                            String mName = p.getString("middleName");
+                            if (lName.toUpperCase().equals(lastName.toUpperCase())) {
+                                if (fName.toUpperCase().equals(firstName.toUpperCase())) {
+                                    if (mName != null && middleName != null) {
+                                        if (mName.toUpperCase().equals(middleName.toUpperCase())) {
+                                            return partyId;
+                                        }
+                                    } else if (middleName == null) {
+                                        return partyId;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
 }
