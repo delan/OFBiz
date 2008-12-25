delan/OFBiz

commit 70cfcb21601cad847aefbc7c31587c14fae4db42

A slightly modified patch from Bilgin Ibryam "Allow multi-pagination in a page" (https://issues.apache.org/jira/browse/OFBIZ-1935) - OFBIZ-1935
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@725053 13f79535-47bb-0310-9956-ffa450edef68

framework/widget/src/org/ofbiz/widget/ModelWidget.java
@@ -112,4 +112,27 @@ public class ModelWidget implements Serializable {
         }
         return result;
     }
+
+    public int getPaginatorNumber(Map<String, Object> context) {
+        int paginator_number = 0; 
+        Map<String, Object> globalCtx = UtilGenerics.checkMap(context.get("globalContext"));      
+        if (globalCtx != null) {
+            Integer paginateNumberInt= (Integer)globalCtx.get("PAGINATOR_NUMBER");
+            if (paginateNumberInt == null) {    
+                paginateNumberInt = Integer.valueOf(0);
+                globalCtx.put("PAGINATOR_NUMBER", paginateNumberInt);
+            }
+            paginator_number = paginateNumberInt.intValue();
+        }    
+        return paginator_number;
+    }      
+    
+    public void incrementPaginatorNumber(Map<String, Object> context) {    
+        Map<String, Object> globalCtx = UtilGenerics.checkMap(context.get("globalContext"));
+        if (globalCtx != null) {
+            Integer paginateNumberInt = Integer.valueOf(getPaginatorNumber(context) + 1);
+            globalCtx.put("PAGINATOR_NUMBER", paginateNumberInt);
+        }     
+    }
+    
 }

framework/widget/src/org/ofbiz/widget/html/HtmlScreenRenderer.java
@@ -246,6 +246,7 @@ public class HtmlScreenRenderer extends HtmlWidgetRenderer implements ScreenStri
         }
 
         // get the parametrized pagination index and size fields
+        int paginatoNumber = modelForm.getPaginatorNumber(context);
         String viewIndexParam = modelForm.getPaginateIndexField(context);
         String viewSizeParam = modelForm.getPaginateSizeField(context);
 
@@ -271,8 +272,8 @@ public class HtmlScreenRenderer extends HtmlWidgetRenderer implements ScreenStri
         }
 
         // for legacy support, the viewSizeParam is VIEW_SIZE and viewIndexParam is VIEW_INDEX when the fields are "viewSize" and "viewIndex"
-        if (viewIndexParam.equals("viewIndex")) viewIndexParam = "VIEW_INDEX";
-        if (viewSizeParam.equals("viewSize")) viewSizeParam = "VIEW_SIZE";
+        if (viewIndexParam.equals("viewIndex" + "_" + paginatoNumber)) viewIndexParam = "VIEW_INDEX" + "_" + paginatoNumber;
+        if (viewSizeParam.equals("viewSize" + "_" + paginatoNumber)) viewSizeParam = "VIEW_SIZE" + "_" + paginatoNumber;
 
         ServletContext ctx = (ServletContext) request.getAttribute("servletContext");
         RequestHandler rh = (RequestHandler) ctx.getAttribute("_REQUEST_HANDLER_");
@@ -284,7 +285,7 @@ public class HtmlScreenRenderer extends HtmlWidgetRenderer implements ScreenStri
         }
         String queryString = UtilHttp.urlEncodeArgs(inputFields);
         // strip legacy viewIndex/viewSize params from the query string
-        queryString = UtilHttp.stripViewParamsFromQueryString(queryString);
+        queryString = UtilHttp.stripViewParamsFromQueryString(queryString, "" + paginatoNumber);
         // strip parametrized index/size params from the query string
         HashSet<String> paramNames = new HashSet<String>();
         paramNames.add(viewIndexParam);

applications/order/entitydef/entitymodel_view.xml
@@ -1348,6 +1348,7 @@ under the License.
       <member-entity entity-alias="SI" entity-name="StatusItem"/>
       <alias-all entity-alias="QWE"/>
       <alias-all entity-alias="WE"/>
+      <!--alias name="statusId" field="statusId" entity-alias="SI"/-->      
       <alias name="statusItemDescription" field="description" entity-alias="SI"/>
       <view-link entity-alias="QWE" rel-entity-alias="WE">
         <key-map field-name="workEffortId"/>
@@ -1360,6 +1361,9 @@ under the License.
       </relation>
       <relation type="one-nofk" rel-entity-name="Quote">
         <key-map field-name="quoteId"/>
+      </relation>
+      <relation type="one-nofk" rel-entity-name="StatusItem">
+        <key-map field-name="statusId"/>
       </relation>
     </view-entity>
     <view-entity entity-name="RequirementAndRole"

applications/party/webapp/partymgr/WEB-INF/controller.xml
@@ -396,13 +396,13 @@ under the License.
     <request-map uri="createPartyAttribute">
         <security https="true" auth="true"/>
         <event type="service" path="" invoke="createPartyAttribute"/>
-        <response name="success" type="view" value="EditPartyAttribute"/>
+        <response name="success" type="view" value="viewprofile"/>
         <response name="error" type="view" value="EditPartyAttribute"/>
     </request-map>
     <request-map uri="updatePartyAttribute">
         <security https="true" auth="true"/>
         <event type="service" path="" invoke="updatePartyAttribute"/>
-        <response name="success" type="view" value="EditPartyAttribute"/>
+        <response name="success" type="view" value="viewprofile"/>
         <response name="error" type="view" value="EditPartyAttribute"/>
     </request-map>
     <request-map uri="removePartyAttribute">

applications/party/webapp/partymgr/party/PartyForms.xml
@@ -612,7 +612,8 @@ under the License.
     </form>
     
     <form name="ListCarrierAccounts" type="list" target="updatePartyCarrierAccount"
-        odd-row-style="alternate-row" default-table-style="basic-table hover-bar">
+        odd-row-style="alternate-row" default-table-style="basic-table hover-bar" 
+        paginate="true"  view-size="3" paginate-target="viewprofile" paginate-target-anchor="ListCarrierAccounts">
         <actions>
             <entity-condition entity-name="PartyCarrierAccount" list-name="carrierAccounts" filter-by-date="true">
                 <condition-expr field-name="partyId" env-name="parameters.partyId"></condition-expr>

applications/party/widget/partymgr/ProfileScreens.xml
@@ -270,7 +270,7 @@
                                 <label style="h3" text="${uiLabelMap.PageTitleFinancialAccountSummary}"/>
                             </container>
                             <container style="screenlet-body">
-                                <iterate-section entry-name="ownedFinAccount"  paginate="true" paginate-target="viewprofile?partyId=${parameters.partyId}" view-size="3"  list-name="ownedFinAccountList">
+                                <iterate-section entry-name="ownedFinAccount"  paginate="true" paginate-target="viewprofile" view-size="3"  list-name="ownedFinAccountList">
                                     <section>
                                         <actions>
                                             <entity-condition entity-name="FinAccountTrans" list-name="ownedFinAccountTransList">

framework/base/src/org/ofbiz/base/util/UtilHttp.java
@@ -919,11 +919,18 @@ public class UtilHttp {
     }
 
     public static String stripViewParamsFromQueryString(String queryString) {
+        return stripViewParamsFromQueryString(queryString, null);
+    }
+    
+    public static String stripViewParamsFromQueryString(String queryString, String paginatorNumber) {
         Set<String> paramNames = new HashSet<String>();
-        paramNames.add("VIEW_INDEX");
-        paramNames.add("VIEW_SIZE");
-        paramNames.add("viewIndex");
-        paramNames.add("viewSize");
+        if (UtilValidate.isNotEmpty(paginatorNumber)) {
+            paginatorNumber = "_" + paginatorNumber;
+        }
+        paramNames.add("VIEW_INDEX" + paginatorNumber);
+        paramNames.add("VIEW_SIZE" + paginatorNumber);
+        paramNames.add("viewIndex" + paginatorNumber);
+        paramNames.add("viewSize" + paginatorNumber);
         return stripNamedParamsFromQueryString(queryString, paramNames);
     }
     

framework/widget/src/org/ofbiz/widget/form/ModelForm.java
@@ -1306,6 +1306,9 @@ public class ModelForm extends ModelWidget {
     }
 
     public void preparePager(Map<String, Object> context) {
+        
+        //  increment the paginator
+        this.incrementPaginatorNumber(context);
         this.rowCount = 0;
         String lookupName = this.getListName();
         if (UtilValidate.isEmpty(lookupName)) {
@@ -2185,6 +2188,8 @@ public class ModelForm extends ModelWidget {
         if (UtilValidate.isEmpty(field)) {
             field = DEFAULT_PAG_INDEX_FIELD;
         }
+        //  append the paginator number
+        field = field + "_" + getPaginatorNumber(context);
         return field;
     }
 
@@ -2199,7 +2204,7 @@ public class ModelForm extends ModelWidget {
             // try parameters.VIEW_INDEX as that is an old OFBiz convention
             Map parameters = (Map) context.get("parameters");
             if (parameters != null) {
-                value = parameters.get("VIEW_INDEX");
+                value = parameters.get("VIEW_INDEX" + "_" + getPaginatorNumber(context));
                 
                 if (value == null) {
                     value = parameters.get(field);
@@ -2224,6 +2229,8 @@ public class ModelForm extends ModelWidget {
         if (UtilValidate.isEmpty(field)) {
             field = DEFAULT_PAG_SIZE_FIELD;
         }
+        //  append the paginator number
+        field = field + "_" + getPaginatorNumber(context);
         return field;
     }
 
@@ -2238,7 +2245,7 @@ public class ModelForm extends ModelWidget {
                 // try parameters.VIEW_SIZE as that is an old OFBiz convention
                 Map parameters = (Map) context.get("parameters");
                 if (parameters != null) {
-                    value = parameters.get("VIEW_SIZE");
+                    value = parameters.get("VIEW_SIZE" + "_" + getPaginatorNumber(context));
 
                     if (value == null) {
                         value = parameters.get(field);

commit 145204ab6843744a7233d3d7094011ead937ac1e

Fix an issue introduced in  r725053. This commit managed  "multi-pagination in a page" see https://issues.apache.org/jira/browse/OFBIZ-1935 This is a quick fix. I wonder if we should keep the screenlet navigation-form-name attribute (at least as is). This for 2 reasons: . Less informations/features than with default (pages numbers, ability to jump to any page) . The last button does not always work (try with widget.form.defaultViewSize=2 when listing invoices)
On the other hand it's cool to have all informations in the screenlet. But then we should extend it to work for the 2 points above

git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@729402 13f79535-47bb-0310-9956-ffa450edef68

framework/widget/src/org/ofbiz/widget/ModelWidget.java
@@ -23,6 +23,7 @@ import java.util.Map;
 import org.w3c.dom.Element;
 import org.ofbiz.base.util.UtilGenerics;
 import org.ofbiz.base.util.UtilProperties;
+import org.ofbiz.base.util.UtilValidate;
 
 /**
  * Widget Library - Widget model class. ModelWidget is a base class that is
@@ -130,8 +131,13 @@ public class ModelWidget implements Serializable {
     public void incrementPaginatorNumber(Map<String, Object> context) {    
         Map<String, Object> globalCtx = UtilGenerics.checkMap(context.get("globalContext"));
         if (globalCtx != null) {
-            Integer paginateNumberInt = Integer.valueOf(getPaginatorNumber(context) + 1);
-            globalCtx.put("PAGINATOR_NUMBER", paginateNumberInt);
+            Boolean NO_PAGINATOR = (Boolean) globalCtx.get("NO_PAGINATOR");
+            if (UtilValidate.isNotEmpty(NO_PAGINATOR)) {
+                globalCtx.remove("NO_PAGINATOR");
+            } else {
+                Integer paginateNumberInt = Integer.valueOf(getPaginatorNumber(context) + 1);
+                globalCtx.put("PAGINATOR_NUMBER", paginateNumberInt);
+            }
         }     
     }
     

framework/widget/src/org/ofbiz/widget/html/HtmlScreenRenderer.java
@@ -246,7 +246,7 @@ public class HtmlScreenRenderer extends HtmlWidgetRenderer implements ScreenStri
         }
 
         // get the parametrized pagination index and size fields
-        int paginatoNumber = modelForm.getPaginatorNumber(context);
+        int paginatorNumber = modelForm.getPaginatorNumber(context);
         String viewIndexParam = modelForm.getPaginateIndexField(context);
         String viewSizeParam = modelForm.getPaginateSizeField(context);
 
@@ -272,8 +272,8 @@ public class HtmlScreenRenderer extends HtmlWidgetRenderer implements ScreenStri
         }
 
         // for legacy support, the viewSizeParam is VIEW_SIZE and viewIndexParam is VIEW_INDEX when the fields are "viewSize" and "viewIndex"
-        if (viewIndexParam.equals("viewIndex" + "_" + paginatoNumber)) viewIndexParam = "VIEW_INDEX" + "_" + paginatoNumber;
-        if (viewSizeParam.equals("viewSize" + "_" + paginatoNumber)) viewSizeParam = "VIEW_SIZE" + "_" + paginatoNumber;
+        if (viewIndexParam.equals("viewIndex" + "_" + paginatorNumber)) viewIndexParam = "VIEW_INDEX" + "_" + paginatorNumber;
+        if (viewSizeParam.equals("viewSize" + "_" + paginatorNumber)) viewSizeParam = "VIEW_SIZE" + "_" + paginatorNumber;
 
         ServletContext ctx = (ServletContext) request.getAttribute("servletContext");
         RequestHandler rh = (RequestHandler) ctx.getAttribute("_REQUEST_HANDLER_");
@@ -285,7 +285,7 @@ public class HtmlScreenRenderer extends HtmlWidgetRenderer implements ScreenStri
         }
         String queryString = UtilHttp.urlEncodeArgs(inputFields);
         // strip legacy viewIndex/viewSize params from the query string
-        queryString = UtilHttp.stripViewParamsFromQueryString(queryString, "" + paginatoNumber);
+        queryString = UtilHttp.stripViewParamsFromQueryString(queryString, "" + paginatorNumber);
         // strip parametrized index/size params from the query string
         HashSet<String> paramNames = new HashSet<String>();
         paramNames.add(viewIndexParam);
@@ -382,6 +382,8 @@ public class HtmlScreenRenderer extends HtmlWidgetRenderer implements ScreenStri
             HttpServletRequest request = (HttpServletRequest) context.get("request");
             HttpServletResponse response = (HttpServletResponse) context.get("response");
             if (request != null && response != null) {
+                Map<String, Object> globalCtx = UtilGenerics.checkMap(context.get("globalContext"));
+                globalCtx.put("NO_PAGINATOR", true);
                 FormStringRenderer savedRenderer = (FormStringRenderer) context.get("formStringRenderer");
                 HtmlFormRenderer renderer = new HtmlFormRenderer(request, response);
                 renderer.setRenderPagination(false);