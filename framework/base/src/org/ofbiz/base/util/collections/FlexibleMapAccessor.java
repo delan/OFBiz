delan/OFBiz

commit 13af6e92a42fb2ed40259eb7fbd11c845bebf29c

No functional changes, simple code refactoring around UtilValidate.is(Not)Empty()
Changes implementation of UtilValidate.isEmpty() for Collection and Map by using isEmpty() instead of c.size() == 0). It's more clear and as efficient. Removes also useless parentheses there.

Makes use of UtilValidate.is(Not)Empty() everywhere it makes senses. This for 2 reasons:
* Make the code more readable by replacing the (!)null/isEmpty pattern.
* Avoid possible NPEs when only isEmpty() was used (after Entity.find... for instance). I found a lot of cases (242 today) where isEmpty() could perhaps be replaced in Framework. But I guess no NPE checks were missing there and anyway it was too much for me to check them all... So I only did the relevant changes in Applications...



git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1169478 13f79535-47bb-0310-9956-ffa450edef68

framework/base/src/org/ofbiz/base/util/collections/FlexibleMapAccessor.java
@@ -121,7 +121,7 @@ public class FlexibleMapAccessor<T> implements Serializable {
      * @return the found value
      */
     public T get(Map<String, ? extends Object> base, Locale locale) {
-        if (base == null || this.isEmpty()) {
+        if (UtilValidate.isEmpty(base)) {
             return null;
         }
         if (!base.containsKey(UelUtil.localizedMapLocaleKey) && locale != null) {

applications/content/src/org/ofbiz/content/content/ContentServices.java
@@ -91,7 +91,7 @@ public class ContentServices {
             return ServiceUtil.returnError(UtilProperties.getMessage(resource, "ContentAssocRetrievingError", UtilMisc.toMap("errorString", e.toString()), locale));
         }
 
-        if (targetOperations == null || targetOperations.isEmpty()) {
+        if (UtilValidate.isEmpty(targetOperations)) {
             results.put("contentList", contentList);
             return results;
         }

applications/manufacturing/src/org/ofbiz/manufacturing/jobshopmgt/ProductionRunServices.java
@@ -3063,7 +3063,7 @@ public class ProductionRunServices {
                                  "userLogin", userLogin);
             resultService = dispatcher.runSync("getManufacturingComponents", serviceContext);
             List<Map<String, Object>> components = UtilGenerics.checkList(resultService.get("componentsMap"));
-            if (components == null || components.isEmpty()) {
+            if (UtilValidate.isEmpty(components)) {
                 return ServiceUtil.returnError(UtilProperties.getMessage(resource, "ManufacturingProductionRunCannotDecomposingInventoryItemNoComponentsFound", UtilMisc.toMap("productId", inventoryItem.getString("productId")), locale));
             }
             Iterator<Map<String, Object>> componentsIt = components.iterator();

applications/marketing/src/org/ofbiz/sfa/vcard/VCard.java
@@ -122,7 +122,7 @@ public class VCard {
                                                         EntityCondition.makeCondition("geoTypeId", EntityOperator.EQUALS, "COUNTRY"),
                                                         EntityCondition.makeCondition("geoName", EntityOperator.LIKE, workAddress.getCountry())), EntityOperator.AND);
                     countryGeoList = delegator.findList("Geo", cond, null, null, null, true);
-                    if (!countryGeoList.isEmpty()) {
+                    if (UtilValidate.isNotEmpty(countryGeoList)) {
                         GenericValue countryGeo = EntityUtil.getFirst(countryGeoList);
                         serviceCtx.put("countryGeoId", countryGeo.get("geoId"));
                     }
@@ -131,7 +131,7 @@ public class VCard {
                             EntityCondition.makeCondition("geoTypeId", EntityOperator.EQUALS, "STATE"),
                             EntityCondition.makeCondition("geoName", EntityOperator.LIKE, workAddress.getRegion())), EntityOperator.AND);
                     stateGeoList = delegator.findList("Geo", condition, null, null, null, true);
-                    if (!stateGeoList.isEmpty()) {
+                    if (UtilValidate.isNotEmpty(stateGeoList)) {
                         GenericValue stateGeo = EntityUtil.getFirst(stateGeoList);
                         serviceCtx.put("stateProvinceGeoId", stateGeo.get("geoId"));
                     }

applications/order/src/org/ofbiz/order/order/OrderReadHelper.java
@@ -2537,7 +2537,7 @@ public class OrderReadHelper {
 
         List<GenericValue> promoAdjustments = EntityUtil.filterByAnd(allOrderAdjustments, UtilMisc.toMap("orderAdjustmentTypeId", "PROMOTION_ADJUSTMENT"));
 
-        if (!promoAdjustments.isEmpty()) {
+        if (UtilValidate.isNotEmpty(promoAdjustments)) {
 
             Iterator<GenericValue> promoAdjIter = promoAdjustments.iterator();
             while (promoAdjIter.hasNext()) {

applications/order/src/org/ofbiz/order/shoppingcart/ShoppingCart.java
@@ -2791,7 +2791,7 @@ public class ShoppingCart implements Iterable<ShoppingCartItem>, Serializable {
         GenericValue orderTerm = this.getDelegator().makeValue("OrderTerm");
         orderTerm.put("termTypeId", termTypeId);
         if (UtilValidate.isEmpty(orderItemSeqId)) {
-        	orderItemSeqId = "_NA_";
+                orderItemSeqId = "_NA_";
         }
         orderTerm.put("orderItemSeqId", orderItemSeqId);
         orderTerm.put("termValue", termValue);

applications/order/src/org/ofbiz/order/shoppingcart/ShoppingCartServices.java
@@ -633,7 +633,7 @@ public class ShoppingCartServices {
 
         List<GenericValue> adjustments = orh.getOrderHeaderAdjustments();
         // If applyQuoteAdjustments is set to false then standard cart adjustments are used.
-        if (!adjustments.isEmpty()) {
+        if (UtilValidate.isNotEmpty(adjustments)) {
             // The cart adjustments are added to the cart
             cart.getAdjustments().addAll(adjustments);
         }

applications/order/src/org/ofbiz/order/shoppinglist/ShoppingListEvents.java
@@ -377,7 +377,7 @@ public class ShoppingListEvents {
             List<GenericValue> existingLists = delegator.findByAnd("ShoppingList", findMap);
             Debug.logInfo("Finding existing auto-save shopping list with:  \nfindMap: " + findMap + "\nlists: " + existingLists, module);
     
-            if (existingLists != null && !existingLists.isEmpty()) {
+            if (UtilValidate.isNotEmpty(existingLists)) {
                 list = EntityUtil.getFirst(existingLists);
                 autoSaveListId = list.getString("shoppingListId");
             }

commit 0ded7247d4f012fd93d6b64feaec58528340abb8

Fix a bug introduced in r1169478 Thanks to Hans for reporting on dev ML
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1169701 13f79535-47bb-0310-9956-ffa450edef68

framework/base/src/org/ofbiz/base/util/collections/FlexibleMapAccessor.java
@@ -121,7 +121,7 @@ public class FlexibleMapAccessor<T> implements Serializable {
      * @return the found value
      */
     public T get(Map<String, ? extends Object> base, Locale locale) {
-        if (UtilValidate.isEmpty(base)) {
+        if (base == null || this.isEmpty()) {
             return null;
         }
         if (!base.containsKey(UelUtil.localizedMapLocaleKey) && locale != null) {