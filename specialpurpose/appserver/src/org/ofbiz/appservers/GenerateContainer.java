delan/OFBiz

commit d0305c514b476d88999463520c90948709e2a503

No functional changes, only formatting. While looking for other cases like r1459222 and r1509164 introduced by r1300812 and r1305309. I noticed there were a lot of wrong for loops formatting (miss a white space after for), this changes it
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1511742 13f79535-47bb-0310-9956-ffa450edef68

specialpurpose/appserver/src/org/ofbiz/appservers/GenerateContainer.java
@@ -156,7 +156,7 @@ public class GenerateContainer implements Container {
                     throw new ContainerException("Error in Geronimo deployment, please check the log");
                 }
 
-            for(int inst = 0; inst <= instancesNumber; inst++) {
+            for (int inst = 0; inst <= instancesNumber; inst++) {
                 instanceNumber = (inst == 0 ? "" : inst).toString();
                 GenerateGeronimoDeployment geronimoDeployment = new GenerateGeronimoDeployment();
                 List<String> classpathJars = geronimoDeployment.generate(args[0], geronimoHostHome, instanceNumber);
@@ -291,8 +291,8 @@ public class GenerateContainer implements Container {
                         }
                     }
                     is.close();
-                        process.waitFor();
-    //                    System.out.println(process.waitFor());
+                        process.waitfor ();
+    //                    System.out.println(process.waitfor ());
     //                    System.out.println("exit value" + process.exitValue());
                     Debug.logInfo(ofbizName + " undeployment ended" , module);
                 } catch (IOException e) {
@@ -352,8 +352,8 @@ public class GenerateContainer implements Container {
                         }
                     }
                     is.close();
-                    process.waitFor();
-//                    System.out.println(process.waitFor());
+                    process.waitfor ();
+//                    System.out.println(process.waitfor ());
 //                    System.out.println("exit value" + process.exitValue());
                     Debug.logInfo(ofbizName + " deployment ended" , module);
                 } catch (IOException e) {

commit 1031e066f8363c73ac9d6189ceaf093010677c3f

Completes r1512129 which fixed 2 errors introduced by r1511742.
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1512147 13f79535-47bb-0310-9956-ffa450edef68

specialpurpose/appserver/src/org/ofbiz/appservers/GenerateContainer.java
@@ -292,7 +292,7 @@ public class GenerateContainer implements Container {
                     }
                     is.close();
                         process.waitFor();
-    //                    System.out.println(process.waitfor ());
+    //                    System.out.println(process.waitFor ());
     //                    System.out.println("exit value" + process.exitValue());
                     Debug.logInfo(ofbizName + " undeployment ended" , module);
                 } catch (IOException e) {
@@ -353,7 +353,7 @@ public class GenerateContainer implements Container {
                     }
                     is.close();
                     process.waitFor();
-//                    System.out.println(process.waitfor ());
+//                    System.out.println(process.waitFor ());
 //                    System.out.println("exit value" + process.exitValue());
                     Debug.logInfo(ofbizName + " deployment ended" , module);
                 } catch (IOException e) {