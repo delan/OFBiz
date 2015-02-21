delan/OFBiz

commit 52e0458fcb116a5a70c8e9b140b13d79d647d723

A patch from Pierre Smits for "Have parties to be assigned to production run tasks" https://issues.apache.org/jira/browse/OFBIZ-5706 It is not possible to assign parties to execute one or more task in a production run.
And
"ProductionRun doesn't capture and show manufacturing organisation" https://issues.apache.org/jira/browse/OFBIZ-5709
The production run should capture the manufacturing (internal) organisation on creation.

git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1659914 13f79535-47bb-0310-9956-ffa450edef68

applications/manufacturing/webapp/manufacturing/WEB-INF/actions/jobshopmgt/ProductionRunDeclaration.groovy
@@ -83,8 +83,12 @@ if (productionRunId) {
         productionRunData.actualStartDate = productionRun.getGenericValue().getTimestamp("actualStartDate");
         productionRunData.actualCompletionDate = productionRun.getGenericValue().getTimestamp("actualCompletionDate");
         productionRunData.currentStatusId = productionRun.getGenericValue().currentStatusId;
-    	productionRunData.facilityId = productionRun.getGenericValue().facilityId;
-
+        productionRunData.facilityId = productionRun.getGenericValue().facilityId;
+        
+        manufacturer = from("WorkEffortPartyAssignment").where("workEffortId", productionRunId, "roleTypeId", "MANUFACTURER").queryOne();
+        if (manufacturer){
+            productionRunData.manufacturerId = manufacturer.partyId;
+        }
         context.productionRunData = productionRunData;
 
         actionForm = parameters.actionForm ?: "beforeActionProductionRun";

applications/manufacturing/webapp/manufacturing/WEB-INF/actions/jobshopmgt/ViewProductionRun.groovy
@@ -41,6 +41,11 @@ if (productionRunId) {
         productionRunData.quantity = productionRun.getQuantity();
         productionRunData.estimatedStartDate = productionRun.getEstimatedStartDate();
         productionRunData.estimatedCompletionDate = productionRun.getEstimatedCompletionDate();
+
+        manufacturer = from("WorkEffortPartyAssignment").where("workEffortId", productionRunId, "roleTypeId", "MANUFACTURER").queryOne();
+        if (manufacturer){
+            productionRunData.manufacturerId = manufacturer.partyId;
+        }
         context.productionRunData = productionRunData;
 
         // Find all the order items to which this production run is linked.

applications/manufacturing/config/ManufacturingUiLabels.xml
@@ -19,6 +19,9 @@
     under the License.
 -->
 <resource xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://ofbiz.apache.org/dtds/ofbiz-properties.xsd">
+    <property key="AddParty">
+        <value xml:lang="en">Add Party</value>
+    </property>
     <property key="FormFieldTitle_allocatedCost">
         <value xml:lang="de">Verrechnete Kosten</value>
         <value xml:lang="en">Allocated Cost</value>
@@ -3770,6 +3773,10 @@
         <value xml:lang="vi">Chế tác lô hàng được cập nhật không thành công, bạn hãy xem vết để biết thêm chi tiết</value>
         <value xml:lang="zh">生产运行更新失败，请查看日志了解详细信息</value>
         <value xml:lang="zh-TW">生產運行更新失敗,請檢視日誌瞭解詳細資訊</value>
+    </property>
+    <property key="ManufacturingProductionRunParties">
+        <value xml:lang="en">Parties</value>
+        <value xml:lang="nl">Partijen</value>
     </property>
     <property key="ManufacturingProductionRunPrintDocuments">
         <value xml:lang="de">Dokumente für Produktionsauftrage drucken</value>

applications/manufacturing/script/org/ofbiz/manufacturing/jobshopmgt/ProductionRunServices.xml
@@ -20,7 +20,36 @@ under the License.
 
 <simple-methods xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
         xsi:noNamespaceSchemaLocation="http://ofbiz.apache.org/dtds/simple-methods-v2.xsd">
-
+    
+    
+    <simple-method method-name="createProductionRunPartyAssign" 
+        short-description="Assign the selected party to the production run or task">
+        
+        <set field="productionRunId" from-field="parameters.productionRunId"/>
+        <set field="partyId" from-field="parameters.partyId"/>
+        <set field="roleTypeId" from-field="parameters.roleTypeId"/>
+        <set field="statusId" value="PRTYASGN_ASSIGNED"/>
+        <set field="parameters.statusId" value="PRTYASGN_ASSIGNED"/>
+        
+        <if-not-empty field="parameters.workEffortId">
+            <set field="workEffortId" from-field="parameters.workEffortId"/>
+        </if-not-empty>
+        <if-empty field="parameters.workEffortId">
+            <set field="parameters.workEffortId" from-field="productionRunId"/>
+            <set field="workEffortId" from-field="productionRunId"/>
+        </if-empty>
+        
+        <entity-one entity-name="WorkEffort" value-field="workEffort"/>
+        <set field="fromDate" from-field="workEffort.estimatedStartDate"/>
+        <set field="statusDateTime" from-field="workEffort.estimatedStartDate"/>
+        <set field="parameters.statusDateTime" from-field="workEffort.estimatedStartDate"/>
+        <set field="parameters.fromDate" from-field="workEffort.estimatedStartDate"/>
+        <field-to-result field="parameters.productionRunId" result-name="productionRunId" />
+        <field-to-result field="parameters.fromDate" result-name="fromDate" />
+        <call-simple-method method-name="assignPartyToWorkEffort"
+            xml-resource="component://workeffort/script/org/ofbiz/workeffort/workeffort/WorkEffortSimpleServices.xml" />
+        
+    </simple-method>
     <simple-method method-name="createProductionRunAssoc" short-description="Associate the production run to another production run">
         
         <if-compare field="parameters.workFlowSequenceTypeId" operator="equals" value="WF_PREDECESSOR">

applications/manufacturing/servicedef/services_production_run.xml
@@ -38,6 +38,17 @@ under the License.
         <attribute name="productionRunId" type="String" mode="OUT" optional="false"/>
         <attribute name="estimatedCompletionDate" type="java.sql.Timestamp" mode="OUT" optional="true"/>
     </service>
+    <service name="createProductionRunPartyAssign" engine="simple"
+        location="component://manufacturing/script/org/ofbiz/manufacturing/jobshopmgt/ProductionRunServices.xml" invoke="createProductionRunPartyAssign" auth="false">
+        <description>
+            Associate a party to the production run
+        </description>
+        <attribute name="productionRunId" type="String" mode="INOUT" optional="false"/>
+        <attribute name="partyId" type="String" mode="IN" optional="false"/>
+        <attribute name="roleTypeId" type="String" mode="IN" optional="false"/>
+        <attribute name="workEffortId" mode="IN" type="String" optional="true"/>
+        <attribute name="fromDate" type="java.sql.Timestamp" mode="INOUT" optional="true"/>
+    </service>
     <service name="createProductionRunAssoc" engine="simple"
                 location="component://manufacturing/script/org/ofbiz/manufacturing/jobshopmgt/ProductionRunServices.xml" invoke="createProductionRunAssoc" auth="false">
         <description>

applications/manufacturing/webapp/manufacturing/WEB-INF/controller.xml
@@ -52,6 +52,10 @@ under the License.
     </request-map>
 
     <!-- Lookup request mappings -->
+    <request-map uri="LookupPartyName">
+        <security auth="true" https="true"/>
+        <response name="success" type="view" value="LookupPartyName"/>
+    </request-map>
     <request-map uri="LookupCustomerName">
         <security auth="true" https="true"/>
         <response name="success" type="view" value="LookupCustomerName"/>
@@ -467,6 +471,19 @@ under the License.
         <security https="true" auth="true"/>
         <response name="success" type="view" value="LinkProductionRun"/>
     </request-map>
+    <request-map uri="assocProductionRunParty">
+        <security https="true" auth="true"/>
+        <response name="success" type="view" value="assocProdRunParty"/>
+    </request-map>
+    <request-map uri="createProductionRunPartyAssign">
+        <security https="true" auth="true"/>
+        <event type="service" invoke="createProductionRunPartyAssign"/>
+        <response name="success" type="view" value="ProductionRunParties"/>
+    </request-map>
+    <request-map uri="productionRunParties">
+        <security https="true" auth="true"/>
+        <response name="success" type="view" value="ProductionRunParties"/>
+    </request-map>
     <request-map uri="createProductionRunAssoc">
         <security https="true" auth="true"/>
         <event type="service" invoke="createProductionRunAssoc"/>
@@ -789,6 +806,7 @@ under the License.
     <view-map name="EditRoutingProductLink" page="component://manufacturing/widget/manufacturing/RoutingScreens.xml#EditRoutingProductLink" type="screen"/>
     <view-map name="EditRoutingTaskFixedAssets" page="component://manufacturing/widget/manufacturing/RoutingScreens.xml#EditRoutingTaskFixedAssets" type="screen"/>
     <!-- Lookup view mappings -->
+    <view-map name="LookupPartyName" type="screen" page="component://party/widget/partymgr/LookupScreens.xml#LookupPartyName"/>
     <view-map name="LookupCustomerName" type="screen" page="component://party/widget/partymgr/LookupScreens.xml#LookupCustomerName"/>
     <view-map name="LookupProduct" page="component://product/widget/catalog/LookupScreens.xml#LookupProduct" type="screen"/>
     <view-map name="LookupVariantProduct" page="component://product/widget/catalog/LookupScreens.xml#LookupVariantProduct" type="screen"/>
@@ -815,6 +833,8 @@ under the License.
     <view-map name="CreateProductionRun" page="component://manufacturing/widget/manufacturing/JobshopScreens.xml#CreateProductionRun" type="screen"/>
     <view-map name="FindProductionRun" page="component://manufacturing/widget/manufacturing/JobshopScreens.xml#FindProductionRun" type="screen"/>
     <view-map name="EditProductionRun" page="component://manufacturing/widget/manufacturing/JobshopScreens.xml#EditProductionRun" type="screen"/>
+    <view-map name="assocProdRunParty" page="component://manufacturing/widget/manufacturing/JobshopScreens.xml#AssocProdRunParty" type="screen"/>
+    <view-map name="ProductionRunParties" page="component://manufacturing/widget/manufacturing/JobshopScreens.xml#ProductionRunParties" type="screen"/>
     <view-map name="LinkProductionRun" page="component://manufacturing/widget/manufacturing/JobshopScreens.xml#LinkProductionRun" type="screen"/>
     <view-map name="PrintProductionRun" type="screenfop" page="component://manufacturing/widget/manufacturing/JobshopScreens.xml#ProductionRunPdf" content-type="application/pdf" encoding="none"/>
     <view-map name="ProductionRunDeclaration" page="component://manufacturing/widget/manufacturing/JobshopScreens.xml#ProductionRunDeclaration" type="screen"/>

applications/manufacturing/widget/manufacturing/JobshopScreens.xml
@@ -73,6 +73,18 @@ under the License.
             </widgets>
         </section>
     </screen>
+    <screen name="CreateProductionRunParty">
+        <section>
+            <actions>
+                <set field="productionRunId" from-field="parameters.productionRunId"/>
+            </actions>
+            <widgets>
+                <screenlet title="${uiLabelMap.AddParty}">
+                    <include-form name="AssocProductionRunParty" location="component://manufacturing/widget/manufacturing/ProductionRunForms.xml"/>
+                </screenlet>
+            </widgets>
+        </section>
+    </screen>
     <screen name="EditProductionRun">
         <section>
             <actions>
@@ -335,6 +347,93 @@ under the License.
                         </screenlet>
                     </decorator-section>
                 </decorator-screen>
+            </widgets>
+        </section>
+    </screen>
+    <screen name="ProductionRunParties">
+        <section>
+            <actions>
+                <set field="titleProperty" value="ManufacturingProductionRunAssocs"/>
+                <set field="tabButtonItem" value="parties"/>
+                <set field="headerItem" value="jobshop"/>
+
+                <set field="productionRunId" from-field="parameters.productionRunId"/>
+                <entity-one entity-name="WorkEffort" value-field="productionRun">
+                    <field-map field-name="workEffortId" from-field="productionRunId"/>
+                </entity-one>
+                <!-- Find all the work efforts that must be completed before this one. -->
+                <entity-condition entity-name="WorkEffortAssoc" list="mandatoryWorkEfforts">
+                    <condition-list>
+                        <condition-expr field-name="workEffortIdTo" from-field="productionRunId"/>
+                        <condition-expr field-name="workEffortAssocTypeId" value="WORK_EFF_PRECEDENCY"/>
+                    </condition-list>
+                </entity-condition>
+                <!-- Find all the work efforts that are dependent from this one. -->
+                <entity-condition entity-name="WorkEffortAssoc" list="dependentWorkEfforts">
+                    <condition-list>
+                        <condition-expr field-name="workEffortIdFrom" from-field="productionRunId"/>
+                        <condition-expr field-name="workEffortAssocTypeId" value="WORK_EFF_PRECEDENCY"/>
+                    </condition-list>
+                </entity-condition>
+            </actions>
+            <widgets>
+                <decorator-screen name="CommonJobshopDecorator" location="${parameters.commonJobshopDecorator}">
+                    <decorator-section name="body">
+                            <section>
+                                <widgets>
+                                    <include-screen name="CreateProductionRunParty"/>
+                                </widgets>
+                            </section>
+                            <section>
+                                <widgets>
+                                    <include-screen name="ProductionRunPartyAssocs"/>
+                                </widgets>
+                            </section>
+                            <section>
+                                <widgets>
+                                    <container>
+                                        <include-screen name="ProductionRunTaskPartyAssocs"/>
+                                    </container>
+                                </widgets>
+                            </section>
+                    </decorator-section>
+                </decorator-screen>
+            </widgets>
+        </section>
+    </screen>
+    <screen name="ProductionRunPartyAssocs">
+        <section>
+            <actions>
+                <set field="productionRunId" from-field="parameters.productionRunId"/>
+                <entity-one entity-name="WorkEffort" value-field="productionRun">
+                    <field-map field-name="workEffortId" from-field="productionRunId"/>
+                </entity-one>
+                <entity-condition entity-name="WorkEffortPartyAssignment" list="productionRunParties">
+                    <condition-list>
+                        <condition-expr field-name="workEffortId" from-field="productionRunId"/>
+                    </condition-list>
+                </entity-condition>
+            </actions>
+            <widgets>
+                <screenlet>
+                    <include-form name="ProductionRunPartyAssocs" location="component://manufacturing/widget/manufacturing/ProductionRunForms.xml"/>
+                </screenlet>
+            </widgets>
+        </section>
+    </screen>
+    <screen name="ProductionRunTaskPartyAssocs">
+        <section>
+            <actions>
+                <set field="productionRunId" from-field="parameters.productionRunId"/>
+                <entity-one entity-name="WorkEffort" value-field="productionRun">
+                    <field-map field-name="workEffortId" from-field="productionRunId"/>
+                </entity-one>
+                <script location="component://manufacturing/webapp/manufacturing/WEB-INF/actions/jobshopmgt/ProductionRunTaskParties.groovy"/>
+            </actions>
+            <widgets>
+                <screenlet>
+                    <include-form name="ProductionRunTaskPartyAssocs" location="component://manufacturing/widget/manufacturing/ProductionRunForms.xml"/>
+                </screenlet>
             </widgets>
         </section>
     </screen>

applications/manufacturing/widget/manufacturing/ManufacturingMenus.xml
@@ -165,6 +165,11 @@ under the License.
                 <parameter param-name="productionRunId"/>
             </link>
         </menu-item>
+        <menu-item name="parties" title="${uiLabelMap.ManufacturingProductionRunParties}">
+            <link target="productionRunParties">
+                <parameter param-name="productionRunId"/>
+            </link>
+        </menu-item>
         <menu-item name="assocs" title="${uiLabelMap.ManufacturingProductionRunAssocs}">
             <link target="ProductionRunAssocs">
                 <parameter param-name="productionRunId"/>

commit 1ffda614676374f29e84dbc28e5c88d79c3f44c4

Fixes a bug introduced by r1659914 for OFBIZ-5706 & OFBIZ-5709
Thanks to Deepak's report on dev ML:

> +        manufacturer = from("WorkEffortPartyAssignment").where("workEffortId", productionRunId, "roleTypeId", "MANUFACTURER").queryOne();
Here we need to use filterByDate for “ WorkEffortPartyAssignment”.

Actually it was a bit more because WorkEffortPartyAssignment PK is 
      <prim-key field="workEffortId"/>
      <prim-key field="partyId"/>
      <prim-key field="roleTypeId"/>
      <prim-key field="fromDate"/>

So I have also replaced .queryOne() by .queryFirst()



git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1661323 13f79535-47bb-0310-9956-ffa450edef68

applications/manufacturing/webapp/manufacturing/WEB-INF/actions/jobshopmgt/ProductionRunDeclaration.groovy
@@ -84,8 +84,8 @@ if (productionRunId) {
         productionRunData.actualCompletionDate = productionRun.getGenericValue().getTimestamp("actualCompletionDate");
         productionRunData.currentStatusId = productionRun.getGenericValue().currentStatusId;
         productionRunData.facilityId = productionRun.getGenericValue().facilityId;
-        
-        manufacturer = from("WorkEffortPartyAssignment").where("workEffortId", productionRunId, "roleTypeId", "MANUFACTURER").queryOne();
+
+        manufacturer = from("WorkEffortPartyAssignment").where("workEffortId", productionRunId, "roleTypeId", "MANUFACTURER").filterByDate().queryFirst();
         if (manufacturer){
             productionRunData.manufacturerId = manufacturer.partyId;
         }

applications/manufacturing/webapp/manufacturing/WEB-INF/actions/jobshopmgt/ViewProductionRun.groovy
@@ -42,7 +42,7 @@ if (productionRunId) {
         productionRunData.estimatedStartDate = productionRun.getEstimatedStartDate();
         productionRunData.estimatedCompletionDate = productionRun.getEstimatedCompletionDate();
 
-        manufacturer = from("WorkEffortPartyAssignment").where("workEffortId", productionRunId, "roleTypeId", "MANUFACTURER").queryOne();
+		manufacturer = from("WorkEffortPartyAssignment").where("workEffortId", productionRunId, "roleTypeId", "MANUFACTURER").filterByDate().queryFirst();
         if (manufacturer){
             productionRunData.manufacturerId = manufacturer.partyId;
         }